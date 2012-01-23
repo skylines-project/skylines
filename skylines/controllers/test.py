089357116c49f0d540da2e519b2110c0093d1e28
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2012-01-23 23:41:07 +0100

    controllers/test: Added display_flight page
    Fullscreen GoogleMaps preview of the flight

--- skylines/controllers/test.py
+++ skylines/controllers/test.py
@@ -4,6 +4,7 @@ Test Controller
 
 from tg import expose
 from skylines.lib.base import BaseController
+from skylines.lib.igc.parser import SimpleParser
 
 
 class TestController(BaseController):
@@ -11,3 +12,12 @@ class TestController(BaseController):
     def index(self):
         """Handle the front-page."""
         return dict(page = 'test')
+
+    @expose('skylines.templates.test.display_flight')
+    def display_flight(self, igc_file):
+        """Handle the flight display page."""
+        parser = SimpleParser()
+        fixes = parser.parse(igc_file.file)
+
+        content = fixes
+        return dict(page = 'test', content = content)
