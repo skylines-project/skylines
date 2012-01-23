f88612fbc9d430ecf72e2bb7c70a9d2b553b1354
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2012-01-23 22:34:44 +0100

    controllers/root: Fixed format

--- skylines/controllers/root.py
+++ skylines/controllers/root.py
@@ -31,58 +31,58 @@ class RootController(BaseController):
 
     """
     secc = SecureController()
-    admin = AdminController(model, DBSession, config_type=TGAdminConfig)
+    admin = AdminController(model, DBSession, config_type = TGAdminConfig)
 
     error = ErrorController()
 
     @expose('skylines.templates.index')
     def index(self):
         """Handle the front-page."""
-        return dict(page='index')
+        return dict(page = 'index')
 
     @expose('skylines.templates.about')
     def about(self):
         """Handle the 'about' page."""
-        return dict(page='about')
+        return dict(page = 'about')
 
     @expose('skylines.templates.environ')
     def environ(self):
         """This method showcases TG's access to the wsgi environment."""
-        return dict(environment=request.environ)
+        return dict(environment = request.environ)
 
     @expose('skylines.templates.data')
     @expose('json')
     def data(self, **kw):
         """This method showcases how you can use the same controller for a data page and a display page"""
-        return dict(params=kw)
+        return dict(params = kw)
     @expose('skylines.templates.authentication')
     def auth(self):
         """Display some information about auth* on this application."""
-        return dict(page='auth')
+        return dict(page = 'auth')
 
     @expose('skylines.templates.index')
-    @require(predicates.has_permission('manage', msg=l_('Only for managers')))
+    @require(predicates.has_permission('manage', msg = l_('Only for managers')))
     def manage_permission_only(self, **kw):
         """Illustrate how a page for managers only works."""
-        return dict(page='managers stuff')
+        return dict(page = 'managers stuff')
 
     @expose('skylines.templates.index')
-    @require(predicates.is_user('editor', msg=l_('Only for the editor')))
+    @require(predicates.is_user('editor', msg = l_('Only for the editor')))
     def editor_user_only(self, **kw):
         """Illustrate how a page exclusive for the editor works."""
-        return dict(page='editor stuff')
+        return dict(page = 'editor stuff')
 
     @expose('skylines.templates.login')
-    def login(self, came_from=lurl('/')):
+    def login(self, came_from = lurl('/')):
         """Start the user login."""
         login_counter = request.environ['repoze.who.logins']
         if login_counter > 0:
             flash(_('Wrong credentials'), 'warning')
-        return dict(page='login', login_counter=str(login_counter),
-                    came_from=came_from)
+        return dict(page = 'login', login_counter = str(login_counter),
+                    came_from = came_from)
 
     @expose()
-    def post_login(self, came_from=lurl('/')):
+    def post_login(self, came_from = lurl('/')):
         """
         Redirect the user to the initially requested page on successful
         authentication or redirect her back to the login page if login failed.
@@ -91,13 +91,13 @@ class RootController(BaseController):
         if not request.identity:
             login_counter = request.environ['repoze.who.logins'] + 1
             redirect('/login',
-                params=dict(came_from=came_from, __logins=login_counter))
+                params = dict(came_from = came_from, __logins = login_counter))
         userid = request.identity['repoze.who.userid']
         flash(_('Welcome back, %s!') % userid)
         redirect(came_from)
 
     @expose()
-    def post_logout(self, came_from=lurl('/')):
+    def post_logout(self, came_from = lurl('/')):
         """
         Redirect the user to the initially requested page on logout and say
         goodbye as well.
