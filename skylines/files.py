1c23bcd5ce4b8b351d1f8175c158f54749c7fc0f
Max Kellermann <max@duempel.org>
2012-05-28 12:29:18 +0200

    files: add assertions

--- skylines/files.py
+++ skylines/files.py
@@ -6,6 +6,8 @@ import re
 from tg import config
 
 def sanitise_filename(name):
+    assert isinstance(name, str) or isinstance(name, unicode)
+
     name = os.path.basename(name)
     name = re.sub(r'[^-_.a-zA-Z0-9]', '_', name)
     name = unicode(name)
@@ -16,18 +18,26 @@ def sanitise_filename(name):
     return name
 
 def filename_to_path(name):
+    assert isinstance(name, str) or isinstance(name, unicode)
+
     return os.path.join(config['skylines.files.path'], name)
 
 def open_file(name):
+    assert isinstance(name, str) or isinstance(name, unicode)
+
     return file(filename_to_path(name))
 
 def next_filename(name):
+    assert isinstance(name, str) or isinstance(name, unicode)
+
     i = name.rfind('.')
     if i < 0:
         i = len(name)
     return name[:i] + '_' + name[i:]
 
 def add_file(name, f):
+    assert isinstance(name, str) or isinstance(name, unicode)
+
     while True:
         path = filename_to_path(name)
         if not os.access(path, os.F_OK):
@@ -41,6 +51,8 @@ def add_file(name, f):
     return name
 
 def delete_file(name):
+    assert isinstance(name, str) or isinstance(name, unicode)
+
     path = filename_to_path(name)
     try:
         os.unlink(path)
