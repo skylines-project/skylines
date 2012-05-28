439eaee540038fc829b5e0dedd9f687fc98eac75
Max Kellermann <max@duempel.org>
2012-05-28 21:14:28 +0200

    controllers.club: add club editor

--- skylines/controllers/clubs.py
+++ skylines/controllers/clubs.py
@@ -4,11 +4,20 @@ from tg import expose, validate, redirect
 from tg.i18n import ugettext as _, lazy_ugettext as l_
 from webob.exc import HTTPNotFound, HTTPForbidden
 from sprox.tablebase import TableBase
-from sprox.formbase import AddRecordForm, Field
+from sprox.formbase import AddRecordForm, EditableForm, Field
 from sprox.fillerbase import TableFiller
 from tw.forms import TextField
 from skylines.lib.base import BaseController
 from skylines.model import DBSession, User, Group, Club
+
+class EditClubForm(EditableForm):
+    __model__ = Club
+    __hide_fields__ = ['id']
+    __limit_fields__ = ['name']
+    __base_widget_args__ = dict(action='save')
+    name = TextField
+
+edit_club_form = EditClubForm(DBSession)
 
 class PilotsTable(TableBase):
     __model__ = User
@@ -41,6 +50,26 @@ class ClubController(BaseController):
     @expose('skylines.templates.clubs.view')
     def index(self):
         return dict(page='settings', club=self.club)
+
+    @expose('skylines.templates.generic.form')
+    def edit(self, **kwargs):
+        if not self.club.is_writable():
+            raise HTTPForbidden
+
+        return dict(page='settings', title=_('Edit Club'),
+                    form=edit_club_form,
+                    values=self.club)
+
+    @expose()
+    @validate(form=edit_club_form, error_handler=edit)
+    def save(self, name, **kwargs):
+        if not self.club.is_writable():
+            raise HTTPForbidden
+
+        self.club.name = name
+        DBSession.flush()
+
+        redirect('.')
 
     @expose('skylines.templates.clubs.pilots')
     def pilots(self):
