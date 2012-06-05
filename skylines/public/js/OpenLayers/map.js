a71efaae00c54e0bfed0b871a209d7ddaa374d9d
Tobias Lohner <tobias@lohner-net.de>
2012-06-05 21:53:57 +0200

    public/js/OpenLayers/map: use XYZ layer instead of TMS for airspaces

--- skylines/public/js/OpenLayers/map.js
+++ skylines/public/js/OpenLayers/map.js
@@ -21,19 +21,8 @@ function initOpenLayers() {
 
   map.addLayer(osmLayer);
 
-  var airspace = new OpenLayers.Layer.TMS("Airspace",
-    "http://www.prosoar.de/airspace/", {
-    type: 'png',
-    getURL: function osm_getTileURL(bounds) {
-      var res = this.map.getResolution();
-      var x = Math.round((bounds.left - this.maxExtent.left) / (res * this.tileSize.w));
-      var y = Math.round((this.maxExtent.top - bounds.top) / (res * this.tileSize.h));
-      var z = this.map.getZoom();
-      var limit = Math.pow(2, z);
-      if (y < 0 || y >= limit) return OpenLayers.Util.getImagesLocation() + "empty.png";
-      else x = ((x % limit) + limit) % limit;
-      return this.url + z + "/" + x + "/" + y + "." + this.type;
-    },
+  var airspace = new OpenLayers.Layer.XYZ("Airspace",
+    "http://www.prosoar.de/airspace/${z}/${x}/${y}.png", {
     isBaseLayer: false,
     transparent: true,
     'visibility': true,
