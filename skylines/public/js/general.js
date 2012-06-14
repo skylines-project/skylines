ec086aea5bc1c97361f2bb3b8ddc3fd1a9546587
Tobias Lohner <tobias@lohner-net.de>
2012-06-14 23:36:41 +0200

    templates.flights: allow pinning of flights

--- skylines/public/js/general.js
+++ skylines/public/js/general.js
@@ -4,4 +4,109 @@ function pad(num, size) {
   return s.substr(s.length-size);
 }
 
+/**
+ * Function: pinFlight
+ *
+ * Saves the flight id into a cookie
+ *
+ * Parameters:
+ * sfid - {int} SkyLines flight ID
+ */
+function pinFlight(sfid) {
+  var pinnedFlights = getPinnedFlights();
+  for (var i = 0; i < pinnedFlights.length; i++) {
+    if (pinnedFlights[i] == sfid) return;
+  }
+
+  pinnedFlights.push(sfid);
+  $.cookie('SkyLines_pinnedFlights', pinnedFlights.join(","), { path: "/" });
+}
+
+/**
+ * Function: unpinFlight
+ *
+ * Removes a pinned flight
+ *
+ * Parameters:
+ * sfid - {int} SkyLines flight ID
+ */
+function unpinFlight(sfid) {
+  var pinnedFlights = getPinnedFlights();
+  var temp = [];
+
+  for (var i = 0; i < pinnedFlights.length; i++) {
+    if (pinnedFlights[i] != sfid) temp.push(pinnedFlights[i]);
+  }
+
+  $.cookie('SkyLines_pinnedFlights', temp.join(","), { path: "/" });
+}
+
+
+/**
+ * Function: getPinnedFlights
+ *
+ * Gets all pinned flights from the cookie
+ *
+ * Returns:
+ * {Array(int)} Array of SkyLine flight IDs
+ */
+function getPinnedFlights() {
+  var cookie = $.cookie('SkyLines_pinnedFlights');
+
+  if (cookie) {
+    var pinnedFlights = cookie.split(",");
+
+    for (var i = 0; i < pinnedFlights.length; i++) {
+      pinnedFlights[i] = parseInt(pinnedFlights[i]);
+    }
+
+    return pinnedFlights;
+  }
+
+  return [];
+}
+
+/**
+ * Function: isPinnedFlight
+ *
+ * Parameters:
+ * sfid - {int} SkyLines flight ID
+ *
+ * Returns:
+ * {bool} pinned flight or not pinned flight
+ */
+function isPinnedFlight(sfid) {
+  var pinnedFlights = getPinnedFlights();
+
+  for (var i = 0; i < pinnedFlights.length; i++) {
+    if (pinnedFlights[i] == sfid) return true;
+  }
+
+  return false;
+}
+
+/**
+ * Function: pinButton
+ *
+ * Parameters:
+ * element - {Object} jQuery element which will be the button
+ * sfid - {int} SkyLines flight ID
+ */
+function pinButton(element, sfid) {
+  element.off("click");
+
+  if (!isPinnedFlight(sfid)) {
+    element.html("<i class='icon-star-empty'></i>Click to pin");
+    element.click(function() {
+      pinFlight(sfid);
+      pinButton($(this), sfid);
+    });
+  } else {
+    element.html("<i class='icon-star'></i>Flight pinned!");
+    element.click(function() {
+      unpinFlight(sfid);
+      pinButton($(this), sfid);
+    });
+  }
+}
 
