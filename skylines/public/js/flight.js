de5af37ab31edae22a5a822b5353e83b066ca274
Tobias Lohner <tobias@lohner-net.de>
2012-06-16 16:28:26 +0200

    public.js.flights: change track colors a bit

--- skylines/public/js/flight.js
+++ skylines/public/js/flight.js
@@ -28,8 +28,8 @@ var barogram_h = [];
  *
  * List of colors for flight path display
  */
-var colors = ['#bf2fa2', '#2f69bf', '#d63a35', '#d649ff'];
-
+//var colors = ['#bf2fa2', '#2f69bf', '#d63a35', '#d649ff'];
+var colors = ['#004bbd', '#bf0099', '#cf7c00', '#ff0000', '#00c994'];
 
 /**
  * Function initOpenLayers
