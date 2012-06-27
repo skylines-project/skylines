584275b619442f28138affab5222949b5291f10b
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2012-06-27 02:24:07 +0200

    model/club: Fixed format

--- skylines/model/club.py
+++ skylines/model/club.py
@@ -8,19 +8,23 @@ from skylines.model.auth import User
 from skylines.model import DeclarativeBase
 from tg import request
 
+
 class Club(DeclarativeBase):
     __tablename__ = 'clubs'
 
     id = Column(Integer, autoincrement=True, primary_key=True)
     name = Column(Unicode(255), unique=True, nullable=False)
 
-    owner_id = Column(Integer, ForeignKey('tg_user.user_id', use_alter=True, name="tg_user.user_id"))
-    owner = relation('User', primaryjoin=(owner_id==User.user_id))
+    owner_id = Column(Integer, ForeignKey('tg_user.user_id', use_alter=True,
+                                           name="tg_user.user_id"))
+
+    owner = relation('User', primaryjoin=(owner_id == User.user_id))
+
     time_created = Column(DateTime, nullable=False, default=datetime.now)
 
     website = Column(Unicode(255))
 
-    members = relation('User', primaryjoin=(User.club_id==id),
+    members = relation('User', primaryjoin=(User.club_id == id),
                        order_by=(User.display_name),
                        backref='club', post_update=True)
     flights = relation('Flight', backref='club')
