ee5678496aaa661af9429c9d74293b73e489d291
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2012-06-28 01:21:15 +0200

    js/flight: Moved OpenLayers code to map.js

--- skylines/public/js/flight.js
+++ skylines/public/js/flight.js
@@ -15,14 +15,6 @@ var top_flight = 0;
 
 
 /**
- * map
- *
- * Holds the OpenLayers map
- */
-var map;
-
-
-/**
  * barogram
  *
  * Holds the Raphael instance
@@ -52,38 +44,7 @@ var colors = ['#004bbd', '#bf0099', '#cf7c00', '#ff0000', '#00c994'];
  * Initialize the map and add airspace and flight path layers.
  */
 
-function initOpenLayers() {
-  OpenLayers.ImgPath = "/images/OpenLayers/"
-
-  map = new OpenLayers.Map("map_canvas", {
-    projection: "EPSG:900913",
-    controls: [],
-    theme: null
-  });
-
-  map.addControl(new OpenLayers.Control.PanZoomBar());
-  map.addControl(new OpenLayers.Control.Navigation());
-  map.addControl(new OpenLayers.Control.KeyboardDefaults());
-  map.addControl(new OpenLayers.Control.Attribution());
-  map.addControl(new OpenLayers.Control.ScaleLine({geodesic: true}));
-
-  var osmLayer = new OpenLayers.Layer.OSM("OpenStreetMap");
-  osmLayer.addOptions({
-    transitionEffect: "resize",
-    numZoomLevels: 18
-  });
-
-  map.addLayer(osmLayer);
-
-  var airspace = new OpenLayers.Layer.XYZ("Airspace",
-    "http://www.prosoar.de/airspace/${z}/${x}/${y}.png", {
-    isBaseLayer: false,
-    transparent: true,
-    'visibility': true,
-    'displayInLayerSwitcher': true
-  });
-  map.addLayer(airspace);
-
+function initFlightLayer() {
   var default_style = new OpenLayers.Style({
     strokeColor: "${color}",
     strokeWidth: 2
@@ -120,45 +81,22 @@ function initOpenLayers() {
       'plane': plane_style
     })
   });
-     
-  map.addLayer(flightPathLayer);
-//  map.events.register("moveend", flightPathLayer, flightPathLayer.redraw);
 
-  map.setCenter(new OpenLayers.LonLat(30, 0).
-    transform(new OpenLayers.Projection("EPSG:4326"),map.getProjectionObject() ),
-    9);
+  map.addLayer(flightPathLayer);
 
   var initRedrawLayer = function(layer) {
-    if (this.timer) return;
-    this.timer = window.setTimeout(function() { this.timer = null; layer.redraw(); }, 50);
+    if (this.timer)
+      return;
+
+    this.timer = window.setTimeout(function() {
+      this.timer = null; layer.redraw();
+    }, 50);
   };
-  map.events.register("move", this, function() { initRedrawLayer(flightPathLayer); });
+
+  map.events.register("move", this, function() {
+    initRedrawLayer(flightPathLayer);
+  });
 };
-
-/**
- * Function: addGoogleLayer
- *
- * Callback when Google Maps API has been loaded.
- * Add the google physical layer to the map
- */
-function addGoogleLayer() {
-  // add google maps if google script loaded
-  if (window.google) {
-    var google_physical_layer = new OpenLayers.Layer.Google(
-      "Google Physical",
-      {type: google.maps.MapTypeId.TERRAIN}
-    );
-    map.addLayer(google_physical_layer);
-
-    var google_satellite_layer = new OpenLayers.Layer.Google(
-      "Google Satellite",
-      {type: google.maps.MapTypeId.HYBRID}
-    );
-    map.addLayer(google_satellite_layer);
-
-    map.addControl(new SimpleLayerSwitcher());
-  }
-}
 
 /**
  * Function: addFlight
---
+++ skylines/public/js/map.js
@@ -0,0 +1,73 @@
+/**
+ * map
+ *
+ * Holds the OpenLayers map
+ */
+var map;
+
+/**
+ * Function initOpenLayers
+ *
+ * Initialize the map and add airspace and flight path layers.
+ */
+function initOpenLayers() {
+  OpenLayers.ImgPath = "/images/OpenLayers/"
+
+  map = new OpenLayers.Map("map_canvas", {
+    projection: "EPSG:900913",
+    controls: [],
+    theme: null
+  });
+
+  map.addControl(new OpenLayers.Control.PanZoomBar());
+  map.addControl(new OpenLayers.Control.Navigation());
+  map.addControl(new OpenLayers.Control.KeyboardDefaults());
+  map.addControl(new OpenLayers.Control.Attribution());
+  map.addControl(new OpenLayers.Control.ScaleLine({geodesic: true}));
+
+  var osmLayer = new OpenLayers.Layer.OSM("OpenStreetMap");
+  osmLayer.addOptions({
+    transitionEffect: "resize",
+    numZoomLevels: 18
+  });
+
+  map.addLayer(osmLayer);
+
+  var airspace = new OpenLayers.Layer.XYZ("Airspace",
+    "http://www.prosoar.de/airspace/${z}/${x}/${y}.png", {
+    isBaseLayer: false,
+    transparent: true,
+    'visibility': true,
+    'displayInLayerSwitcher': true
+  });
+  map.addLayer(airspace);
+
+  map.setCenter(new OpenLayers.LonLat(30, 0).
+    transform(new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject()),
+    9);
+};
+
+/**
+ * Function: addGoogleLayer
+ *
+ * Callback when Google Maps API has been loaded.
+ * Add the google physical layer to the map
+ */
+function addGoogleLayer() {
+  // add google maps if google script loaded
+  if (window.google) {
+    var google_physical_layer = new OpenLayers.Layer.Google(
+      "Google Physical",
+      {type: google.maps.MapTypeId.TERRAIN}
+    );
+    map.addLayer(google_physical_layer);
+
+    var google_satellite_layer = new OpenLayers.Layer.Google(
+      "Google Satellite",
+      {type: google.maps.MapTypeId.HYBRID}
+    );
+    map.addLayer(google_satellite_layer);
+
+    map.addControl(new SimpleLayerSwitcher());
+  }
+}
