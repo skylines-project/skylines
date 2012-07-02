e5f4a91ace94ff8a9a14c3fca5d0a8a56de0ea40
Max Kellermann <max@duempel.org>
2012-07-02 23:46:57 +0200

    daemon.py: parse *.ini path from command line

--- daemon.py
+++ daemon.py
@@ -10,7 +10,16 @@ from skylines.config.environment import load_environment
 
 sys.path.append(os.path.dirname(sys.argv[0]))
 
-conf = appconfig('config:/etc/skylines/production.ini')
+conf_path = '/etc/skylines/production.ini'
+if len(sys.argv) > 1:
+    conf_path = sys.argv[1]
+    del sys.argv[1]
+
+if len(sys.argv) != 1:
+    print >>sys.stderr, "Usage: %s [config.ini]" % sys.argv[0]
+    sys.exit(1)
+
+conf = appconfig('config:' + os.path.abspath(conf_path))
 load_environment(conf.global_conf, conf.local_conf)
 
 if __name__ == '__main__':
