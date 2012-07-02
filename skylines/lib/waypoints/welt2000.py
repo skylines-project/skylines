bcd162fefc2d817c1b5ea963e70dcbccb8aeec12
Tobias Lohner <tobias@lohner-net.de>
2012-07-02 14:50:59 +0200

    lib.waypoints.welt2000: save welt2000.txt in a temporary path

--- skylines/lib/waypoints/welt2000.py
+++ skylines/lib/waypoints/welt2000.py
@@ -5,7 +5,7 @@ from tg import config
 from skylines.lib.waypoints.welt2000_reader import parse_welt2000_waypoints
 
 def __get_database_file(dir_data):
-    path = os.path.join(dir_data, 'welt2000', 'WELT2000.TXT')
+    path = os.path.join(dir_data, 'WELT2000.TXT')
 
     # Create Welt2000 data folder if necessary
     if not os.path.exists(os.path.dirname(path)):
@@ -25,10 +25,13 @@ def __get_database_file(dir_data):
 
 def get_database(bounds = None):
     # Get Welt2000 file
-    path = __get_database_file(config['skylines.assets.path'])
+    path = __get_database_file(config['skylines.temporary_dir'])
 
     # Parse Welt2000 file
     with open(path, "r") as f:
-        # Return parsed WaypointList
-        return parse_welt2000_waypoints(f, bounds)
+        parsed = parse_welt2000_waypoints(f, bounds)
 
+    os.remove(path)
+
+    # Return parsed WaypointList
+    return parsed
