3b24ff30126945a6eb26e70adc5f2653ac2c300d
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2012-07-03 03:22:00 +0200

    form: Fixed format

--- skylines/form/form.py
+++ skylines/form/form.py
@@ -2,6 +2,7 @@ from tw.forms import Form, SubmitButton
 
 SubmitButton.css_class = 'btn btn-primary'
 
+
 class BootstrapMixin(object):
     """
     Mix-in class for containers that use a table to render their fields
@@ -9,6 +10,7 @@ class BootstrapMixin(object):
     params = ["show_labels", "hover_help"]
     show_labels = True
     hover_help = False
+
 
 class BootstrapForm(Form, BootstrapMixin):
     template = "skylines.templates.form.bootstrap_form"
