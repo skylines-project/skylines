skylines-project/skylines

commit 59dbc5c0f234c24434b8a3d2aede379cca394b4f

controllers.settings: move code to UserController
Allow admins to edit any user.

skylines/controllers/root.py
@@ -16,7 +16,6 @@ from skylines.controllers.clubs import ClubsController
 from skylines.controllers.flights import FlightsController
 from skylines.controllers.ranking import RankingController
 from skylines.controllers.tracking import TrackingController
-from skylines.controllers.settings import SettingsController
 from skylines.controllers.static import StaticResourceController
 from skylines.controllers.statistics import StatisticsController
 
@@ -45,7 +44,6 @@ class RootController(BaseController):
     flights = FlightsController()
     ranking = RankingController()
     tracking = TrackingController()
-    settings = SettingsController()
     static = StaticResourceController()
     statistics = StatisticsController()
 

skylines/controllers/settings.py
@@ -1,87 +1,0 @@
-# -*- coding: utf-8 -*-
-
-from tg import expose, validate, request, redirect
-from repoze.what.predicates import not_anonymous
-from sqlalchemy.sql.expression import and_, or_
-from sprox.formbase import AddRecordForm, EditableForm
-from tw.forms import TextField
-from skylines.lib.base import BaseController
-from skylines.model import DBSession, User, Club, Flight
-from skylines.controllers.users import ClubSelectField
-from skylines.model.igcfile import IGCFile
-from skylines.form import BootstrapForm
-
-class SelectClubForm(EditableForm):
-    __base_widget_type__ = BootstrapForm
-    __model__ = User
-    __hide_fields__ = ['user_id']
-    __limit_fields__ = ['club']
-    club = ClubSelectField
-
-select_club_form = SelectClubForm(DBSession)
-
-
-class NewClubForm(AddRecordForm):
-    __base_widget_type__ = BootstrapForm
-    __model__ = Club
-    __limit_fields__ = ['name']
-    name = TextField
-
-new_club_form = NewClubForm(DBSession)
-
-
-class SettingsController(BaseController):
-    allow_only = not_anonymous()
-
-    @expose('skylines.templates.settings.index')
-    def index(self):
-        user = request.identity['user']
-        return dict(user=user)
-
-    @expose('skylines.templates.settings.change_club')
-    def change_club(self, **kwargs):
-        user = request.identity['user']
-        return dict(user=user,
-                    select_form=select_club_form,
-                    create_form=new_club_form)
-
-    @expose()
-    @validate(form=select_club_form, error_handler=change_club)
-    def select_club(self, club, **kwargs):
-        user = request.identity['user']
-        user.club_id = club
-
-        # assign the user's new club to all of his flights that have
-        # no club yet
-        flights = DBSession.query(Flight).outerjoin(IGCFile)
-        flights = flights.filter(and_(Flight.club_id == None,
-                                      or_(Flight.pilot_id == user.user_id,
-                                          IGCFile.owner_id == user.user_id)))
-        for flight in flights:
-            flight.club_id = club
-
-        DBSession.flush()
-
-        redirect('.')
-
-    @expose()
-    @validate(form=new_club_form, error_handler=change_club)
-    def create_club(self, name, **kw):
-        user = request.identity['user']
-
-        club = Club(name=name)
-        club.owner_id = user.user_id
-        DBSession.add(club)
-
-        user.club = club
-
-        DBSession.flush()
-
-        redirect('.')
-
-    @expose()
-    def tracking_register(self, came_from = '/tracking/info'):
-        user = request.identity['user']
-        user.generate_tracking_key()
-
-        redirect(came_from)

skylines/controllers/users.py
@@ -10,12 +10,12 @@ from formencode.validators import FieldsMatch, Email, String
 from tw.forms import PasswordField, TextField
 from skylines.lib.base import BaseController
 from skylines.model import DBSession, User, Group, Club, Flight, Follower
+from skylines.model.igcfile import IGCFile
 from skylines.form import BootstrapForm
-from sqlalchemy.sql.expression import desc
+from sqlalchemy.sql.expression import desc, and_, or_
 from sqlalchemy import func
 from repoze.what.predicates import not_anonymous, has_permission
 from skylines.model.geo import Location
-
 
 class ClubSelectField(PropertySingleSelectField):
     def _my_update_params(self, d, nullable=False):
@@ -24,6 +24,24 @@ class ClubSelectField(PropertySingleSelectField):
                   [(club.id, club.name) for club in clubs]
         d['options'] = options
         return d
+
+class SelectClubForm(EditableForm):
+    __base_widget_type__ = BootstrapForm
+    __model__ = User
+    __hide_fields__ = ['user_id']
+    __limit_fields__ = ['club']
+    club = ClubSelectField
+
+select_club_form = SelectClubForm(DBSession)
+
+
+class NewClubForm(AddRecordForm):
+    __base_widget_type__ = BootstrapForm
+    __model__ = Club
+    __limit_fields__ = ['name']
+    name = TextField
+
+new_club_form = NewClubForm(DBSession)
 
 user_validator = Schema(chained_validators=(FieldsMatch('password',
                                                         'verify_password',
@@ -95,6 +113,63 @@ class UserController(BaseController):
         DBSession.flush()
 
         redirect('.')
+
+    @expose('skylines.templates.users.change_club')
+    def change_club(self, **kwargs):
+        if not self.user.is_writable():
+            raise HTTPForbidden
+
+        return dict(user=self.user,
+                    select_form=select_club_form,
+                    create_form=new_club_form)
+
+    @expose()
+    @validate(form=select_club_form, error_handler=change_club)
+    def select_club(self, club, **kwargs):
+        if not self.user.is_writable():
+            raise HTTPForbidden
+
+        self.user.club_id = club
+
+        # assign the user's new club to all of his flights that have
+        # no club yet
+        flights = DBSession.query(Flight).outerjoin(IGCFile)
+        flights = flights.filter(and_(Flight.club_id == None,
+                                      or_(Flight.pilot_id == self.user.user_id,
+                                          IGCFile.owner_id == self.user.user_id)))
+        for flight in flights:
+            flight.club_id = club
+
+        DBSession.flush()
+
+        redirect('.')
+
+    @expose()
+    @validate(form=new_club_form, error_handler=change_club)
+    def create_club(self, name, **kw):
+        if not self.user.is_writable():
+            raise HTTPForbidden
+
+        current_user = request.identity['user']
+
+        club = Club(name=name)
+        club.owner_id = current_user.user_id
+        DBSession.add(club)
+
+        self.user.club = club
+
+        DBSession.flush()
+
+        redirect('.')
+
+    @expose()
+    def tracking_register(self, came_from = '/tracking/info'):
+        if not self.user.is_writable():
+            raise HTTPForbidden
+
+        self.user.generate_tracking_key()
+
+        redirect(came_from)
 
     def get_largest_flight(self):
         return DBSession.query(Flight) \

skylines/templates/master/topbar.html
@@ -59,7 +59,7 @@
               <li><a href="/flights/pilot/${request.identity['user'].user_id}"><i class="icon-plane"></i> Flights</a></li>
               <li><a href="/statistics/pilot/${request.identity['user'].user_id}"><i class="icon-tasks"></i> Statistics</a></li>
               <li class="divider"></li>
-              <li><a href="${tg.url('/settings/')}"><i class="icon-user"></i> Settings</a></li>
+              <li><a href="/users/${request.identity['user'].user_id}/"><i class="icon-user"></i> Settings</a></li>
               <li><a href="${tg.url('/logout_handler')}"><i class="icon-signout"></i> Logout</a></li>
             </ul>
           </li>

skylines/templates/settings/index.html
@@ -1,67 +1,0 @@
-<?python
-page = 'settings'
-title = 'Settings'
-?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-                      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"
-      xmlns:py="http://genshi.edgewall.org/"
-      xmlns:xi="http://www.w3.org/2001/XInclude">
-
-  <xi:include href="master/page.html" />
-
-  <head>
-    <meta content="text/html; charset=UTF-8" http-equiv="Content-Type" py:if="False"/>
-    <title>Settings</title>
-  </head>
-
-  <body>
-    <div class="row">
-      <div class="well span8 offset2">
-        <table class="table">
-          <tr>
-            <th>Login</th>
-            <td py:content="user.user_name">Foo</td>
-          </tr>
-          <tr>
-            <th>EMail</th>
-            <td py:content="user.email_address">Foo</td>
-          </tr>
-          <tr>
-            <th>Full name</th>
-            <td py:content="user.display_name">Foo</td>
-          </tr>
-          <tr>
-            <th>Club</th>
-            <td>
-              <a py:if="user.club" py:content="user.club.name" href="/clubs/${user.club.id}/">Foo</a>
-              <span py:if="not user.club" py:strip="True">None</span>
-              <a href="change_club" class="btn btn-mini">Change</a>
-            </td>
-          </tr>
-          <tr>
-            <th>Live Tracking Key</th>
-            <td>
-              <py:if test="user.tracking_key">
-                <span class="label label-info">${user.tracking_key_hex}</span>
-                <a href="/tracking/info" class="btn btn-mini">More Information</a>
-              </py:if>
-              <py:if test="not user.tracking_key">
-                <a href="/settings/tracking_register?came_from=/settings/" class="btn btn-mini">Generate Key</a>
-              </py:if>
-            </td>
-          </tr>
-          <tr py:if="request.identity and 'manage' in request.identity['permissions']">
-            <th>Groups</th>
-            <td>
-              <ul>
-                <li py:for="group in user.groups" py:content="group">Group name</li>
-              </ul>
-            </td>
-          </tr>
-        </table>
-      </div>
-    </div>
-  </body>
-</html>

skylines/templates/tracking/info.html
@@ -40,7 +40,7 @@ title = 'Live Tracking'
     </p>
 
     <p>
-      <a href="/settings/tracking_register" class="btn btn-mini">Generate new key</a>
+      <a href="/users/${user.user_id}/tracking_register" class="btn btn-mini">Generate new key</a>
       <a href="/tracking/${user.user_id}" class="btn btn-mini">Your Live Tracks</a>
     </p>
     </py:if>

skylines/templates/tracking/list.html
@@ -18,11 +18,6 @@ title = 'Live Tracking'
   </head>
 
   <body>
-    <div class="alert alert-info" py:if="request.identity and request.identity['user'].tracking_key is None">
-      To register your account for Live Tracking, go to your <a
-      href="/settings/">account Settings</a>.
-    </div>
-
     <table id="flight-table" class="table table-striped">
       <thead>
         <tr>

commit cc3804db81d0882ef90e7514804f634d9f8b6f0f

root: Reintroduce /settings/ page as redirection
This fixes a regression in commit 59dbc5c0f234c24434b8a3d2aede379cca394b4f

[mk: not a regression, just dead external links]

skylines/controllers/root.py
@@ -2,8 +2,9 @@
 """Main Controller"""
 
 from datetime import datetime
-from tg import expose, flash, url, lurl, request, redirect
+from tg import expose, flash, url, lurl, request, redirect, require
 from tg.i18n import ugettext as _, lazy_ugettext as l_
+from repoze.what.predicates import Any, not_anonymous
 from skylines import model
 from skylines.model import DBSession
 from tgext.admin.tgadminconfig import TGAdminConfig
@@ -107,3 +108,8 @@ class RootController(BaseController):
 
         flash(_('You are now logged out. We hope to see you back soon!'))
         redirect(came_from)
+
+    @expose()
+    @require(Any(not_anonymous(), msg='Please login to see this page!'))
+    def settings(self):
+        redirect('/users/' + str(request.identity['user'].user_id))