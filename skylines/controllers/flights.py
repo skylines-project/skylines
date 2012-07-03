7e496606d4fdbf1cc3296f1ffd4e42e07478d819
Tobias Lohner <tobias@lohner-net.de>
2012-07-03 16:26:25 +0200

    controllers.flights: prefill change_aircraft form with guesses

--- skylines/controllers/flights.py
+++ skylines/controllers/flights.py
@@ -19,7 +19,7 @@ from skylines import files
 from skylines.model import DBSession, User, Club, Flight, IGCFile, Model, Airport
 from skylines.controllers.upload import UploadController
 from skylines.lib.datatables import GetDatatableRecords
-from skylines.lib.igc import read_igc_header
+from skylines.lib.igc import read_igc_header, guess_model, guess_registration
 from skylines.lib.analysis import analyse_flight, flight_path
 from skylines.form import BootstrapForm
 from skylinespolyencode import SkyLinesPolyEncoder
@@ -147,11 +147,24 @@ class FlightController(BaseController):
         if not self.flight.is_writable():
             raise HTTPForbidden
 
+        if self.flight.model_id is None:
+            model_id = guess_model(self.flight.igc_file)
+        else:
+            model_id = self.flight.model_id
+
+        if self.flight.registration is None:
+            if self.flight.igc_file.registration is not None:
+                registration = self.flight.igc_file.registration
+            else:
+                registration = guess_registration(self.flight.igc_file)
+        else:
+            registration = self.flight.registration
+
         return dict(page='settings', title=_('Change Aircraft'),
                     user=request.identity['user'],
                     form=select_aircraft_form,
-                    values=dict(model=self.flight.model_id,
-                                registration=self.flight.registration))
+                    values=dict(model=model_id,
+                                registration=registration))
 
     @expose()
     @validate(form=select_aircraft_form, error_handler=change_aircraft)
