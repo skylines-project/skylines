37442b0740aa14b88cdff7ce53a345ed0d4d491d
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2012-07-06 22:12:14 +0200

    templates: Moved imports into lib/helpers.py
    The exported symbols from helpers.py are available as "h" in the templates
    through tg2 magic

--- skylines/lib/helpers.py
+++ skylines/lib/helpers.py
@@ -3,3 +3,18 @@
 """WebHelpers used in SkyLines."""
 
 from webhelpers import date, feedgenerator, html, number, misc, text
+import simplejson as json
+from tg.flash import flash
+from datetime import timedelta
+
+from babel.dates import format_date, format_time, format_datetime
+from babel.numbers import format_number
+
+
+def format_timedelta(delta):
+    if isinstance(delta, timedelta):
+        seconds = delta.total_seconds()
+    else:
+        seconds = delta
+
+    return '%d:%02d' % (seconds / 3600, seconds % 3600 / 60)
