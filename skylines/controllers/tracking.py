72428649fa37941618b8db07eec35044f1419870
Tobias Lohner <tobias@lohner-net.de>
2012-07-08 00:26:06 +0200

    controllers.tracking: move get_flight_path[2] to a global function
    To be reused.

--- skylines/controllers/tracking.py
+++ skylines/controllers/tracking.py
@@ -10,72 +10,76 @@ from skylines.lib.base import BaseController
 from skylines.model import DBSession, User, TrackingFix
 from skylinespolyencode import SkyLinesPolyEncoder
 
+def get_flight_path2(pilot, last_update = None):
+    query = DBSession.query(TrackingFix)
+    query = query.filter(TrackingFix.pilot == pilot)
+    query = query.filter(TrackingFix.time >= datetime.now() - timedelta(hours=12))
+    query = query.order_by(TrackingFix.time)
+
+    start_fix = query.first()
+
+    if not start_fix:
+        return None
+
+    start_time = start_fix.time.hour * 3600 + start_fix.time.minute * 60 + start_fix.time.second
+
+    if last_update:
+        query = query.filter(TrackingFix.time >= \
+            start_fix.time + timedelta(seconds=(last_update - start_time)))
+
+    result = []
+    for fix in query:
+        location = fix.location
+        if location is None:
+            continue
+
+        time_delta = fix.time - start_fix.time
+        time = start_time + time_delta.days * 86400 + time_delta.seconds
+
+        result.append((time, location.latitude, location.longitude,
+                       fix.altitude))
+    return result
+
+def get_flight_path(pilot, threshold = 0.001, last_update = None):
+    fp = get_flight_path2(pilot, last_update = last_update)
+    if fp is None or len(fp) == 0:
+        raise HTTPNotFound
+
+    num_levels = 4
+    zoom_factor = 4
+    zoom_levels = [0]
+    zoom_levels.extend([round(-log(32.0 / 45.0 * (threshold * pow(zoom_factor, num_levels - i - 1)), 2)) for i in range(1, num_levels)])
+
+    encoder = SkyLinesPolyEncoder(num_levels=4, threshold=threshold, zoom_factor=4)
+    fixes = dict()
+
+    if len(fp) == 1:
+        fixes['points'] = [(fp[0][0], fp[0][1])]
+        fixes['levels'] = [0]
+        fixes['numLevels'] = num_levels
+
+    else:
+        max_delta_time = max(4, (fp[-1][0] - fp[0][0]) / 500)
+
+        fixes = map(lambda x: (x[2], x[1], (x[0] / max_delta_time * threshold)), fp)
+        fixes = encoder.classify(fixes, remove=False, type="ppd")
+
+    encoded = encoder.encode(fixes['points'], fixes['levels'])
+
+    barogram_t = encoder.encodeList([fp[i][0] for i in range(len(fp)) if fixes['levels'][i] != -1])
+    barogram_h = encoder.encodeList([fp[i][3] for i in range(len(fp)) if fixes['levels'][i] != -1])
+
+    return dict(encoded=encoded, zoom_levels = zoom_levels, fixes = fixes,
+                barogram_t=barogram_t, barogram_h=barogram_h)
+
+
 
 class TrackController(BaseController):
     def __init__(self, pilot):
         self.pilot = pilot
 
-    def __get_flight_path2(self, last_update = None):
-        query = DBSession.query(TrackingFix)
-        query = query.filter(TrackingFix.pilot == self.pilot)
-        query = query.filter(TrackingFix.time >= datetime.now() - timedelta(hours=12))
-        query = query.order_by(TrackingFix.time)
-
-        start_fix = query.first()
-
-        if not start_fix:
-            return None
-
-        start_time = start_fix.time.hour * 3600 + start_fix.time.minute * 60 + start_fix.time.second
-
-        if last_update:
-            query = query.filter(TrackingFix.time >= \
-                start_fix.time + timedelta(seconds=(last_update - start_time)))
-
-        result = []
-        for fix in query:
-            location = fix.location
-            if location is None:
-                continue
-
-            time_delta = fix.time - start_fix.time
-            time = start_time + time_delta.days * 86400 + time_delta.seconds
-
-            result.append((time, location.latitude, location.longitude,
-                           fix.altitude))
-        return result
-
-    def __get_flight_path(self, threshold = 0.001, last_update = None):
-        fp = self.__get_flight_path2(last_update = last_update)
-        if fp is None or len(fp) == 0:
-            raise HTTPNotFound
-
-        num_levels = 4
-        zoom_factor = 4
-        zoom_levels = [0]
-        zoom_levels.extend([round(-log(32.0 / 45.0 * (threshold * pow(zoom_factor, num_levels - i - 1)), 2)) for i in range(1, num_levels)])
-
-        encoder = SkyLinesPolyEncoder(num_levels=4, threshold=threshold, zoom_factor=4)
-        fixes = dict()
-
-        if len(fp) == 1:
-            fixes['points'] = [(fp[0][0], fp[0][1])]
-            fixes['levels'] = [0]
-            fixes['numLevels'] = num_levels
-
-        else:
-            max_delta_time = max(4, (fp[-1][0] - fp[0][0]) / 500)
-
-            fixes = map(lambda x: (x[2], x[1], (x[0] / max_delta_time * threshold)), fp)
-            fixes = encoder.classify(fixes, remove=False, type="ppd")
-
-        encoded = encoder.encode(fixes['points'], fixes['levels'])
-
-        barogram_t = encoder.encodeList([fp[i][0] for i in range(len(fp)) if fixes['levels'][i] != -1])
-        barogram_h = encoder.encodeList([fp[i][3] for i in range(len(fp)) if fixes['levels'][i] != -1])
-
-        return dict(encoded=encoded, zoom_levels = zoom_levels, fixes = fixes,
-                    barogram_t=barogram_t, barogram_h=barogram_h)
+    def __get_flight_path(self, **kw):
+        return get_flight_path(self.pilot, **kw)
 
     @expose('skylines.templates.tracking.view')
     def index(self):
