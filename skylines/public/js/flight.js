d77d8703e92723e893e02edf0aac9024ccdc5766
Tobias Lohner <tobias@lohner-net.de>
2012-07-15 15:24:27 +0200

    public.js.flight: make flight primary if added from json

--- skylines/public/js/flight.js
+++ skylines/public/js/flight.js
@@ -225,12 +225,13 @@ function addFlightFromJSON(url) {
         if (flights[flight_id].sfid == data.sfid) return;
       }
 
-      addFlight(data.sfid, data.encoded.points, data.encoded.levels,
-                data.num_levels, data.barogram_t, data.barogram_h,
-                data.enl, data.zoom_levels, data.contests);
+      var flight_id = addFlight(data.sfid, data.encoded.points, data.encoded.levels,
+                                data.num_levels, data.barogram_t, data.barogram_h,
+                                data.enl, data.zoom_levels, data.contests);
 
       initRedrawLayer(map.getLayersByName("Flight")[0]);
       $.proxy(updateBarogram, { reset_y_axis: true })();
+      setPrimaryFlight(flight_id);
     }
   });
 };
