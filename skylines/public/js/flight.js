6ae985e0bf8bc56e755dde9d5d21e415d60a6695
Tobias Lohner <tobias@lohner-net.de>
2012-07-15 16:36:50 +0200

    public.js.flight: show contest traces for primary flight only

--- skylines/public/js/flight.js
+++ skylines/public/js/flight.js
@@ -445,6 +445,19 @@ function setPrimaryFlight(primary) {
   if (primary_flight != primary) {
     // update barogram and set primary_flight if it changed
     barogram.linechart.setPrimary(primary);
+
+    var old_contest_traces = map.getLayersByName("Flight")[0].getFeaturesByAttribute("sfid", flights[primary_flight].sfid);
+    var new_contest_traces = map.getLayersByName("Flight")[0].getFeaturesByAttribute("sfid", flights[primary].sfid);
+
+    for (var i = 0; i < old_contest_traces.length; i++) {
+      old_contest_traces[i].renderIntent = 'hidden';
+      old_contest_traces[i].layer.drawFeature(old_contest_traces[i]);
+    }
+
+    for (var i = 0; i < new_contest_traces.length; i++) {
+      new_contest_traces[i].renderIntent = 'contest';
+      new_contest_traces[i].layer.drawFeature(new_contest_traces[i]);
+    }
     primary_flight = primary;
   }
 }
