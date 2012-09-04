25196f5a5a390dc4d8585dccbd3a17fc9ae55a61
Andrey Lebedev <andrey@lebedev.lt>
2012-09-04 22:15:52 +0300

    controllers.flights: properly format percentage
    Previously, if percentage was equal to zero, blank string showed
    instead.

--- skylines/controllers/flights.py
+++ skylines/controllers/flights.py
@@ -149,8 +149,8 @@ def format_phase(phase):
     """
     is_circling = phase.phase_type == FlightPhase.PT_CIRCLING
     r = dict(start="%s" % format_time(phase.start_time),
-             fraction="%d%%" % phase.fraction if phase.fraction else "",
-             speed=units.format_speed(float(phase.speed)) if phase.speed else "",
+             fraction="%d%%" % phase.fraction if phase.fraction is not None else "",
+             speed=units.format_speed(phase.speed) if phase.speed is not None else "",
              vario=units.format_lift(phase.vario),
              alt_diff=units.format_altitude(phase.alt_diff),
              count=phase.count,
