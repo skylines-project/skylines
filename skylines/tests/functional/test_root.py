f03bd19fce550f6403737b3e4408e28eaf9d32ed
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2012-09-28 14:01:17 +0200

    test_root: Fixed RootController unit test

--- skylines/tests/functional/test_root.py
+++ skylines/tests/functional/test_root.py
@@ -10,7 +10,7 @@ functional tests exercise the whole application and its WSGI stack.
 Please read http://pythonpaste.org/webtest/ for more information.
 
 """
-from nose.tools import assert_true
+from nose.tools import eq_, assert_true
 
 from skylines.tests import TestController
 
@@ -19,17 +19,8 @@ class TestRootController(TestController):
     """Tests for the method in the root controller."""
 
     def test_index(self):
-        """The front page is working properly"""
+        """The front page is redirecting properly"""
         response = self.app.get('/')
-        msg = 'TurboGears 2 is rapid web application development toolkit '\
-              'designed to make your life easier.'
-        # You can look for specific strings:
-        assert_true(msg in response)
 
-        # You can also access a BeautifulSoup'ed response in your tests
-        # (First run $ easy_install BeautifulSoup
-        # and then uncomment the next two lines)
-
-        #links = response.html.findAll('a')
-        #print links
-        #assert_true(links, "Mummy, there are no links here!")
+        eq_(response.status_int, 302)
+        assert_true(response.location.endswith('/flights/today'))
