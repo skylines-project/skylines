cf1e250c05210cf0b3e9fd52d23250331b42750a
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2012-09-28 15:44:36 +0200

    urlize: Fixed format

--- skylines/lib/urlize.py
+++ skylines/lib/urlize.py
@@ -46,26 +46,28 @@ URLIZE_RE = '(%s)' % '|'.join([
     r'\b[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b',
 ])
 
+
 class UrlizePattern(markdown.inlinepatterns.Pattern):
     """ Return a link Element given an autolink (`http://example/com`). """
     def handleMatch(self, m):
         url = m.group(2)
-        
+
         if url.startswith('<'):
             url = url[1:-1]
-            
+
         text = url
-        
-        if not url.split('://')[0] in ('http','https','ftp'):
+
+        if not url.split('://')[0] in ('http', 'https', 'ftp'):
             if '@' in url and not '/' in url:
                 url = 'mailto:' + url
             else:
                 url = 'http://' + url
-    
+
         el = markdown.util.etree.Element("a")
         el.set('href', url)
         el.text = markdown.util.AtomicString(text)
         return el
+
 
 class UrlizeExtension(markdown.Extension):
     """ Urlize Extension for Python-Markdown. """
@@ -73,6 +75,7 @@ class UrlizeExtension(markdown.Extension):
     def extendMarkdown(self, md, md_globals):
         """ Replace autolink with UrlizePattern """
         md.inlinePatterns['autolink'] = UrlizePattern(URLIZE_RE, md)
+
 
 def makeExtension(configs=None):
     return UrlizeExtension(configs=configs)
