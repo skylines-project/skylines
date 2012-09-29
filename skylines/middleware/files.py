5d312dc5044703237b40600a8720cd019ad851ce
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2012-09-29 16:07:47 +0200

    middleware/files: Removed unused parameter conf

--- skylines/middleware/files.py
+++ skylines/middleware/files.py
@@ -8,9 +8,8 @@ from skylines.middleware import PrefixHandlerMiddleware
 
 
 class FilesMiddleware(PrefixHandlerMiddleware):
-    def __init__(self, conf, mount_point = '/files'):
+    def __init__(self, mount_point = '/files'):
         PrefixHandlerMiddleware.__init__(self, mount_point)
-        self.conf = conf
 
     def handle(self, path, environ, start_response):
         try:
