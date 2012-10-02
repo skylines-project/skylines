ce077af45755ea18f01cf942229e4863bfa13b8d
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2012-10-02 20:50:58 +0200

    ranking: Merged duplicate code

--- skylines/controllers/ranking.py
+++ skylines/controllers/ranking.py
@@ -4,50 +4,41 @@ from sqlalchemy import func
 from skylines.lib.base import BaseController
 from skylines.model import DBSession, User, Club, Flight, Airport
 
+__all__ = ['RankingController']
+
 
 class RankingController(BaseController):
     @expose()
     def index(self, **kw):
         redirect('/ranking/clubs')
 
-    @expose('skylines.templates.ranking.pilots')
-    def pilots(self, **kw):
-        subq = DBSession.query(Flight.pilot_id,
+    @staticmethod
+    def __get_result(model, flight_field):
+        subq = DBSession.query(getattr(Flight, flight_field),
                                func.count('*').label('count'),
                                func.sum(Flight.olc_plus_score).label('total')) \
-               .group_by(Flight.pilot_id).subquery()
-        result = DBSession.query(User, subq.c.count, subq.c.total,
+               .group_by(getattr(Flight, flight_field)).subquery()
+
+        result = DBSession.query(model, subq.c.count, subq.c.total,
                                  over(func.rank(),
                                       order_by=desc('total')).label('rank')) \
-                 .join((subq, subq.c.pilot_id == User.id))
+                 .join((subq, getattr(subq.c, flight_field) == model.id))
+
         result = result.order_by(desc('total'))
         result = result.limit(20)
-        return dict(tab='pilots', result=result)
+        return result
+
+    @expose('skylines.templates.ranking.pilots')
+    def pilots(self, **kw):
+        return dict(tab='pilots',
+                    result=self.__get_result(User, 'pilot_id'))
 
     @expose('skylines.templates.ranking.clubs')
     def clubs(self, **kw):
-        subq = DBSession.query(Flight.club_id,
-                               func.count('*').label('count'),
-                               func.sum(Flight.olc_plus_score).label('total')) \
-               .group_by(Flight.club_id).subquery()
-        result = DBSession.query(Club, subq.c.count, subq.c.total,
-                                 over(func.rank(),
-                                      order_by=desc('total')).label('rank')) \
-                 .join((subq, subq.c.club_id == Club.id))
-        result = result.order_by(desc('total'))
-        result = result.limit(20)
-        return dict(tab='clubs', result=result)
+        return dict(tab='clubs',
+                    result=self.__get_result(Club, 'club_id'))
 
     @expose('skylines.templates.ranking.airports')
     def airports(self, **kw):
-        subq = DBSession.query(Flight.takeoff_airport_id,
-                               func.count('*').label('count'),
-                               func.sum(Flight.olc_plus_score).label('total')) \
-               .group_by(Flight.takeoff_airport_id).subquery()
-        result = DBSession.query(Airport, subq.c.count, subq.c.total,
-                                 over(func.rank(),
-                                      order_by=desc('total')).label('rank')) \
-                 .join((subq, subq.c.takeoff_airport_id == Airport.id))
-        result = result.order_by(desc('total'))
-        result = result.limit(20)
-        return dict(tab='airports', result=result)
+        return dict(tab='airports',
+                    result=self.__get_result(Airport, 'takeoff_airport_id'))
