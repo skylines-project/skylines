1498b612f77aa3a6f83d4eb6f44b2738afd921f6
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2012-11-11 20:52:10 +0100

    helpers/country: Added basic language_to_country_code() function
    This is used to show the corresponding flags for the available languages
    and should be extended on demand.

--- skylines/lib/helpers/country.py
+++ skylines/lib/helpers/country.py
@@ -1,9 +1,16 @@
 from __future__ import absolute_import
 from pycountry import countries
 
-__all__ = ['country_name']
+__all__ = ['country_name', 'language_to_country_code']
 
+__conversion_dict = {'en': 'gb',
+                     'ja': 'jp'}
 
 def country_name(code):
     return countries.get(alpha2=code.upper()).name
 
+def language_to_country_code(language):
+    if language in __conversion_dict:
+        return __conversion_dict[language]
+    else:
+        return language
