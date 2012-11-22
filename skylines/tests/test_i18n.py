4495a36181ca38d66509f3d493aa2cd98542e5f4
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2012-11-22 06:32:32 +0100

    test_i18n: Generate only one unit test per .po file

--- skylines/tests/test_i18n.py
+++ skylines/tests/test_i18n.py
@@ -9,22 +9,19 @@ re_python_old = re.compile(r'%(?:\((\w+)\))?(\w+)')
 
 def test_pofiles():
     for filename in glob.glob(os.path.join('skylines','i18n','*','LC_MESSAGES','skylines.po')):
-        po = polib.pofile(filename)
-        for entry in po.translated_entries():
-            if not entry.obsolete and not entry.msgstr == '':
-                yield check_placeholders, filename, entry.msgid, entry.msgstr
+        yield check_pofile, filename
+
+def check_pofile(filename):
+    po = polib.pofile(filename)
+    for entry in po.translated_entries():
+        if not entry.obsolete and not entry.msgstr == '':
+            check_placeholders(filename, entry.msgid, entry.msgstr)
 
 def check_placeholders(filename, msgid, msgstr):
-    check_python_new(msgid, msgstr)
-    check_python_old(msgid, msgstr)
+    check_python(filename, msgid, msgstr, re_python_new)
+    check_python(filename, msgid, msgstr, re_python_old)
 
-def check_python_new(msgid, msgstr):
-    check_python(msgid, msgstr, re_python_new)
-
-def check_python_old(msgid, msgstr):
-    check_python(msgid, msgstr, re_python_old)
-
-def check_python(msgid, msgstr, re):
+def check_python(filename, msgid, msgstr, re):
     matches_orig = re.findall(msgid)
     if len(matches_orig) == 0:
         return
@@ -40,4 +37,7 @@ def check_python(msgid, msgstr, re):
         counter_trans[match] += 1
 
     assert counter_trans == counter_orig, \
-           'Python string format placeholders are not matching up!'
+           (u'Python string format placeholders are not matching up!\n\n'
+             'File: {}\n\n'
+             'Original: {}\n'
+             'Translation: {}'.format(filename, msgid, msgstr))
