ed0cccc9049b3e081549338ca02d86d9a7b1119a
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2012-11-23 20:49:08 +0100

    test_root: Used built-in assert instead of nose.tools
    There is no point in using nose.tools for those easy assertions

--- skylines/tests/functional/test_root.py
+++ skylines/tests/functional/test_root.py
@@ -1,17 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Functional test suite for the root controller.
-
-This is an example of how functional tests can be written for controllers.
-
-As opposed to a unit-test, which test a small unit of functionality,
-functional tests exercise the whole application and its WSGI stack.
-
-Please read http://pythonpaste.org/webtest/ for more information.
-
 """
-from nose.tools import eq_, assert_true
-
 from skylines.tests import TestController
 
 
@@ -22,5 +12,5 @@ class TestRootController(TestController):
         """The front page is redirecting properly"""
         response = self.app.get('/')
 
-        eq_(response.status_int, 302)
-        assert_true(response.location.endswith('/flights/today'))
+        assert response.status_int == 302
+        assert response.location.endswith('/flights/today')
