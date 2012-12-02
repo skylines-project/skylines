cb358c5c8bdff2584d48dbe2f4c5248b4d1bac4f
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2012-12-02 11:40:29 +0100

    formatter/flight: Merged import statements

--- skylines/lib/formatter/flight.py
+++ skylines/lib/formatter/flight.py
@@ -1,6 +1,5 @@
 from tg.i18n import ugettext as _
-from skylines.lib.formatter import format_date
-from skylines.lib.formatter.units import format_distance
+from skylines.lib.formatter import format_date, format_distance
 
 __all__ = ['format_flight_title']
 
