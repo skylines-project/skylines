c6664c2edd2d3177ea578f57e94084fd3aef92d8
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2012-12-02 11:20:41 +0100

    lib/helpers: Moved format_flight_title into new formatter.flight module

--- skylines/lib/formatter/__init__.py
+++ skylines/lib/formatter/__init__.py
@@ -1,3 +1,4 @@
 from babel.numbers import format_number, format_decimal
 
 from skylines.lib.formatter.datetime import *
+from skylines.lib.formatter.flight import *
--- skylines/lib/helpers/__init__.py
+++ skylines/lib/helpers/__init__.py
@@ -5,7 +5,6 @@
 from webhelpers import date, feedgenerator, html, number, misc, text
 import simplejson as json
 from tg import flash
-from tg.i18n import ugettext as _
 
 from skylines.lib.formatter import *
 from skylines.lib.helpers.includeguard import not_included_yet
@@ -13,23 +12,3 @@ from skylines.lib.helpers.markdown import markdown
 from skylines.lib.helpers.string import *
 from skylines.lib.helpers.country import *
 from skylines.lib.units import format_distance, get_setting_name
-
-
-def format_flight_title(flight):
-    title = _('{distance} on {date}').format(distance=format_distance(flight.olc_classic_distance),
-                                             date=format_date(flight.date_local))
-
-    if flight.pilot is None:
-        return title, ''
-
-    if flight.co_pilot is None:
-        tagline = (_('{something} by {pilot_name}').
-                   format(something='',
-                          pilot_name=unicode(flight.pilot)))
-    else:
-        tagline = (_('{something} by {pilot_name} and {co_pilot_name}').
-                   format(something='',
-                          pilot_name=unicode(flight.pilot),
-                          co_pilot_name=unicode(flight.co_pilot)))
-
-    return title, tagline
