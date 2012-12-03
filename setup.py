d7866d7388f11aa480a823f05834f5036fe6d65e
Max Kellermann <max@duempel.org>
2012-12-03 23:00:04 +0100

    setup.py: remove debug print

--- setup.py
+++ setup.py
@@ -55,8 +55,6 @@ if sys.version_info[:2] == (2, 4):
     testpkgs.extend(['hashlib', 'pysqlite'])
     install_requires.extend(['hashlib', 'pysqlite'])
 
-print install_requires
-
 setup(
     name='SkyLines',
     version='0.1',
