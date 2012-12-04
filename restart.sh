1380f9c00b3f8cfed4f7a8a851869db48c012779
Max Kellermann <max@duempel.org>
2012-12-04 10:48:57 +0100

    restart.sh: new utility script for the production server

---
+++ restart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Prepare the SkyLines application and restart all daemons.  This is
+# meant to be run on the production server.
+#
+
+set -e
+
+cd `dirname $0`
+python setup.py egg_info
+python setup.py compile_catalog
+sv restart skylines-fastcgi
+sv restart mapserver-fastcgi
+sv restart skylines-daemon
