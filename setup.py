93394f1f9018e90e5d0d32e476eb005221c74678
Bruno Bonamin <bruno@bonamin.org>
2013-01-16 17:02:46 -0300

    Specify SQLAlchemy version to 0.7.9, otherwise errors occur in the new user form

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ install_requires = [
     "Genshi",
     "zope.sqlalchemy >= 0.4",
     "repoze.tm2 >= 1.0a5",
-    "sqlalchemy",
+    "sqlalchemy == 0.7.9",
     "repoze.what >= 1.0.8",
     "repoze.who-friendlyform >= 1.0.4",
     "repoze.what-pylons >= 1.0",
