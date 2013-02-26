13dab7ba6013404e6bb7b6abe0a45184a58d09fb
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-02-26 22:57:24 +0100

    config: Enabled do expression extension for Jinja2
    Useful for building lists with <list>.append()

--- skylines/config/app_cfg.py
+++ skylines/config/app_cfg.py
@@ -34,7 +34,7 @@ base_config.default_renderer = 'genshi'
 base_config.renderers.append('genshi')
 
 base_config.renderers.append('jinja')
-base_config.jinja_extensions = ['jinja2.ext.i18n', 'jinja2.ext.with_']
+base_config.jinja_extensions = ['jinja2.ext.i18n', 'jinja2.ext.with_', 'jinja2.ext.do']
 
 def install_gettext_callables(app):
     from tg.i18n import ugettext, ungettext
