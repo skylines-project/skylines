33bae69265d047bc6bcd69ad70c02d83ed3c0098
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-02-26 22:56:24 +0100

    lib/helpers: Added datetime module to helpers
    That enables us to use h.datetime in the templates

--- skylines/lib/helpers/__init__.py
+++ skylines/lib/helpers/__init__.py
@@ -2,6 +2,8 @@
 
 """WebHelpers used in SkyLines."""
 
+from __future__ import absolute_import
+
 from webhelpers import date, feedgenerator, html, number, misc, text
 import simplejson as json
 from tg import flash
@@ -11,3 +13,5 @@ from skylines.lib.markdown import markdown
 from skylines.lib.helpers.includeguard import not_included_yet
 from skylines.lib.helpers.string import *
 from skylines.lib.helpers.country import *
+
+import datetime
