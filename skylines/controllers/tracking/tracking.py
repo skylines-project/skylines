46863eaa5f20f953a1ab4b683052527f131dbaaa
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-03-01 19:09:58 +0100

    templates/tracking: Converted tracking list to Jinja2

--- skylines/controllers/tracking/tracking.py
+++ skylines/controllers/tracking/tracking.py
@@ -19,7 +19,7 @@ class TrackingController(BaseController):
     lt24 = LiveTrack24Controller()
     external = ExternalTrackingController()
 
-    @expose('tracking/list.html')
+    @expose('jinja:tracking/list.jinja')
     def index(self, **kw):
         na_cache = cache.get_cache('tracking.nearest_airport', expire=60 * 60)
 
