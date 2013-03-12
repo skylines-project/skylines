29f7dc275fe335145dbfa3850b68b55c6f7122df
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-03-12 19:37:26 +0100

    tests: Added gjslint test
    Our own JS files are run through gjslint, which needs to be installed
    separately, since it is not a python dependency.
    
    https://developers.google.com/closure/utilities/docs/linter_howto

---
+++ skylines/tests/test_gjslint.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import nose
+from subprocess import CalledProcessError, check_output as run
+from functools import partial
+
+GJSLINT_COMMAND = 'gjslint'
+
+GJSLINT_OPTIONS = ['--nojsdoc']
+
+JS_BASE_FOLDER = os.path.join('skylines', 'public', 'js')
+
+JS_FILES = [
+    'flight.js',
+    'general.js',
+    'map.js',
+    'topbar.js',
+    'tracking.js',
+    'units.js',
+]
+
+
+def get_language_code(filename):
+    filename = os.path.split(filename)[0]
+    filename = os.path.split(filename)[0]
+    filename = os.path.split(filename)[1]
+    return filename
+
+
+def test_js_files():
+    for filename in JS_FILES:
+        f = partial(run_gjslint, filename)
+        f.description = 'gjslint {}'.format(filename)
+        yield f
+
+
+def run_gjslint(filename):
+    path = os.path.join(JS_BASE_FOLDER, filename)
+
+    args = [GJSLINT_COMMAND]
+    args.extend(GJSLINT_OPTIONS)
+    args.append(path)
+
+    try:
+        run(args)
+    except CalledProcessError, e:
+        print e.output
+        raise AssertionError('gjslint has found errors.')
+    except OSError:
+        raise OSError('Failed to run gjslint. Please check that you have '
+                      'installed it properly.')
+
+
+if __name__ == "__main__":
+    sys.argv.append(__name__)
+    nose.run()
