19dc14e1771a14cc27cd6a3e0cb4547bb9ee6b04
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-03-22 21:02:20 +0100

    test_pep8: Added PEP8_IGNORE_ERRORS array

--- skylines/tests/test_pep8.py
+++ skylines/tests/test_pep8.py
@@ -4,13 +4,28 @@ from subprocess import CalledProcessError, check_output as run
 
 PEP8_COMMAND = 'pep8'
 
-PEP8_OPTIONS = ['--ignore=E501,E701,E126,E711']
+PEP8_OPTIONS = []
+
+PEP8_IGNORE_ERRORS = [
+    'E501',
+    'E701',
+    'E126',
+    'E711',
+]
 
 
 def test_pep8():
     """ Run skylines package through pep8 """
     args = [PEP8_COMMAND]
+
+    # Append custom options
     args.extend(PEP8_OPTIONS)
+
+    # Append ignored errors
+    if len(PEP8_IGNORE_ERRORS) != 0:
+        args.append('--ignore=' + ','.join(PEP8_IGNORE_ERRORS))
+
+    # Append package name that should be checked
     args.append('skylines')
 
     try:
