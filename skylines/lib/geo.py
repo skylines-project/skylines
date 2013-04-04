ffe1e7e64b2d2b79f463b8a1f8f0d06797811a88
Tobias Lohner <tobias@lohner-net.de>
2013-04-04 09:32:38 +0200

    model.geo: add geographic_distance method

---
+++ skylines/lib/geo.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import math
+
+EARTH_RADIUS = 6367009
+
+
+def geographic_distance(loc1, loc2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    lat1, lon1, lat2, lon2 = map(math.radians,
+                                 [loc1.latitude, loc1.longitude,
+                                  loc2.latitude, loc2.longitude])
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = math.sin(dlat / 2) ** 2 + math.cos(lat1) * \
+        math.cos(lat2) * math.sin(dlon / 2) ** 2
+    c = 2 * math.asin(math.sqrt(a))
+
+    return EARTH_RADIUS * c
