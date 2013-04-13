255e5ce79b51eb52ce2897703c14c5e3c52ee0d2
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-04-13 03:13:52 +0200

    model/geo: Removed unused from_wkt() method

--- skylines/model/geo.py
+++ skylines/model/geo.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
-import re
 from sqlalchemy import func
 from sqlalchemy.sql.expression import cast
 from geoalchemy2.types import Geometry, Geography
 from geoalchemy2.shape import to_shape
 from skylines.model.session import DBSession
 from skylines.lib.geo import geographic_distance
-
-wkt_re = re.compile(r'POINT\(([\+\-\d.]+) ([\+\-\d.]+)\)')
 
 
 class Location(object):
@@ -17,15 +14,6 @@ class Location(object):
 
     def to_wkt(self):
         return 'POINT({0} {1})'.format(self.longitude, self.latitude)
-
-    @staticmethod
-    def from_wkt(wkt):
-        match = wkt_re.match(wkt)
-        if not match:
-            return None
-
-        return Location(latitude=float(match.group(2)),
-                        longitude=float(match.group(1)))
 
     @staticmethod
     def from_wkb(wkb):
