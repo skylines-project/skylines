6fa13c9e788253051a343b7dec6e6b3dce37ea13
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-04-16 00:45:14 +0200

    model/elevation: Added get() classmethod

--- skylines/model/elevation.py
+++ skylines/model/elevation.py
@@ -1,7 +1,9 @@
 from sqlalchemy import Column
 from sqlalchemy.types import Integer
 from geoalchemy2.types import Raster
+
 from skylines.model.base import DeclarativeBase
+from .session import DBSession
 
 
 class Elevation(DeclarativeBase):
@@ -9,3 +11,19 @@ class Elevation(DeclarativeBase):
 
     rid = Column(Integer, autoincrement=True, primary_key=True)
     rast = Column(Raster)
+
+    @classmethod
+    def get(cls, location):
+        """
+        Returns the elevation at the given location or None.
+
+        location should be WKBElement or WKTElement.
+        """
+
+        elevation = cls.rast.ST_Value(location)
+
+        query = DBSession.query(elevation.label('elevation')) \
+            .filter(location.ST_Intersects(cls.rast)) \
+            .filter(elevation != None)
+
+        return query.scalar()
