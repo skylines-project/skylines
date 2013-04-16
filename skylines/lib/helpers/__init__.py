6ce17b77b11c7914fa49d7f8e3a8872e3db74400
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-04-16 18:50:01 +0200

    lib/helpers: Removed obsolete includeguard module

--- skylines/lib/helpers/__init__.py
+++ skylines/lib/helpers/__init__.py
@@ -10,7 +10,6 @@ from tg import flash
 
 from skylines.lib.formatter import *
 from skylines.lib.markdown import markdown
-from skylines.lib.helpers.includeguard import not_included_yet
 from skylines.lib.helpers.string import *
 from skylines.lib.helpers.country import *
 
