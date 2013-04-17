0dba41ef32a075a06803e47c03eb0c7ef92340bd
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-04-17 17:37:38 +0200

    lib/datatables: Removed unused/commented code

--- skylines/lib/datatables.py
+++ skylines/lib/datatables.py
@@ -12,8 +12,6 @@ def GetDatatableRecords(kw, querySet, columnIndexNameMap):
 
     """
 
-    # Get the number of columns
-    #cols = int(kw.get('iColumns', 0))
     # Safety measure.
     iDisplayLength = min(int(kw.get('iDisplayLength', 50)), 100)
     # Where the data starts from (page)
@@ -45,46 +43,12 @@ def GetDatatableRecords(kw, querySet, columnIndexNameMap):
 
         querySet = querySet.order_by(*asortingCols)
 
-#    # Determine which columns are searchable
-#    searchableColumns = []
-#    for col in range(0,cols):
-#        if request.GET.get('bSearchable_{0}'.format(col), False) == 'true': searchableColumns.append(columnIndexNameMap[col])
-#
-#    # Apply filtering by value sent by user
-#    customSearch = request.GET.get('sSearch', '').encode('utf-8');
-#    if customSearch != '':
-#        outputQ = None
-#        first = True
-#        for searchableColumn in searchableColumns:
-#            kwargz = {searchableColumn+"__icontains" : customSearch}
-#            outputQ = outputQ | Q(**kwargz) if outputQ else Q(**kwargz)
-#        querySet = querySet.filter(outputQ)
-#
-#    # Individual column search
-#    outputQ = None
-#    for col in range(0,cols):
-#        if request.GET.get('sSearch_{0}'.format(col), False) > '' and request.GET.get('bSearchable_{0}'.format(col), False) == 'true':
-#            kwargz = {columnIndexNameMap[col]+"__icontains" : request.GET['sSearch_{0}'.format(col)]}
-#            outputQ = outputQ & Q(**kwargz) if outputQ else Q(**kwargz)
-#    if outputQ: querySet = querySet.filter(outputQ)
-
     # count how many records match the final criteria
     iTotalRecords = iTotalDisplayRecords = querySet.count()
     # get the slice
     querySet = querySet[startRecord:endRecord]
     # required echo response
     sEcho = int(kw.get('sEcho', 0))
-
-#    a = querySet.values()
-#    for row in a:
-#        rowkeys = row.keys()
-#        rowvalues = row.values()
-#        rowlist = []
-#        for col in range(0,len(colitems)):
-#            for idx, val in enumerate(rowkeys):
-#                if val == colitems[col]:
-#                    rowlist.append(str(rowvalues[idx]))
-#        aaData.append(rowlist)
 
     response_dict = {}
     response_dict.update({'sEcho': sEcho, 'iTotalRecords': iTotalRecords,
