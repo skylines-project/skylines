cd1d7c5779150b673ee9ec1894ec48c43ad8640f
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-05-02 22:58:13 +0200

    shell.py: Fall back to development.ini if production config is missing

--- shell.py
+++ shell.py
@@ -13,9 +13,18 @@ from skylines.model import *
 
 sys.path.append(os.path.dirname(sys.argv[0]))
 
-conf_path = '/etc/skylines/production.ini'
+PRO_CONF_PATH = '/etc/skylines/production.ini'
+DEV_CONF_PATH = 'development.ini'
+
 if len(sys.argv) > 1:
     conf_path = sys.argv[1]
+    if not os.path.exists(conf_path):
+        parser.error('Config file "{}" not found.'.format(conf_path))
+elif os.path.exists(PRO_CONF_PATH):
+    conf_path = PRO_CONF_PATH
+else:
+    conf_path = DEV_CONF_PATH
+
 conf = appconfig('config:' + os.path.abspath(conf_path))
 load_environment(conf.global_conf, conf.local_conf)
 
