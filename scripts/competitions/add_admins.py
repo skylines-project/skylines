4247641bae6dc4f2a9d1ca61d09dc9234e604192
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-05-19 19:27:42 +0200

    scripts: Added competitions/add_admins.py

---
+++ scripts/competitions/add_admins.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+#
+# Add new admins to a competition
+#
+
+import argparse
+import sys
+from datetime import datetime
+import transaction
+
+from skylines.config import environment
+from skylines.model.session import DBSession
+from skylines.model import User, Competition
+
+
+# Parse command line parameters
+
+parser = argparse.ArgumentParser(
+    description='Add new admins to a competition.')
+
+parser.add_argument('--config', metavar='config.ini',
+                    help='path to the configuration INI file')
+
+parser.add_argument(
+    'competition_id', type=int, help='id of the competition')
+
+parser.add_argument(metavar='admin-id', dest='admin_ids',
+                    nargs='+', type=int, help='id of the participant')
+
+args = parser.parse_args()
+
+# Load environment
+
+if not environment.load_from_file(args.config):
+    parser.error('Config file "{}" not found.'.format(args.config))
+
+# Load competition
+
+competition = Competition.get(args.competition_id)
+if not competition:
+    sys.exit('No competition found with id: {}.'.format(args.competition_id))
+
+# Build user list
+
+admins = {u.id: u for u in User.query().filter(User.id.in_(args.admin_ids))}
+
+# Add the admins to the competition
+
+for admin_id in args.admin_ids:
+
+    admin = admins.get(admin_id, None)
+    if not admin:
+        print 'No user found with id: {}'.format(admin_id)
+        continue
+
+    competition.admins.append(admin)
+    print 'Added user "{}" (id: {}) to competition with id: {}' \
+        .format(admin.name, admin.id, competition.id)
+
+transaction.commit()
