5729fa429476592869abd8170ceb50858685b2ec
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-05-22 23:57:50 +0200

    flask: Serve /about from flask

--- skylines/views.py
+++ skylines/views.py
@@ -1,3 +1,5 @@
+from flask import render_template
+
 from skylines import app
 from .model import User
 
@@ -8,6 +10,10 @@ def index():
 @app.route('/hello')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/about')
+def about():
+    return render_template('about.jinja')
 
 @app.route('/users/')
 def user_list():
