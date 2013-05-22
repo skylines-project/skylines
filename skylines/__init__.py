e28f0accf06462ba365151de571d3d781e62fa9a
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-05-22 23:42:55 +0200

    flask: Load asset bundles into webassets environment

--- skylines/__init__.py
+++ skylines/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask.ext.babel import Babel
 from flask.ext.assets import Environment
+from webassets.loaders import PythonLoader
 
 
 def create_app():
     app = Flask(__name__, static_folder='public')
     babel = Babel(app)
+
+    bundles = PythonLoader('skylines.assets.bundles').load_bundles()
     assets = Environment(app)
+    assets.register(bundles)
+
     return app
 
 app = create_app()
