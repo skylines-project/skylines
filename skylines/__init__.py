f8108015509997015426adf7c4b6dd2e0330164c
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-05-22 22:55:22 +0200

    flask: Added create_app() function

--- skylines/__init__.py
+++ skylines/__init__.py
@@ -2,8 +2,13 @@ from flask import Flask
 from flask.ext.babel import Babel
 from flask.ext.assets import Environment
 
-app = Flask(__name__, static_folder='public')
-babel = Babel(app)
-assets = Environment(app)
+
+def create_app():
+    app = Flask(__name__, static_folder='public')
+    babel = Babel(app)
+    assets = Environment(app)
+    return app
+
+app = create_app()
 
 import skylines.views
