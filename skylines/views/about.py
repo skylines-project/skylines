53879b21d9a58e322f4d380e2597c728c8eb39de
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-05-24 21:53:06 +0200

    flask/views: Added /about/imprint route

--- skylines/views/about.py
+++ skylines/views/about.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from flask.ext.babel import _
 
 from skylines import app
 
@@ -6,3 +7,13 @@ from skylines import app
 @app.route('/about')
 def about():
     return render_template('about.jinja')
+
+
+@app.route('/about/imprint')
+def imprint():
+    content = app.config.get(
+        'SKYLINES_IMPRINT',
+        'Please set the SKYLINES_IMPRINT variable in the config file.')
+
+    return render_template(
+        'generic/page-FLASK.jinja', title=_('Imprint'), content=content)
