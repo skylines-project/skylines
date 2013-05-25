a4cf1d7f007828afdd40e6ef53417963a4be6c4e
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-05-25 16:26:37 +0200

    flask: Use toscawidget middleware wrapper
    Recipe from http://adampointer.blogspot.de/2011/02/using-flask-and-toscawidgets.html

--- flask_app.py
+++ flask_app.py
@@ -3,6 +3,9 @@
 # Flask app
 from skylines import app as flask_app
 
+# ToscaWidgets
+from tw.api import make_middleware
+
 # TurboGears app
 from skylines.config.middleware import make_app
 from skylines.config.environment import conf_from_file
@@ -13,6 +16,9 @@ from paste.cascade import Cascade
 
 
 if __name__ == '__main__':
+    # Insert ToscaWidgets Middleware
+    flask_app.wsgi_app = make_middleware(flask_app.wsgi_app, stack_registry=True)
+
     # Create TurboGears app
     tg_conf = conf_from_file()
     tg_app = make_app(tg_conf.global_conf, **tg_conf.local_conf)
