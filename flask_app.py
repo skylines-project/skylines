f537256c51fc60f427707f223adcf2144c96dbd3
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-05-28 22:55:54 +0200

    flask: Removed WSGI cascade

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Flask app
-from skylines import app as flask_app
+from skylines import app
 
 # ToscaWidgets
 from tw.api import make_middleware
@@ -11,18 +11,14 @@ from skylines.config import environment as tg_env
 
 # WSGI server
 from werkzeug.serving import run_simple
-from paste.cascade import Cascade
 
 
 if __name__ == '__main__':
     # Insert ToscaWidgets Middleware
-    flask_app.wsgi_app = make_middleware(flask_app.wsgi_app, stack_registry=True)
+    app.wsgi_app = make_middleware(app.wsgi_app, stack_registry=True)
 
     # Create TurboGears app
     tg_env.load_from_file()
-
-    # Create WSGI cascade with tg priority
-    app = Cascade([flask_app])
 
     # Run the WSGI server
     run_simple('localhost', 5000, app,
