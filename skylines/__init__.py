9d578af2e59bf5562e2d7a6ef18e24d3f24043e9
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-05-30 02:11:44 +0200

    flask: Moved extension imports into methods
    There is no need to import these modules if we don't use them

--- skylines/__init__.py
+++ skylines/__init__.py
@@ -1,10 +1,4 @@
-from flask import Flask, g, request
-from flask.ext.babel import Babel
-from skylines.assets import Environment
-from flask.ext.login import LoginManager, current_user
-from flask.ext.cache import Cache
-from flask.ext.sqlalchemy import SQLAlchemy
-from skylines.lib import helpers
+from flask import Flask
 
 
 class SkyLines(Flask):
@@ -29,23 +23,28 @@ class SkyLines(Flask):
 
     def add_sqlalchemy(self):
         # Create and configure SQLAlchemy extension
+        from flask.ext.sqlalchemy import SQLAlchemy
         self.db = SQLAlchemy(self, session_options=dict(expire_on_commit=False))
 
     def add_cache(self):
         # Create and attach Cache extension
+        from flask.ext.cache import Cache
         self.cache = Cache(self)
 
     def add_babel(self):
         # Create and attach Babel extension
+        from flask.ext.babel import Babel
         self.babel = Babel(self)
 
     def add_login_manager(self):
         # Create and attach Login extension
+        from flask.ext.login import LoginManager
         self.login_manager = LoginManager()
         self.login_manager.init_app(self)
 
     def add_assets(self):
         # Create and attach Assets extension
+        from skylines.assets import Environment
         self.assets = Environment(self)
         self.assets.load_bundles('skylines.assets.bundles')
 
@@ -54,6 +53,10 @@ class SkyLines(Flask):
         self.wsgi_app = make_middleware(self.wsgi_app, stack_registry=True)
 
     def add_tg2_compat(self):
+        from flask import request, g
+        from flask.ext.login import current_user
+        from skylines.lib import helpers
+
         @self.before_request
         def inject_request_identity():
             """ for compatibility with tg2 """
