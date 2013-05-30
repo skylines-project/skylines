17f4eaa9f1778a9a22cd1e70aad2f82d908ffa31
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-05-30 02:01:54 +0200

    flask: Simplified WSGI server run call

--- flask_app.py
+++ flask_app.py
@@ -3,11 +3,6 @@
 # Flask app
 from skylines import app
 
-# WSGI server
-from werkzeug.serving import run_simple
-
-
 if __name__ == '__main__':
     # Run the WSGI server
-    run_simple('localhost', 8080, app,
-               use_reloader=True, use_debugger=True, use_evalex=True)
+    app.run(port=8080)
