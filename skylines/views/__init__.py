3b90218100f0bb14f286123a4da938b9d68df8ae
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-05-30 02:34:38 +0200

    views: Added /files route

--- skylines/views/__init__.py
+++ skylines/views/__init__.py
@@ -8,6 +8,7 @@ from skylines.views.api import api_blueprint
 from skylines.views.assets import assets_blueprint
 from skylines.views.club import club_blueprint
 from skylines.views.clubs import clubs_blueprint
+from skylines.views.files import files_blueprint
 from skylines.views.flight import flight_blueprint
 from skylines.views.flights import flights_blueprint
 from skylines.views.livetrack24 import lt24_blueprint
@@ -31,6 +32,7 @@ def register(app):
     app.register_blueprint(assets_blueprint)
     app.register_blueprint(club_blueprint, url_prefix='/clubs/<club_id>')
     app.register_blueprint(clubs_blueprint, url_prefix='/clubs')
+    app.register_blueprint(files_blueprint, url_prefix='/files')
     app.register_blueprint(flight_blueprint, url_prefix='/flights/<flight_id>')
     app.register_blueprint(flights_blueprint, url_prefix='/flights')
     app.register_blueprint(lt24_blueprint)
