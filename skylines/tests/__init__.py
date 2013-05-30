f5b1bacb0152772f7677e781f37a3cde17a15c27
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-05-30 13:32:19 +0200

    tests: Converted __init__.py to flask

--- skylines/tests/__init__.py
+++ skylines/tests/__init__.py
@@ -1,31 +1,22 @@
 # -*- coding: utf-8 -*-
 """Unit and functional test suite for SkyLines."""
 
-from os import path
-
-from tg import config
-from paste.script.appinstall import SetupCommand
-from routes import url_for
-
-from skylines import model
+from skylines import model, db
+from skylines.websetup.bootstrap import bootstrap
 
 __all__ = ['setup_db', 'setup_app', 'teardown_db', 'url_for']
 
 
 def setup_db():
     """Method used to build a database"""
-    engine = config['pylons.app_globals'].sa_engine
-    model.init_model(engine)
-    model.metadata.create_all(engine)
+    model.metadata.create_all(db.engine)
 
 
 def setup_app():
-    test_file = path.join(config.here, 'test.ini')
-    cmd = SetupCommand('setup-app')
-    cmd.run([test_file])
+    setup_db()
+    bootstrap()
 
 
 def teardown_db():
     """Method used to destroy a database"""
-    engine = config['pylons.app_globals'].sa_engine
-    model.metadata.drop_all(engine)
+    model.metadata.drop_all(db.engine)
