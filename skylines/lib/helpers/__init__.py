5fc9d2a274beeb1ef30799bfef9912c4e9f103d4
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-05-30 03:40:49 +0200

    lib/helpers: Simplified url() parameter usage

--- skylines/lib/helpers/__init__.py
+++ skylines/lib/helpers/__init__.py
@@ -18,17 +18,12 @@ from skylines.lib.formatter.datetime import *
 from skylines.lib.formatter.units import *
 from skylines.lib.markdown import markdown
 
-# The dict implementation of Jinja2 only works for keyword parameters,
-# but not for merging to dictionaries. We export the builtin Python dict()
-# function here to get around that problem for building URLs.
-dict = dict
-
 # Jinja2 doesn't seem to have min/max... strange!
 min = min
 max = max
 
 
-def url(base_url='/', params={}):
+def url(base_url='/', **params):
     if not isinstance(base_url, basestring) and hasattr(base_url, '__iter__'):
         base_url = '/'.join(base_url)
 
