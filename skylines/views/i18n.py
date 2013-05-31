a9e4d4781ce365d705e6b1af188c45578f9eda3c
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-05-31 17:45:15 +0200

    i18n: Reimplemented primary and secondary locales

--- skylines/views/i18n.py
+++ skylines/views/i18n.py
@@ -1,29 +1,50 @@
 from flask import redirect, request, session, url_for, g
 from flask.ext.babel import get_locale
 from babel import Locale
+from babel.util import distinct
 from formencode.api import set_stdtranslation
 
 
 def register(app):
-    """"""
+    available_locales = {
+        str(l): l for l in app.babel_instance.list_translations()
+    }
 
-    available_locales = app.babel_instance.list_translations()
-
-    @app.before_request
-    def inject_active_locale():
-        g.available_locales = available_locales
-        g.active_locale = get_locale()
-
-    @app.babel_instance.localeselector
-    def select_locale():
-        available = map(str, available_locales)
+    def _get_preferred_languages():
         preferred = []
 
         session_language = session.get('language', None)
         if session_language:
-            preferred.append(session_language)
+            preferred.append(str(session_language))
 
         preferred.extend([l[0] for l in request.accept_languages])
+        return preferred
+
+    def _get_primary_locales():
+        preferred = _get_preferred_languages() or ['en']
+        primary = []
+
+        for language in preferred:
+            if language in available_locales:
+                primary.append(available_locales[language])
+            elif language[:2] in available_locales:
+                primary.append(available_locales[language[:2]])
+
+        return map(None, distinct(primary))
+
+    @app.before_request
+    def inject_active_locale():
+        g.available_locales = available_locales.values()
+        g.active_locale = get_locale()
+
+        g.primary_locales = _get_primary_locales()
+        g.secondary_locales = \
+            [l for l in g.available_locales if l not in g.primary_locales]
+
+    @app.babel_instance.localeselector
+    def select_locale():
+        available = available_locales.keys()
+        preferred = _get_preferred_languages()
 
         # Find best match
         best_match = Locale.negotiate(preferred, available)
