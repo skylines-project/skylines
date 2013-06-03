196abddb208552b013decee8b0bd61aca7a1d266
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-06-03 22:57:46 +0200

    model/base: Added monkey_patch_model() function

--- skylines/model/base.py
+++ skylines/model/base.py
@@ -2,22 +2,24 @@ from .search import search_query
 from skylines import db
 
 
-def query(cls, **kw):
-    q = db.session.query(cls)
+def monkey_patch_model():
+    def query(cls, **kw):
+        q = db.session.query(cls)
 
-    if kw:
-        q = q.filter_by(**kw)
+        if kw:
+            q = q.filter_by(**kw)
 
-    return q
+        return q
+
+    def get(cls, id):
+        return cls.query().get(id)
+
+    db.Model.flask_query = db.Model.query
+    db.Model.query = classmethod(query)
+    db.Model.get = classmethod(get)
+    db.Model.search_query = classmethod(search_query)
 
 
-def get(cls, id):
-    return cls.query().get(id)
-
-
-db.Model.flask_query = db.Model.query
-db.Model.query = classmethod(query)
-db.Model.get = classmethod(get)
-db.Model.search_query = classmethod(search_query)
+monkey_patch_model()
 
 metadata = db.metadata
