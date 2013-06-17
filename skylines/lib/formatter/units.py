d7e6fba43aa6e1c9e1e59a16b384502b6eaa3db7
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-06-17 21:37:56 +0200

    lib.formatter.units: Simplified unit definitions
    Merged common code

--- skylines/lib/formatter/units.py
+++ skylines/lib/formatter/units.py
@@ -4,40 +4,27 @@ from flask.ext.babel import lazy_gettext as l_
 from .numbers import format_decimal
 
 distance_units = (
-    (u'm', lambda value, p: format_decimal(round(value, p),
-                                           format=u'{0:.{1}f} m'.format(0.0, p)), 0),
-    (u'km', lambda value, p: format_decimal(round(value / 1000, p),
-                                            format=u'{0:.{1}f} km'.format(0.0, p)), 0),
-    (u'NM', lambda value, p: format_decimal(round(value / 1852., p),
-                                            format=u'{0:.{1}f} NM'.format(0.0, p)), 0),
-    (u'mi', lambda value, p: format_decimal(round(value / 1609.34, p),
-                                            format=u'{0:.{1}f} mi'.format(0.0, p)), 0),
+    (u'm', 1, u'{0:.{1}f} m', 0),
+    (u'km', 1 / 1000., u'{0:.{1}f} km', 0),
+    (u'NM', 1 / 1852., u'{0:.{1}f} NM', 0),
+    (u'mi', 1 / 1609.34, u'{0:.{1}f} mi', 0),
 )
 
 speed_units = (
-    (u'm/s', lambda value, p: format_decimal(round(value, p),
-                                             format=u'{0:.{1}f} m/s'.format(0.0, p)), 1),
-    (u'km/h', lambda value, p: format_decimal(round(value * 3.6, p),
-                                              format=u'{0:.{1}f} km/h'.format(0.0, p)), 1),
-    (u'kt', lambda value, p: format_decimal(round(value * 1.94384449, p),
-                                            format=u'{0:.{1}f} kt'.format(0.0, p)), 1),
-    (u'mph', lambda value, p: format_decimal(round(value * 2.23693629, p),
-                                             format=u'{0:.{1}f} mph'.format(0.0, p)), 1),
+    (u'm/s', 1, u'{0:.{1}f} m/s', 1),
+    (u'km/h', 3.6, u'{0:.{1}f} km/h', 1),
+    (u'kt', 1.94384449, u'{0:.{1}f} kt', 1),
+    (u'mph', 2.23693629, u'{0:.{1}f} mph', 1),
 )
 
 lift_units = (
-    (u'm/s', lambda value, p: format_decimal(round(value, p),
-                                             format=u'{0:.{1}f} m/s'.format(0.0, p)), 1),
-    (u'kt', lambda value, p: format_decimal(round(value * 1.94384449, p),
-                                            format=u'{0:.{1}f} kt'.format(0.0, p)), 1),
-    (u'ft/min', lambda value, p: format_decimal(round(value * 196.850394, p),
-                                                format=u'{0:.{1}f} ft/min'.format(0.0, p)), 0),
+    (u'm/s', 1, u'{0:.{1}f} m/s', 1),
+    (u'kt', 1.94384449, u'{0:.{1}f} kt', 1),
+    (u'ft/min', 1 * 196.850394, u'{0:.{1}f} ft/min', 0),
 )
 altitude_units = (
-    (u'm', lambda value, p: format_decimal(round(value, p),
-                                           format=u'{0:.{1}f} m'.format(0.0, p)), 0),
-    (u'ft', lambda value, p: format_decimal(round(value, p),
-                                            format=u'{0:.{1}f} ft'.format(0.0, p)), 0)
+    (u'm', 1, u'{0:.{1}f} m', 0),
+    (u'ft', 1, u'{0:.{1}f} ft', 0)
 )
 
 unit_presets = (
@@ -110,9 +97,13 @@ def _format(units, name, default, value, ndigits=None):
         setting = default
 
     if ndigits is None:
-        ndigits = units[setting][2]
+        ndigits = units[setting][3]
 
-    return units[setting][1](float(value), ndigits)
+    factor = units[setting][1]
+    format = units[setting][2]
+
+    value = round(value * factor, ndigits)
+    return format_decimal(value, format=format.format(0.0, ndigits))
 
 
 def format_distance(value, ndigits=None):
