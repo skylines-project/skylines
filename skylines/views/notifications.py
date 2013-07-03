5ab242d08392a8c3379460a4a94f74ab17e41f5d
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-07-03 23:04:34 +0200

    notifications: 1-based pagination

--- skylines/views/notifications.py
+++ skylines/views/notifications.py
@@ -86,11 +86,11 @@ def index():
 
     query = _filter_query(query, request.args)
 
-    page = request.args.get('page', type=int, default=0)
+    page = request.args.get('page', type=int, default=1)
     per_page = request.args.get('per_page', type=int, default=50)
 
     query = query.limit(per_page)
-    query = query.offset(page * per_page)
+    query = query.offset((page - 1) * per_page)
 
     def get_event(notification):
         event = notification.event
@@ -106,7 +106,7 @@ def index():
 
     template_vars = dict(events=events, types=Event.Type)
 
-    if page > 0:
+    if page > 1:
         template_vars['prev_page'] = page - 1
     if events_count == per_page:
         template_vars['next_page'] = page + 1
