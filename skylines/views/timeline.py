0e001eae386d1b39972b1b644906478c6a9018e9
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-07-16 23:28:12 +0200

    timeline: Added new "Timeline" view
    Similar to the "Notifications" view, but for the whole application.

---
+++ skylines/views/timeline.py
@@ -0,0 +1,39 @@
+from flask import Blueprint, render_template, request
+from sqlalchemy.orm import joinedload
+
+from skylines.lib.util import str_to_bool
+from skylines.model import Event
+from .notifications import _filter_query, _group_events
+
+timeline_blueprint = Blueprint('timeline', 'skylines')
+
+
+@timeline_blueprint.route('/')
+def index():
+    query = Event.query() \
+        .options(joinedload('actor')) \
+        .options(joinedload('flight')) \
+        .order_by(Event.time.desc())
+
+    query = _filter_query(query, request.args)
+
+    page = request.args.get('page', type=int, default=1)
+    per_page = request.args.get('per_page', type=int, default=50)
+
+    query = query.limit(per_page)
+    query = query.offset((page - 1) * per_page)
+
+    events = query.all()
+    events_count = len(events)
+
+    if request.args.get('grouped', True, type=str_to_bool):
+        events = _group_events(events)
+
+    template_vars = dict(events=events, types=Event.Type)
+
+    if page > 1:
+        template_vars['prev_page'] = page - 1
+    if events_count == per_page:
+        template_vars['next_page'] = page + 1
+
+    return render_template('timeline/list.jinja', **template_vars)
