f193782d1f06815fc28d4c6ea58d78cc188f68c5
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-08-18 15:13:52 +0200

    scripts: Show tracking sessions in tracking/stats.py

--- scripts/tracking/stats.py
+++ scripts/tracking/stats.py
@@ -22,6 +22,8 @@ if not to_envvar(args.config):
     parser.error('Config file "{}" not found.'.format(args.config))
 
 
+from datetime import timedelta
+from itertools import chain
 from skylines import db
 from skylines.model import TrackingFix, User
 
@@ -32,22 +34,63 @@ def get_pilot(user_id):
 
 
 def get_base_query(user_id):
-    return db.session.query(TrackingFix).filter_by(pilot_id=user_id)
+    return db.session.query(TrackingFix) \
+        .filter_by(pilot_id=user_id) \
+        .order_by(TrackingFix.time)
+
+
+def gather_sessions_statistics(user_id):
+    sessions = []
+
+    session = None
+    last_fix = None
+    for fix in chain(get_base_query(user_id), [None]):
+        is_start = (last_fix is None)
+        is_end = (fix is None)
+
+        # check if this is a new live tracking session (dt > 3 hours)
+        dt = (fix.time - last_fix.time) if (fix and last_fix) else None
+        is_new_session = dt and dt > timedelta(hours=3)
+
+
+        # save last_fix in session and append it to the session list
+        if is_end or is_new_session:
+            session['end'] = last_fix.time
+            sessions.append(session)
+
+        # start a new session
+        if is_start or is_new_session:
+            session = dict()
+            session['start'] = fix.time
+
+        last_fix = fix
+
+    return sessions
 
 
 def gather_statistics(args):
     stats = dict()
     stats['pilot'] = get_pilot(args.user)
     stats['num_fixes'] = get_base_query(args.user).count()
+    stats['sessions'] = gather_sessions_statistics(args.user)
     return stats
 
 
 def print_statistics(stats):
     pilot = stats.get('pilot')
+    sessions = stats.get('sessions')
 
     print 'Live tracking statistics for user: {} (ID: {})'.format(pilot.get('name'), pilot.get('id'))
     print
+    print 'Number of sessions: {}'.format(len(sessions))
     print 'Number of received fixes: {}'.format(stats.get('num_fixes'))
+    print
+    print 'Sessions:'
+    for session in sessions:
+        print '{date}: {start}-{end}'.format(
+            date=session.get('start').strftime('%d.%m.%Y'),
+            start=session.get('start').strftime('%H:%M'),
+            end=session.get('end').strftime('%H:%M'))
 
 
 stats = gather_statistics(args)
