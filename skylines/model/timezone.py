2cc8b7315f4d8e66800c40a24b477adbc06a7b2d
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-09-08 12:43:10 +0200

    model/timezone: Fixed copy/paste issue in __repr__() method

--- skylines/model/timezone.py
+++ skylines/model/timezone.py
@@ -16,7 +16,7 @@ class TimeZone(db.Model):
         return self.tzid
 
     def __repr__(self):
-        return ('<AircraftModel: id=%d tzid=\'%s\'>' % (self.id, self.tzid)).encode('unicode_escape')
+        return ('<TimeZone: id=%d tzid=\'%s\'>' % (self.id, self.tzid)).encode('unicode_escape')
 
     @classmethod
     def by_location(cls, location):
