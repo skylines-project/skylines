e622b51e9c3e1f387ca669aa3338d867f772c0ce
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-10-27 22:21:31 +0100

    setup.py: Moved main requirements list back to setup.py script

--- setup.py
+++ setup.py
@@ -2,9 +2,36 @@
 
 from setuptools import setup, find_packages
 
-requirements = open('requirements.txt').readlines()
-test_requirements = [r for r in open('test-requirements.txt').readlines()
-                     if not r.startswith('http')]
+requirements = [
+    'flask==0.10.1',
+    'werkzeug==0.9.4',
+    'Flask-Babel==0.9',
+    'Flask-Assets==0.8',
+    'Flask-Login==0.2.7',
+    'Flask-Cache==0.12',
+    'Flask-SQLAlchemy==1.0',
+    'Flask-DebugToolbar==0.8.0',
+    'Flask-WTF==0.9.3',
+    'sqlalchemy==0.8.2',
+    'alembic==0.6.0',
+    'skylinespolyencode==0.1.3',
+    'psycopg2==2.4.6',
+    'geoalchemy2==0.2.1',
+    'shapely==1.2.18',
+    'crc16==0.1.1',
+    'markdown==2.3.1',
+    'pytz',
+    'webassets==0.8',
+    'cssmin==0.1.4',
+    'twisted==13.1',
+    'closure',
+    'WebHelpers==1.3',
+    'celery_with_redis==3.0',
+    'xcsoar==0.1',
+]
+
+test_requirements = [r for r in open('requirements.txt').readlines()
+                     if not r.startswith('http') and not r.startswith('-e')]
 
 setup(
     name='SkyLines',
