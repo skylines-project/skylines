c59063462208cf107f6c8912b00aedd39c2f0d87
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-11-08 18:25:18 +0100

    tests: Replaced PEP8 test with flake8 test

---
+++ tests/test_flake8.py
@@ -0,0 +1,32 @@
+import sys
+import nose
+from subprocess import CalledProcessError, check_output as run
+
+FLAKE8_COMMAND = 'flake8'
+
+FLAKE8_INPUTS = [
+    'skylines',
+    'scripts',
+]
+
+
+def test_flake8():
+    """ Run skylines package through flake8 """
+    args = [FLAKE8_COMMAND]
+
+    # Append package name that should be checked
+    args.extend(FLAKE8_INPUTS)
+
+    try:
+        run(args)
+    except CalledProcessError, e:
+        print e.output
+        raise AssertionError('flake8 has found errors.')
+    except OSError:
+        raise OSError('Failed to run flake8. Please check that you have '
+                      'installed it properly.')
+
+
+if __name__ == "__main__":
+    sys.argv.append(__name__)
+    nose.run()
