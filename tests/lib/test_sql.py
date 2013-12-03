cdfb159ba5f56aa042399d3af9672de602203fba
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-12-03 18:17:05 +0100

    tests/lib/test_sql: Use test class to get rid of global context variable

--- tests/lib/test_sql.py
+++ tests/lib/test_sql.py
@@ -6,26 +6,6 @@ from skylines import db, app
 from tests import setup_db, teardown_db
 from sqlalchemy import Column, Integer, String, Unicode
 
-context = None
-
-
-def setup():
-    global context
-    context = app.app_context()
-    context.push()
-
-    # Setup the database
-    setup_db()
-    db.session.add(TestTable(name='John Doe', uni='Jane and John Doe'))
-    db.session.commit()
-
-
-def teardown():
-    # Remove the database again
-    teardown_db()
-
-    context.pop()
-
 
 class TestTable(db.Model):
     __tablename__ = 'ilike_test'
@@ -35,27 +15,43 @@ class TestTable(db.Model):
     uni = Column(Unicode(32))
 
 
-def test_weighted_ilike():
-    """ String.weighted_ilike() works as expected """
+class TestSqlLib:
 
-    eq_(db.session.query(
-        TestTable.name.weighted_ilike('%John%', 1)).scalar(), 1)
-    eq_(db.session.query(
-        TestTable.name.weighted_ilike('%John%', 5)).scalar(), 5)
-    eq_(db.session.query(
-        TestTable.name.weighted_ilike('%John%', 100)).scalar(), 100)
+    def setup(self):
+        self.context = app.app_context()
+        self.context.push()
 
-    eq_(db.session.query(
-        TestTable.name.weighted_ilike('%John%')).scalar(), 1)
+        # Setup the database
+        setup_db()
+        db.session.add(TestTable(name='John Doe', uni='Jane and John Doe'))
+        db.session.commit()
 
-    eq_(float(db.session.query(
-        TestTable.name.weighted_ilike('%John%', 0.1)).scalar()), 0.1)
+    def teardown(self):
+        # Remove the database again
+        teardown_db()
 
+        self.context.pop()
 
-def test_weighted_ilike_exception():
-    """ String.weighted_ilike() fails as expected """
+    def test_weighted_ilike(self):
+        """ String.weighted_ilike() works as expected """
 
-    assert_raises(AssertionError, TestTable.name.weighted_ilike, '%John%', '5')
+        eq_(db.session.query(
+            TestTable.name.weighted_ilike('%John%', 1)).scalar(), 1)
+        eq_(db.session.query(
+            TestTable.name.weighted_ilike('%John%', 5)).scalar(), 5)
+        eq_(db.session.query(
+            TestTable.name.weighted_ilike('%John%', 100)).scalar(), 100)
+
+        eq_(db.session.query(
+            TestTable.name.weighted_ilike('%John%')).scalar(), 1)
+
+        eq_(float(db.session.query(
+            TestTable.name.weighted_ilike('%John%', 0.1)).scalar()), 0.1)
+
+    def test_weighted_ilike_exception(self):
+        """ String.weighted_ilike() fails as expected """
+
+        assert_raises(AssertionError, TestTable.name.weighted_ilike, '%John%', '5')
 
 
 if __name__ == "__main__":
