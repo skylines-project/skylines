a16de627d95d09ff3f1a6f566947938d9a79198d
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2014-02-26 18:57:46 +0100

    fabric: Call restart.sh in repo folder directly
    .. and do the git reset call

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,10 @@
-from fabric.api import env, run
+from fabric.api import env, cd, run
 
 env.use_ssh_config = True
 env.hosts = ['root@skylines']
 
 
 def restart():
-    run('./restart.sh')
+    with cd('/opt/skylines/src'):
+        run('git reset --hard')
+        run('./restart.sh')
