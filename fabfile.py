4ff67e7f36e76d178cd7e8d608a45045c76126eb
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2014-02-26 18:59:38 +0100

    fabric: Moved instructions from restart.sh into the fabfile

--- fabfile.py
+++ fabfile.py
@@ -7,4 +7,18 @@ env.hosts = ['root@skylines']
 def restart():
     with cd('/opt/skylines/src'):
         run('git reset --hard')
-        run('./restart.sh')
+
+        # compile i18n .mo files
+        run('./manage.py babel compile')
+
+        # generate JS/CSS assets
+        run('./manage.py assets build')
+
+        # do database migrations
+        run('sudo -u skylines ./manage.py migrate upgrade')
+
+        # restart services
+        run('sv restart skylines-fastcgi')
+        run('sv restart mapserver-fastcgi')
+        run('sv restart skylines-daemon')
+        run('sv restart celery-daemon')
