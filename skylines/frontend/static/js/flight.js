skylines-project/skylines

commit bacf581e91543a552e5db36524f8c46119705dc1

flight.js: redraw baro only on time interval changes

skylines/frontend/static/js/flight.js
@@ -169,12 +169,16 @@ function updateBaroScale() {
       last_t = flight.t[last];
   });
 
-  if (last_t == 0)
-    baro.clearTimeInterval();
-  else
-    baro.setTimeInterval(first_t, last_t);
+  var redraw = false;
 
-  baro.draw();
+  if (last_t == 0) {
+    baro.clearTimeInterval();
+    redraw = true;
+  } else {
+    redraw = baro.setTimeInterval(first_t, last_t);
+  }
+
+  if (redraw) baro.draw();
 }
 
 

skylines/frontend/static/js/baro.js
@@ -179,11 +179,18 @@ function slBarogram(placeholder) {
    *
    * @param {number} start The earliest time that should be shown.
    * @param {number} end The latest time that should be shown.
+   * @return {Boolean} true if the time interval has changed
    */
   baro.setTimeInterval = function(start, end) {
     var opt = flot.getOptions();
-    opt.xaxes[0].min = start * 1000;
-    opt.xaxes[0].max = end * 1000;
+    if (opt.xaxes[0].min != start * 1000 &&
+        opt.xaxes[0].max != end * 1000) {
+      opt.xaxes[0].min = start * 1000;
+      opt.xaxes[0].max = end * 1000;
+      return true;
+    } else {
+      return false;
+    }
   };
 
 

commit 221734abb25007979805c693bbbb1603cae38829

flight.js: force baro scale update on addFlight and removeFlight
This seems to got broken with bacf581e91543a552e5db36524f8c46119705dc1

skylines/frontend/static/js/flight.js
@@ -122,7 +122,7 @@ function initFlightLayer() {
     initRedrawLayer(flightPathLayer);
   });
 
-  map.events.register('moveend', null, updateBaroScale);
+  map.events.register('moveend', null, function(e) { updateBaroScale(); });
 
   map.hover_enabled = true;
 
@@ -146,7 +146,7 @@ function initFixTable() {
 
 initFixTable();
 
-function updateBaroScale() {
+function updateBaroScale(opt_force) {
   var first_t = 999999;
   var last_t = 0;
 
@@ -181,7 +181,7 @@ function updateBaroScale() {
     redraw = baro.setTimeInterval(first_t, last_t);
   }
 
-  if (redraw) baro.draw();
+  if (redraw || opt_force) baro.draw();
 }
 
 
@@ -331,7 +331,7 @@ function addFlight(sfid, _lonlat, _levels, _num_levels, _time, _height, _enl,
   });
 
   updateBaroData();
-  updateBaroScale();
+  updateBaroScale(true);
 
   // Set fix data table into "selectable" mode if
   // more than one flight is loaded
@@ -423,7 +423,7 @@ function removeFlight(sfid) {
   flights.remove(sfid);
   fix_table.removeRow(sfid);
   updateBaroData();
-  updateBaroScale();
+  updateBaroScale(true);
 }
 
 