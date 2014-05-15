4a7e01a904de44269a4d3dd1a084f62e7c5d7348
Tobias Lohner <tobias@lohner-net.de>
2014-05-15 23:07:11 +0200

    commands.import_.srtm: add option to import local files

--- skylines/commands/import_/srtm.py
+++ skylines/commands/import_/srtm.py
@@ -3,6 +3,8 @@ from flask.ext.script import Command, Option
 
 import os
 import subprocess
+import shutil
+from glob import glob
 from flask import current_app
 from skylines.model import db
 
@@ -13,11 +15,45 @@ class SRTM(Command):
     SERVER_URL = 'http://download.xcsoar.org/mapgen/data/srtm3/'
 
     option_list = (
-        Option('x', type=int),
-        Option('y', type=int),
+        Option('--tile', nargs=2, type=int, metavar=('x', 'y'), help='Import SRTM tile x y'),
+        Option('--file', nargs='+', metavar=('filename'), help='Import tile from local file(s)'),
     )
 
-    def run(self, x, y):
+    def run(self, tile, file):
+        if tile and file:
+            print 'Please select either a tile to download or specify local file(s).'
+            sys.exit(1)
+
+        # Change path to configured srtm data path
+        path = current_app.config['SKYLINES_ELEVATION_PATH']
+
+        if not os.path.exists(path):
+            print 'Creating {} ...'.format(path)
+            os.mkdir(path)
+
+        if tile:
+            os.chdir(path)
+            filename = self.download_tile(tile[0], tile[1])
+            self.raster2pgsql(filename)
+
+        if file:
+            files = []
+            for item in file:
+                files += glob(item)
+
+            for filename in files:
+                abspath = os.path.abspath(filename)
+                tiled_tif_filename = os.path.join(path, os.path.basename(abspath))
+
+                if os.path.exists(tiled_tif_filename):
+                    print "{} exists already.".format(tiled_tif_filename)
+                    continue
+
+                shutil.copy(abspath, tiled_tif_filename)
+
+                self.raster2pgsql(tiled_tif_filename)
+
+    def download_tile(self, x, y):
         if not 1 <= x <= 72:
             print 'x has to be between 1 and 72'
             sys.exit(1)
@@ -27,15 +63,6 @@ class SRTM(Command):
             sys.exit(1)
 
         basename = 'srtm_{x:02}_{y:02}'.format(x=x, y=y)
-
-        # Change path to configured srtm data path
-        path = current_app.config['SKYLINES_ELEVATION_PATH']
-
-        if not os.path.exists(path):
-            print 'Creating {} ...'.format(path)
-            os.mkdir(path)
-
-        os.chdir(path)
 
         # Check if tiled GeoTIFF file already exists
         tiled_tif_filename = basename + '_tiled.tif'
@@ -51,6 +78,9 @@ class SRTM(Command):
         url = self.SERVER_URL + tif_filename
         subprocess.check_call(['wget', '-O', tiled_tif_filename, url])
 
+        return tiled_tif_filename
+
+    def raster2pgsql(self, tiled_tif_filename):
         # Create SQL statements
         print 'Converting {} to SQL ...'.format(tiled_tif_filename)
         args = [
