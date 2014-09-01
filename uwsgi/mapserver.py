bb4beb724282986be443cf3b62ff3896dc8f7108
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2014-09-01 06:13:01 +0200

    uwsgi: Added mapserver configuration

--- uwsgi/mapserver.py
+++ uwsgi/mapserver.py
@@ -1,35 +1,15 @@
-#!/usr/bin/env python
-#
-# Wrapper script for launching UMN mapserver as a FastCGI in lighttpd.
-#
-
 import sys, os
-import thread
 import argparse
 import mapscript
-from flup.server.fcgi import WSGIServer
 
 parser = argparse.ArgumentParser(description='Run the SkyLines MapScript FastCGI daemon.')
 parser.add_argument('map_file', nargs='?', metavar='airspace.map',
                     default='mapserver/airspace/airspace.map',
                     help='path to the airspace map file')
-parser.add_argument('--socket', nargs='?', metavar='PATH',
-                    help='path of the local socket')
-parser.add_argument('--logfile', nargs='?', metavar='PATH',
-                    help='path of the log file')
 args = parser.parse_args()
 
-# stderr doesn't work with FastCGI; the following is a hack to get a
-# log file with diagnostics anyway
-if args.logfile:
-    sys.stderr = sys.stdout = file(args.logfile, 'a')
 
-sys.path.append(os.path.dirname(sys.argv[0]))
-
-thread.stack_size(524288)
-
-
-def wsgi_app(environ, start_response):
+def application(environ, start_response):
     try:
         req = mapscript.OWSRequest()
         req.loadParamsFromURL(environ.get('QUERY_STRING'))
@@ -51,8 +31,3 @@ def wsgi_app(environ, start_response):
     except:
         start_response('500 Ooops', [('Content-type', 'text/plain')])
         return ['An error occured\n' + mapscript.msIO_getStdoutBufferString()]
-
-if __name__ == '__main__':
-    WSGIServer(wsgi_app, bindAddress=args.socket, umask=0,
-               minSpare=1, maxSpare=5, maxThreads=10).run()
-
