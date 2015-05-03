6f595d6c60b51dde3828c8283ea08dfc7750f5cc
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-05-03 18:18:37 +0200

    tests/frontend/test_root: Use pytest fixtures
    ... instead of TestController

--- tests/frontend/functional/test_root.py
+++ tests/frontend/functional/test_root.py
@@ -1,15 +1,8 @@
 # -*- coding: utf-8 -*-
-"""
-Functional test suite for the root controller.
-"""
-from . import TestController
 
 
-class TestRootController(TestController):
-    """Tests for the method in the root controller."""
+def test_index(browser):
+    """ The front page is show the about page """
+    browser.open('/')
 
-    def test_index(self):
-        """ The front page is show the about page """
-        self.browser.open('/')
-
-        assert 'Welcome to SkyLines' in self.browser.contents
+    assert 'Welcome to SkyLines' in browser.contents
