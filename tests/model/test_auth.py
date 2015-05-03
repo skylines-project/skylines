e9111924d746bfc08d1e436fc4cc28659b1587e4
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-05-03 22:37:28 +0200

    tests/model/test_auth: Use global 'test_user' fixture

--- tests/model/test_auth.py
+++ tests/model/test_auth.py
@@ -1,27 +1,6 @@
 # -*- coding: utf-8 -*-
-import pytest
 
 from skylines.model import db, User
-
-attrs = dict(
-    email_address=u"ignucius@example.org",
-    first_name=u"Test",
-    last_name=u"User",
-    password="something"
-)
-
-
-@pytest.fixture(scope='function')
-def test_user(db_session):
-    obj = User(**attrs)
-    db_session.add(obj)
-    db_session.commit()
-    return obj
-
-
-def test_obj_creation_email(test_user):
-    """The obj constructor must set the email right"""
-    assert test_user.email_address == u"ignucius@example.org"
 
 
 def test_no_permissions_by_default(test_user):
@@ -31,7 +10,7 @@ def test_no_permissions_by_default(test_user):
 
 def test_getting_by_email(test_user):
     """Users should be fetcheable by their email addresses"""
-    him = User.by_email_address(u"ignucius@example.org")
+    him = User.by_email_address(test_user.email_address)
     assert him == test_user
 
 
@@ -40,6 +19,6 @@ def test_query_obj(test_user):
     obj = db.session.query(User).one()
 
     assert obj == test_user
-    assert obj.email_address == attrs.get('email_address')
-    assert obj.first_name == attrs.get('first_name')
-    assert obj.last_name == attrs.get('last_name')
+    assert obj.email_address == test_user.email_address
+    assert obj.first_name == test_user.first_name
+    assert obj.last_name == test_user.last_name
