aed59b0ed17f9ef326c0ea949a6f35f17422cf7f
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-05-03 19:24:02 +0200

    tests/model/test_auth: Use simple methods instead of class

--- tests/model/test_auth.py
+++ tests/model/test_auth.py
@@ -1,31 +1,45 @@
 # -*- coding: utf-8 -*-
+import pytest
 
-from skylines import model
-from . import ModelTest
+from skylines.model import db, User
+
+attrs = dict(
+    email_address=u"ignucius@example.org",
+    first_name=u"Test",
+    last_name=u"User",
+    password="something"
+)
 
 
-class TestUser(ModelTest):
-    """Unit test case for the ``User`` model."""
+@pytest.fixture(scope='function')
+def test_user(db):
+    obj = User(**attrs)
+    db.add(obj)
+    db.commit()
+    return obj
 
-    klass = model.User
-    attrs = dict(
-        email_address=u"ignucius@example.org",
-        first_name=u"Test",
-        last_name=u"User",
-    )
 
-    def do_get_dependencies(self):
-        return dict(password="something")
+def test_obj_creation_email(test_user):
+    """The obj constructor must set the email right"""
+    assert test_user.email_address == u"ignucius@example.org"
 
-    def test_obj_creation_email(self):
-        """The obj constructor must set the email right"""
-        assert self.obj.email_address == u"ignucius@example.org"
 
-    def test_no_permissions_by_default(self):
-        """User objects should have no permission by default."""
-        assert self.obj.admin == False
+def test_no_permissions_by_default(test_user):
+    """User objects should have no permission by default."""
+    assert test_user.admin == False
 
-    def test_getting_by_email(self):
-        """Users should be fetcheable by their email addresses"""
-        him = model.User.by_email_address(u"ignucius@example.org")
-        assert him == self.obj
+
+def test_getting_by_email(test_user):
+    """Users should be fetcheable by their email addresses"""
+    him = User.by_email_address(u"ignucius@example.org")
+    assert him == test_user
+
+
+def test_query_obj(test_user):
+    """Model objects can be queried"""
+    obj = db.session.query(User).one()
+
+    assert obj == test_user
+    assert obj.email_address == attrs.get('email_address')
+    assert obj.first_name == attrs.get('first_name')
+    assert obj.last_name == attrs.get('last_name')
