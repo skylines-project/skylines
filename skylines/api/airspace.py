e2fe4b8cd2c88143b7ce05585cbf9b7d0a25bcfd
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-05-05 19:29:27 +0200

    api/airspace: Use AirspaceSchema class for serialization

--- skylines/api/airspace.py
+++ skylines/api/airspace.py
@@ -1,3 +1,4 @@
+from skylines.api.schemas import airspace_list_schema
 from skylines.model import Airspace, Location
 
 
@@ -5,15 +6,7 @@ def get_airspaces_by_location(location):
     if not isinstance(location, Location):
         raise TypeError('Invalid `location` parameter.')
 
-    airspaces = Airspace.by_location(location)
-    return map(airspace_to_dict, airspaces)
+    data, errors = airspace_list_schema.dump(
+        Airspace.by_location(location).all(), many=True)
 
-
-def airspace_to_dict(airspace):
-    return {
-        'name': airspace.name,
-        'base': airspace.base,
-        'top': airspace.top,
-        'airspace_class': airspace.airspace_class,
-        'country': airspace.country_code,
-    }
+    return data
