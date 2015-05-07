6f80414015923538b941abb5f108b6dbfc637c4a
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-05-07 20:37:47 +0200

    Run API server on port 5001 by default

--- Vagrantfile
+++ Vagrantfile
@@ -75,6 +75,7 @@ Vagrant.configure("2") do |config|
   config.vm.box_url = 'http://puppet-vagrant-boxes.puppetlabs.com/ubuntu-server-12042-x64-vbox4210.box'
 
   config.vm.network 'forwarded_port', guest: 5000, host: 5000
+  config.vm.network 'forwarded_port', guest: 5001, host: 5001
 
   config.vm.provision 'shell', inline: $script, privileged: false
 end
--- skylines/commands/__init__.py
+++ skylines/commands/__init__.py
@@ -35,7 +35,7 @@ manager = Manager(_create_app)
 manager.add_option('-c', '--config', dest='config', required=False)
 manager.add_command("shell", Shell())
 manager.add_command("runserver", Server(host='0.0.0.0'))
-manager.add_command("run_api_server", APIServer(host='0.0.0.0'))
+manager.add_command("run_api_server", APIServer(host='0.0.0.0', port=5001))
 manager.add_command("migrate", MigrateCommand)
 
 manager.add_command("aircraft", aircraft_manager)
