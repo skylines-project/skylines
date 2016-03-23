56e5e07b3bfa083554e42aeb99c671b6b8d059bd
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-03-23 22:42:02 +0100

    api/views/user: Fix /user route

--- skylines/api/views/user.py
+++ skylines/api/views/user.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, g
+from flask import Blueprint, request
 
+from skylines.model import User
 from skylines.api.oauth import oauth
 from skylines.api.schemas import current_user_schema
 from skylines.api.views.json import jsonify
@@ -11,5 +12,6 @@ user = Blueprint('user', 'skylines')
 @user.route('/user')
 @oauth.required()
 def read():
-    result = current_user_schema.dump(g.user)
+    user = User.get(request.user_id)
+    result = current_user_schema.dump(user)
     return jsonify(result.data)
