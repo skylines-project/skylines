2521a55d7efa7815eb02ab5372e9bfa0e6039db8
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-06-02 20:10:05 +0200

    fabfile: Run Ember.js build step on deploy

--- fabfile.py
+++ fabfile.py
@@ -29,6 +29,11 @@ def restart():
     with cd(SRC_DIR):
         run('git reset --hard')
 
+        with cd('ember'):
+            run('npm install')
+            run('bower install')
+            run('node_modules/.bin/ember build -o ../skylines/frontend/static/ember -prod')
+
         # compile i18n .mo files
         manage('babel compile')
 
