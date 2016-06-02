745e3ce30856ddba2b4b22a4644a6da3a94e3826
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-06-02 19:38:28 +0200

    ember/config: Disable "locationType" setting

--- ember/config/environment.js
+++ ember/config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'skylines',
     environment: environment,
     baseURL: '/',
-    locationType: 'auto',
+    locationType: 'none',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
