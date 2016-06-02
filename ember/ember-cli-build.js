3da6799c282bf4dba3d73443f8e119a54069661e
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-06-02 18:23:41 +0200

    ember/config: Store config in JS file

--- ember/ember-cli-build.js
+++ ember/ember-cli-build.js
@@ -4,7 +4,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    storeConfigInMeta: false,
   });
 
   // Use `app.import` to add additional libraries to the generated
