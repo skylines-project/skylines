c734fc6e102a04198349fb6d9bc61a22ea0dbfd9
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-06-02 21:31:27 +0200

    ember: Add "pin-star" component

---
+++ ember/app/components/pin-star.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  pinnedFlights: Ember.inject.service(),
+
+  tagName: 'span',
+  classNames: ['pin'],
+  classNameBindings: ['pinned'],
+
+  pinned: Ember.computed('flightId', 'pinnedFlights.pinned.[]', function() {
+    return this.get('pinnedFlights.pinned').contains(this.get('flightId'));
+  }),
+
+  didInsertElement() {
+    this.$().tooltip({
+      placement: 'left',
+      title: 'Activate this to show the flight on top of other flights on the map'
+    });
+  },
+
+  click() {
+    this.get('pinnedFlights').toggle(this.get('flightId'));
+  },
+});
