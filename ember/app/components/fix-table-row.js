9773343204b86390ead088b871abf6ce44827913
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-06-06 19:19:58 +0200

    components/fix-table: Extract "fix-table-row" component

---
+++ ember/app/components/fix-table-row.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'tr',
+  classNameBindings: ['selected'],
+  attributeBindings: ['style'],
+
+  style: Ember.computed('selectable', function() {
+    if (this.get('selectable')) {
+      return Ember.String.htmlSafe('cursor: pointer');
+    }
+  }),
+
+  badgeStyle: Ember.computed('row.color', function() {
+    return Ember.String.htmlSafe(`background-color: ${this.get('row.color')}`);
+  }),
+
+  click() {
+    this.get('onSelect')(this.get('row.id'));
+  },
+
+  actions: {
+    remove() {
+      this.get('onRemove')(this.get('row.id'));
+    },
+  },
+});
