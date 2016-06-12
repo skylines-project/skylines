2995d41e7d22a9e6e445859ceea18267461055ed
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-06-12 18:47:15 +0200

    services/flight-phase: Calculate flight phase coordinates

--- ember/app/services/flight-phase.js
+++ ember/app/services/flight-phase.js
@@ -1,10 +1,52 @@
+/* global getNextSmallerIndex */
+
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  fixCalc: Ember.inject.service(),
+
   selection: null,
+
+  flight: Ember.computed.readOnly('fixCalc.flights.0'),
+
+  coordinates: Ember.computed('flight', 'selection.{start,end}', function() {
+    let selection = this.get('selection');
+    if (!selection) {
+      return;
+    }
+
+    let {start, end} = selection;
+
+    let flight = this.get('flight');
+    let times = flight.get('time');
+
+    let start_index = getNextSmallerIndex(times, start);
+    let end_index = getNextSmallerIndex(times, end);
+    if (start_index >= end_index) {
+      return;
+    }
+
+    let coordinates = flight.get('geometry').getCoordinates();
+    return coordinates.slice(start_index, end_index + 1);
+  }),
+
+  startCoordinate: Ember.computed.readOnly('coordinates.firstObject'),
+  endCoordinate: Ember.computed.readOnly('coordinates.lastObject'),
+
+  startPoint: computedPoint('coordinates.firstObject'),
+  endPoint: computedPoint('coordinates.lastObject'),
 
   init() {
     this._super(...arguments);
     window.flightPhaseService = this;
   },
 });
+
+function computedPoint(key) {
+  return Ember.computed(key, function() {
+    let coordinate = this.get(key)
+    if (coordinate) {
+      return new ol.geom.Point(coordinate);
+    }
+  })
+}
