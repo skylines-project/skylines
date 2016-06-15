901200f09423089e97679f5523d7e13f44fb8357
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-06-15 22:14:17 +0200

    services/cesium-loader: Prevent duplicate script tags

--- ember/app/services/cesium-loader.js
+++ ember/app/services/cesium-loader.js
@@ -3,17 +3,26 @@ import Ember from 'ember';
 const CESIUM_BASE_URL = '/cesium/';
 
 export default Ember.Service.extend({
+  loaderPromise: null,
+
   init() {
     this._super(...arguments);
     window.cesiumLoader = this;
   },
 
   load() {
-    return new Ember.RSVP.Promise(resolve => {
-      let cesium = document.createElement('script');
-      cesium.src = CESIUM_BASE_URL + 'Cesium.js';
-      cesium.onload = resolve;
-      document.body.appendChild(cesium);
-    });
+    let promise = this.get('loaderPromise');
+    if (!promise) {
+      promise = new Ember.RSVP.Promise(resolve => {
+        let cesium = document.createElement('script');
+        cesium.src = CESIUM_BASE_URL + 'Cesium.js';
+        cesium.onload = resolve;
+        document.body.appendChild(cesium);
+      });
+
+      this.set('loaderPromise', promise);
+    }
+
+    return promise;
   },
 });
