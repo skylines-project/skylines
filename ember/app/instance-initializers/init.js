6480a76d83e2a0457cf15b6417ce45d7d5db8cc5
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-06-16 20:58:04 +0200

    initializers: Remove unnecessary service boot up

--- ember/app/instance-initializers/init.js
+++ ember/app/instance-initializers/init.js
@@ -1,8 +1,5 @@
 export function initialize(appInstance) {
   // boot up services
-  appInstance.lookup('service:cesium-loader');
-  appInstance.lookup('service:flight-phase');
-  appInstance.lookup('service:fix-calc');
   appInstance.lookup('service:pinned-flights');
 }
 
