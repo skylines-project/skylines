a9c22a3429714c2de7b2a93065d47580c64d3eec
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-06-18 18:14:24 +0200

    components/tracking-table-row: Add missing "altitudeAGL" property

--- ember/app/components/tracking-table-row.js
+++ ember/app/components/tracking-table-row.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
+import safeComputed from '../utils/safe-computed';
+
 export default Ember.Component.extend({
   tagName: 'tr',
+
+  altitudeAGL: safeComputed('track.altitude', 'track.elevation',
+    (altitude, elevation) => Math.max(altitude - elevation, 0)),
 });
