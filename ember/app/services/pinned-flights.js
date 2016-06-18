9e02a0fed50686120712a54d5a45b27e406792f1
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-06-18 15:59:35 +0200

    js/general: Move pinning code into "pinned-flights" service

--- ember/app/services/pinned-flights.js
+++ ember/app/services/pinned-flights.js
@@ -1,4 +1,4 @@
-/* global getPinnedFlights, pinFlight, unpinFlight */
+/* global showPinnedFlightsLink */
 
 import Ember from 'ember';
 
@@ -30,3 +30,61 @@ export default Ember.Service.extend({
     }
   },
 });
+
+/**
+ * Saves the flight id into the pinnedFlights cookie
+ *
+ * @param {Number} sfid SkyLines flight ID.
+ */
+function pinFlight(sfid) {
+  var pinnedFlights = getPinnedFlights();
+  for (var i = 0; i < pinnedFlights.length; i++) {
+    if (pinnedFlights[i] == sfid) return;
+  }
+
+  pinnedFlights.push(sfid);
+  Ember.$.cookie('SkyLines_pinnedFlights', pinnedFlights.join(','), { path: '/' });
+
+  // show pinned flights link in list view if found in DOM
+  showPinnedFlightsLink();
+}
+
+/**
+ * Removes a pinned flight from the pinnedFlights cookie
+ *
+ * @param {Number} sfid SkyLines flight ID.
+ */
+function unpinFlight(sfid) {
+  var pinnedFlights = getPinnedFlights();
+  var temp = [];
+
+  for (var i = 0; i < pinnedFlights.length; i++) {
+    if (pinnedFlights[i] != sfid) temp.push(pinnedFlights[i]);
+  }
+
+  Ember.$.cookie('SkyLines_pinnedFlights', temp.join(','), { path: '/' });
+
+  // toggle the pinned flights link in list view if found in DOM
+  showPinnedFlightsLink();
+}
+
+/**
+ * Gets all pinned flights from the pinnedFlight cookie
+ *
+ * @return {Array<Number>} Array of SkyLines flight IDs.
+ */
+function getPinnedFlights() {
+  var cookie = Ember.$.cookie('SkyLines_pinnedFlights');
+
+  if (cookie) {
+    var pinnedFlights = cookie.split(',');
+
+    for (var i = 0; i < pinnedFlights.length; i++) {
+      pinnedFlights[i] = parseInt(pinnedFlights[i]);
+    }
+
+    return pinnedFlights;
+  }
+
+  return [];
+}
--- skylines/frontend/static/js/general.js
+++ skylines/frontend/static/js/general.js
@@ -12,67 +12,6 @@ function pad(num, size) {
 
 
 /**
- * Saves the flight id into the pinnedFlights cookie
- *
- * @param {Number} sfid SkyLines flight ID.
- */
-function pinFlight(sfid) {
-  var pinnedFlights = getPinnedFlights();
-  for (var i = 0; i < pinnedFlights.length; i++) {
-    if (pinnedFlights[i] == sfid) return;
-  }
-
-  pinnedFlights.push(sfid);
-  $.cookie('SkyLines_pinnedFlights', pinnedFlights.join(','), { path: '/' });
-
-  // show pinned flights link in list view if found in DOM
-  showPinnedFlightsLink();
-}
-
-
-/**
- * Removes a pinned flight from the pinnedFlights cookie
- *
- * @param {Number} sfid SkyLines flight ID.
- */
-function unpinFlight(sfid) {
-  var pinnedFlights = getPinnedFlights();
-  var temp = [];
-
-  for (var i = 0; i < pinnedFlights.length; i++) {
-    if (pinnedFlights[i] != sfid) temp.push(pinnedFlights[i]);
-  }
-
-  $.cookie('SkyLines_pinnedFlights', temp.join(','), { path: '/' });
-
-  // toggle the pinned flights link in list view if found in DOM
-  showPinnedFlightsLink();
-}
-
-
-/**
- * Gets all pinned flights from the pinnedFlight cookie
- *
- * @return {Array<Number>} Array of SkyLines flight IDs.
- */
-function getPinnedFlights() {
-  var cookie = $.cookie('SkyLines_pinnedFlights');
-
-  if (cookie) {
-    var pinnedFlights = cookie.split(',');
-
-    for (var i = 0; i < pinnedFlights.length; i++) {
-      pinnedFlights[i] = parseInt(pinnedFlights[i]);
-    }
-
-    return pinnedFlights;
-  }
-
-  return [];
-}
-
-
-/**
  * Returns the URL for the current page and add pinned flights
  * to the URL which are only stored client-side inside a cookie.
  *
