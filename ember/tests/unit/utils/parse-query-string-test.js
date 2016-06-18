f6181231a762846dd313cb19e7ef498d15c760e1
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-06-18 13:47:13 +0200

    Replace QUnit with Mocha

--- ember/tests/unit/utils/parse-query-string-test.js
+++ ember/tests/unit/utils/parse-query-string-test.js
@@ -1,24 +1,25 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
 import parseQueryString from 'skylines/utils/parse-query-string';
-import { module, test } from 'qunit';
 
-module('Unit | Utility | parse query string');
-
-test('it returns empty object on empty input', function(assert) {
-  let result = parseQueryString('');
-  assert.deepEqual(result, {});
-});
-
-test('it parses query strings', function(assert) {
-  let result = parseQueryString('a=5&b=abc');
-  assert.deepEqual(result, {
-    a: '5',
-    b: 'abc',
+describe('Unit | Utility | parse query string', function() {
+  it('returns empty object on empty input', function() {
+    let result = parseQueryString('');
+    expect(result).to.deep.equal({});
   });
-});
 
-test('it parses query strings with leading question mark', function(assert) {
-  let result = parseQueryString('?foo=42');
-  assert.deepEqual(result, {
-    foo: '42',
+  it('parses query strings', function() {
+    let result = parseQueryString('a=5&b=abc');
+    expect(result).to.deep.equal({
+      a: '5',
+      b: 'abc',
+    });
+  });
+
+  it('parses query strings with leading question mark', function() {
+    let result = parseQueryString('?foo=42');
+    expect(result).to.deep.equal({
+      foo: '42',
+    });
   });
 });
