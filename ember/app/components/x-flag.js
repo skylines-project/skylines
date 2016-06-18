01de6e0bd1d07b8256e6ed5857eda88c7fe0515d
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-06-18 18:15:02 +0200

    components/x-flag: Use lower country code

--- ember/app/components/x-flag.js
+++ ember/app/components/x-flag.js
@@ -1,5 +1,9 @@
 import Ember from 'ember';
 
+import safeComputed from '../utils/safe-computed';
+
 export default Ember.Component.extend({
   tagName: '',
+
+  codeLower: safeComputed('code', code => code.toLowerCase()),
 });
