51d9ec326f1842a3096e0728c3d62e6eb3e7eea3
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-06-21 21:21:28 +0200

    routes/wildcard: Remove pilot, club and airport properties from model

--- ember/app/routes/stats/wildcard.js
+++ ember/app/routes/stats/wildcard.js
@@ -4,12 +4,29 @@ export default Ember.Route.extend({
   ajax: Ember.inject.service(),
 
   model({wildcard}) {
-    return this.get('ajax').request(`/statistics/${wildcard}`).then(model => {
-      if (wildcard) {
-        let data = wildcard.split('/');
-        Ember.set(model, data[0], parseInt(data[1], 10));
-      }
-      return model;
-    });
+    if (wildcard) {
+      let data = wildcard.split('/');
+      this.set('page', data[0]);
+      this.set('id', parseInt(data[1], 10));
+    } else {
+      this.set('page', null);
+      this.set('id', null);
+    }
+
+    return this.get('ajax').request(`/statistics/${wildcard}`);
+  },
+
+  setupController(controller) {
+    this._super(...arguments);
+
+    controller.set('pilot', null);
+    controller.set('club', null);
+    controller.set('airport', null);
+
+    let page = this.get('page');
+    let id = this.get('id');
+    if (page && id) {
+      controller.set(page, id);
+    }
   },
 });
