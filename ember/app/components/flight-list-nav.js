fd65fee0081c92cfade18a44656d2e4cbf9ca640
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-06-23 18:25:54 +0200

    Add "flight-list-nav" component

---
+++ ember/app/components/flight-list-nav.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+import addDays from '../utils/add-days';
+import safeComputed from '../utils/safe-computed';
+
+export default Ember.Component.extend({
+  account: Ember.inject.service(),
+  pinnedFlights: Ember.inject.service(),
+
+  tagName: 'ul',
+  classNames: ['nav', 'nav-pills'],
+
+  hasPinned: Ember.computed.notEmpty('pinnedFlights.pinned'),
+
+  prevDate: safeComputed('date', date => addDays(date, -1)),
+  nextDate: safeComputed('date', date => addDays(date, 1)),
+});
---
+++ ember/tests/integration/components/flight-list-nav-test.js
@@ -0,0 +1,158 @@
+import Ember from 'ember';
+
+import { expect } from 'chai';
+import { describeComponent, it } from 'ember-mocha';
+import { beforeEach } from 'mocha';
+import hbs from 'htmlbars-inline-precompile';
+
+let options = { integration: true };
+
+describeComponent('flight-list-nav', 'Integration: FlightListNavComponent', options, function() {
+  beforeEach(function() {
+    this.register('service:account', Ember.Service.extend({
+      user: null,
+      club: null,
+    }));
+
+    this.register('service:pinned-flights', Ember.Service.extend({
+      pinned: [],
+    }));
+
+    this.inject.service('account', { as: 'account' });
+    this.inject.service('pinned-flights', { as: 'pinned' });
+  });
+
+  it('renders default view', function() {
+    this.render(hbs`{{flight-list-nav date=date airport=airport club=club pilot=pilot}}`);
+    expect(this.$('li')).to.have.length(2);
+    expect(this.$('li:nth-child(1)').text().trim()).to.equal('All');
+    expect(this.$('li:nth-child(2)').text().trim()).to.equal('Latest');
+  });
+
+  it('shows date', function() {
+    this.render(hbs`{{flight-list-nav date="2016-06-24"}}`);
+    expect(this.$('li')).to.have.length(4);
+    expect(this.$('li:nth-child(1)').text().trim()).to.equal('All');
+    expect(this.$('li:nth-child(2)').text().trim()).to.equal('');
+    expect(this.$('li:nth-child(3)').text().trim()).to.equal('2016-06-24');
+    expect(this.$('li:nth-child(4)').text().trim()).to.equal('');
+  });
+
+  it('shows date in "latest" mode', function() {
+    this.render(hbs`{{flight-list-nav date="2016-06-24" latest=true}}`);
+    expect(this.$('li')).to.have.length(4);
+    expect(this.$('li:nth-child(1)').text().trim()).to.equal('All');
+    expect(this.$('li:nth-child(2)').text().trim()).to.equal('');
+    expect(this.$('li:nth-child(3)').text().trim()).to.equal('2016-06-24');
+    expect(this.$('li:nth-child(4)').text().trim()).to.equal('');
+  });
+
+  it('shows selected airport', function() {
+    this.set('airport', { id: 123, name: 'Meiersberg' });
+
+    this.render(hbs`{{flight-list-nav date=date airport=airport club=club pilot=pilot}}`);
+    expect(this.$('li')).to.have.length(3);
+    expect(this.$('li:nth-child(1)').text().trim()).to.equal('All');
+    expect(this.$('li:nth-child(2)').text().trim()).to.equal('Latest');
+    expect(this.$('li:nth-child(3)').text().trim()).to.equal('Meiersberg');
+  });
+
+  it('shows selected club', function() {
+    this.set('club', { id: 3, name: 'SFN' });
+
+    this.render(hbs`{{flight-list-nav date=date airport=airport club=club pilot=pilot}}`);
+    expect(this.$('li')).to.have.length(3);
+    expect(this.$('li:nth-child(1)').text().trim()).to.equal('All');
+    expect(this.$('li:nth-child(2)').text().trim()).to.equal('Latest');
+    expect(this.$('li:nth-child(3)').text().trim()).to.equal('SFN');
+  });
+
+  it('shows own club', function() {
+    this.set('account.club', { id: 42, name: 'SFZ Aachen' });
+
+    this.render(hbs`{{flight-list-nav date=date airport=airport club=club pilot=pilot}}`);
+    expect(this.$('li')).to.have.length(3);
+    expect(this.$('li:nth-child(1)').text().trim()).to.equal('All');
+    expect(this.$('li:nth-child(2)').text().trim()).to.equal('Latest');
+    expect(this.$('li:nth-child(3)').text().trim()).to.equal('SFZ Aachen');
+  });
+
+  it('shows selected club and own club', function() {
+    this.set('club', { id: 3, name: 'SFN' });
+    this.set('account.club', { id: 42, name: 'SFZ Aachen' });
+
+    this.render(hbs`{{flight-list-nav date=date airport=airport club=club pilot=pilot}}`);
+    expect(this.$('li')).to.have.length(4);
+    expect(this.$('li:nth-child(1)').text().trim()).to.equal('All');
+    expect(this.$('li:nth-child(2)').text().trim()).to.equal('Latest');
+    expect(this.$('li:nth-child(3)').text().trim()).to.equal('SFN');
+    expect(this.$('li:nth-child(4)').text().trim()).to.equal('SFZ Aachen');
+  });
+
+  it('shows club just once if selected club equals own club', function() {
+    this.set('club', { id: 42, name: 'SFZ Aachen' });
+    this.set('account.club', { id: 42, name: 'SFZ Aachen' });
+
+    this.render(hbs`{{flight-list-nav date=date airport=airport club=club pilot=pilot}}`);
+    expect(this.$('li')).to.have.length(3);
+    expect(this.$('li:nth-child(1)').text().trim()).to.equal('All');
+    expect(this.$('li:nth-child(2)').text().trim()).to.equal('Latest');
+    expect(this.$('li:nth-child(3)').text().trim()).to.equal('SFZ Aachen');
+  });
+
+  it('shows selected pilot', function() {
+    this.set('pilot', { id: 5, name: 'foobar' });
+
+    this.render(hbs`{{flight-list-nav date=date airport=airport club=club pilot=pilot}}`);
+    expect(this.$('li')).to.have.length(3);
+    expect(this.$('li:nth-child(1)').text().trim()).to.equal('All');
+    expect(this.$('li:nth-child(2)').text().trim()).to.equal('Latest');
+    expect(this.$('li:nth-child(3)').text().trim()).to.equal('foobar');
+  });
+
+  it('shows own user (and unassigned flights link)', function() {
+    this.set('account.user', { id: 42, name: 'john doe' });
+
+    this.render(hbs`{{flight-list-nav date=date airport=airport club=club pilot=pilot}}`);
+    expect(this.$('li')).to.have.length(4);
+    expect(this.$('li:nth-child(1)').text().trim()).to.equal('All');
+    expect(this.$('li:nth-child(2)').text().trim()).to.equal('Latest');
+    expect(this.$('li:nth-child(3)').text().trim()).to.equal('john doe');
+    expect(this.$('li:nth-child(4)').text().trim()).to.equal('Unassigned');
+  });
+
+  it('shows selected pilot and own user', function() {
+    this.set('pilot', { id: 3, name: 'SFN' });
+    this.set('account.user', { id: 42, name: 'john doe' });
+
+    this.render(hbs`{{flight-list-nav date=date airport=airport club=club pilot=pilot}}`);
+    expect(this.$('li')).to.have.length(5);
+    expect(this.$('li:nth-child(1)').text().trim()).to.equal('All');
+    expect(this.$('li:nth-child(2)').text().trim()).to.equal('Latest');
+    expect(this.$('li:nth-child(3)').text().trim()).to.equal('SFN');
+    expect(this.$('li:nth-child(4)').text().trim()).to.equal('john doe');
+    expect(this.$('li:nth-child(5)').text().trim()).to.equal('Unassigned');
+  });
+
+  it('shows pilot just once if selected pilot equals own user', function() {
+    this.set('pilot', { id: 42, name: 'john doe' });
+    this.set('account.user', { id: 42, name: 'john doe' });
+
+    this.render(hbs`{{flight-list-nav date=date airport=airport club=club pilot=pilot}}`);
+    expect(this.$('li')).to.have.length(4);
+    expect(this.$('li:nth-child(1)').text().trim()).to.equal('All');
+    expect(this.$('li:nth-child(2)').text().trim()).to.equal('Latest');
+    expect(this.$('li:nth-child(3)').text().trim()).to.equal('john doe');
+    expect(this.$('li:nth-child(4)').text().trim()).to.equal('Unassigned');
+  });
+
+  it('shows pinned flights if available', function() {
+    this.set('pinned.pinned', [1, 2, 3]);
+
+    this.render(hbs`{{flight-list-nav date=date airport=airport club=club pilot=pilot}}`);
+    expect(this.$('li')).to.have.length(3);
+    expect(this.$('li:nth-child(1)').text().trim()).to.equal('All');
+    expect(this.$('li:nth-child(2)').text().trim()).to.equal('Latest');
+    expect(this.$('li:nth-child(3)').text().trim()).to.equal('Pinned');
+  });
+});
