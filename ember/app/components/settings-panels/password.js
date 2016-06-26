b974db48970ca2865a7743510ddcbadd273d67e6
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-06-26 22:07:59 +0200

    components/settings-panel/password: Fix ESLint issue

--- ember/app/components/settings-panels/password.js
+++ ember/app/components/settings-panels/password.js
@@ -1,5 +1,4 @@
 import Ember from 'ember';
-import { ForbiddenError } from 'ember-ajax/errors';
 import { validator, buildValidations } from 'ember-cp-validations';
 
 const Validations = buildValidations({
