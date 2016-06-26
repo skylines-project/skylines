0d5691d8013d378c37b931c147126d0d381dc185
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-06-26 21:41:32 +0200

    routes/flight: Remove obsolete beforeModel() hack

--- ember/app/routes/flight.js
+++ ember/app/routes/flight.js
@@ -1,12 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  beforeModel(transition) {
-    let id = transition.params && transition.params.flight && transition.params.flight.flight_id;
-    if (!id.match(/\d+/)) {
-      return this.transitionTo('flights.list', id);
-    }
-  },
-
   model() {},
 });
