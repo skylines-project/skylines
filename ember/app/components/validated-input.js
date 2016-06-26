9227b96b32ce2a1a0261dba531a396f6dfe30caf
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-06-26 09:58:01 +0200

    Add "validated-input" component

---
+++ ember/app/components/validated-input.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['form-group has-feedback'],
+  classNameBindings: ['showErrorClass:has-error', 'isValid:has-success'],
+
+  type: 'text',
+  value: null,
+  validation: null,
+  label: null,
+
+  notValidating: Ember.computed.not('validation.isValidating'),
+  didValidate: Ember.computed.readOnly('targetObject.didValidate'),
+  hasContent: Ember.computed.notEmpty('value'),
+  isValid: Ember.computed.and('hasContent', 'validation.isValid', 'notValidating'),
+  isInvalid: Ember.computed.readOnly('validation.isInvalid'),
+  showErrorClass: Ember.computed.and('notValidating', 'showMessage', 'hasContent', 'validation'),
+  showMessage: Ember.computed('validation.isDirty', 'isInvalid', 'didValidate', function() {
+    return (this.get('validation.isDirty') || this.get('didValidate')) && this.get('isInvalid');
+  }),
+});
