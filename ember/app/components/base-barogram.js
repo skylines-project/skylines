e12850d6948be614b99499893044c03e9e26c5d9
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-06-30 18:59:18 +0200

    components/base-barogram: Move upload specific code into upload-barogram

--- ember/app/components/base-barogram.js
+++ ember/app/components/base-barogram.js
@@ -55,23 +55,6 @@ export default Ember.Component.extend(Ember.Evented, {
     opt.selection.mode = 'x';
   },
 
-  setFlightTimes(takeoff, scoring_start, scoring_end, landing) {
-    this.get('flot').setSelection({
-      takeoff: takeoff * 1000,
-      scoring_start: scoring_start * 1000,
-      scoring_end: scoring_end * 1000,
-      landing: landing * 1000,
-    });
-  },
-
-  updateFlightTime(time, field) {
-    this.get('flot').updateSelection(time * 1000, field);
-  },
-
-  getFlightTime() {
-    return this.get('flot').getSelection();
-  },
-
   hoverModeObserver: Ember.observer('hoverMode', function() {
     Ember.run.once(this, 'onHoverModeUpdate');
   }),
--- ember/app/components/upload-barogram.js
+++ ember/app/components/upload-barogram.js
@@ -9,4 +9,21 @@ export default BarogramComponent.extend({
     this._super(...arguments);
     window[`barogram-${this.get('prefix')}`] = this;
   },
+
+  setFlightTimes(takeoff, scoring_start, scoring_end, landing) {
+    this.get('flot').setSelection({
+      takeoff: takeoff * 1000,
+      scoring_start: scoring_start * 1000,
+      scoring_end: scoring_end * 1000,
+      landing: landing * 1000,
+    });
+  },
+
+  updateFlightTime(time, field) {
+    this.get('flot').updateSelection(time * 1000, field);
+  },
+
+  getFlightTime() {
+    return this.get('flot').getSelection();
+  },
 });
