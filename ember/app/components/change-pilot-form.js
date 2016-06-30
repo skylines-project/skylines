eac9e1e27a7928a1aa702fbaf933bd6b728a8db4
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-06-30 22:25:15 +0200

    components/change-pilot-form: Extract "pilot-select" component

--- ember/app/components/change-pilot-form.js
+++ ember/app/components/change-pilot-form.js
@@ -43,42 +43,8 @@ export default Ember.Component.extend(Validations, {
   pending: false,
   error: null,
 
-  pilots: Ember.computed('account.user', 'account.club', 'clubMembers.[]', function() {
-    let pilots = [{ id: null }, this.get('account.user')];
-    if (this.get('account.club')) {
-      pilots.push({ groupName: this.get('account.club.name'), options: this.get('clubMembers') });
-    }
-    return pilots;
-  }),
-
-  pilot: Ember.computed('pilotId', {
-    get() {
-      return this.findPilot(this.get('pilotId'));
-    },
-    set(key, value) {
-      this.set('pilotId', value.id);
-      return value;
-    },
-  }),
-
   showPilotNameInput: Ember.computed.equal('pilotId', null),
-
-  copilot: Ember.computed('copilotId', {
-    get() {
-      return this.findPilot(this.get('copilotId'));
-    },
-    set(key, value) {
-      this.set('copilotId', value.id);
-      return value;
-    },
-  }),
-
   showCopilotNameInput: Ember.computed.equal('copilotId', null),
-
-  findPilot(id) {
-    let pilots = this.get('pilots');
-    return pilots.findBy('id', id) || pilots.get('lastObject.options').findBy('id', id);
-  },
 
   sendChangeRequest() {
     let id = this.get('flightId');
---
+++ ember/app/components/pilot-select.js
@@ -0,0 +1,33 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  account: Ember.inject.service(),
+
+  tagName: '',
+
+  clubMembers: [],
+  pilotId: null,
+
+  pilots: Ember.computed('account.user', 'account.club', 'clubMembers.[]', function() {
+    let pilots = [{ id: null }, this.get('account.user')];
+    if (this.get('account.club')) {
+      pilots.push({ groupName: this.get('account.club.name'), options: this.get('clubMembers') });
+    }
+    return pilots;
+  }),
+
+  pilot: Ember.computed('pilotsWithNull.[]', 'pilotId', function() {
+    return this.findPilot(this.get('pilotId'));
+  }),
+
+  findPilot(id) {
+    let pilots = this.get('pilots');
+    return pilots.findBy('id', id) || pilots.get('lastObject.options').findBy('id', id);
+  },
+
+  actions: {
+    onChange(pilot) {
+      this.get('onChange')(pilot.id);
+    },
+  },
+});
