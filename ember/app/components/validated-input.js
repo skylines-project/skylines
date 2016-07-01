cb046817bce969c8489beed5f678836e57ec99a4
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-07-01 19:36:13 +0200

    Extracted "validated-block" component

--- ember/app/components/validated-input.js
+++ ember/app/components/validated-input.js
@@ -1,21 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['form-group has-feedback'],
-  classNameBindings: ['showErrorClass:has-error', 'isValid:has-success'],
+  tagName: '',
 
   type: 'text',
   value: null,
-  validation: null,
-  label: null,
-
-  notValidating: Ember.computed.not('validation.isValidating'),
-  didValidate: Ember.computed.readOnly('targetObject.didValidate'),
-  hasContent: Ember.computed.notEmpty('value'),
-  isValid: Ember.computed.and('hasContent', 'validation.isValid', 'notValidating'),
-  isInvalid: Ember.computed.readOnly('validation.isInvalid'),
-  showErrorClass: Ember.computed.and('notValidating', 'showMessage', 'hasContent', 'validation'),
-  showMessage: Ember.computed('validation.isDirty', 'isInvalid', 'didValidate', function() {
-    return (this.get('validation.isDirty') || this.get('didValidate')) && this.get('isInvalid');
-  }),
 });
