d6a522f3c9f02e73dd5e7bb52d7b3b6136e7ec2a
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-07-06 18:57:47 +0200

    routes/flight: Parse "flight_ids" and save IDs array into model

--- ember/app/routes/flight.js
+++ ember/app/routes/flight.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {},
+  model(params) {
+    let ids = params.flight_ids.split(',').map(it => parseInt(it, 10));
+    return { ids };
+  },
 });
