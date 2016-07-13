1de45f1217315439a9315f1cce09794eb310bb31
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-07-13 19:24:47 +0200

    components/flight-list: Add "table-flights" CSS class

--- ember/app/components/flight-list.js
+++ ember/app/components/flight-list.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'table',
-  classNames: ['table', 'table-striped', 'table-condensed'],
+  classNames: ['table', 'table-striped', 'table-condensed', 'table-flights'],
 
   flights: [],
   showDate: true,
