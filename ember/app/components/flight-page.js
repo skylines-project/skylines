f001d439defd5744097a0cfa8ca2070fd492013e
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-08-04 21:16:47 +0200

    templates/flights/map: Move sidebar handling code into flight-page component

--- ember/app/components/flight-page.js
+++ ember/app/components/flight-page.js
@@ -6,6 +6,24 @@ export default Ember.Component.extend({
 
   classNames: ['olFullscreen'],
 
+  didInsertElement() {
+    let sidebar = this.$('#sidebar').sidebar();
+
+    this.$('#barogram_panel').resize(() => {
+      let height = this.$('#barogram_panel').height() + 10;
+      sidebar.css('bottom', height);
+      this.$('.ol-scale-line').css('bottom', height);
+      this.$('.ol-attribution').css('bottom', height);
+    });
+
+    if (window.location.hash &&
+      sidebar.find(`li > a[href="#${window.location.hash.substring(1)}"]`).length != 0) {
+      sidebar.open(window.location.hash.substring(1));
+    }
+
+    window.paddingFn = () => [20, 20, this.$('#barogram_panel').height() + 20, sidebar.width() + 20];
+  },
+
   actions: {
     selectWingman(id) {
       let fixCalc = this.get('fixCalc');
