a2e24d876ea66ac31c57de8d1c37b3196acdb9c9
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-09-27 21:37:46 +0200

    components/flight-map: Add "onExtendChange" and "onCesiumEnabledChange" actions

--- ember/app/components/flight-map.js
+++ ember/app/components/flight-map.js
@@ -7,12 +7,28 @@ export default BaseMapComponent.extend({
 
   flights: Ember.computed.readOnly('fixCalc.flights'),
 
+  didInsertElement() {
+    this._super(...arguments);
+    this.get('map').on('moveend', this._handleMoveEnd, this);
+  },
+
+  willDestroyElement() {
+    this._super(...arguments);
+    this.get('map').off('moveend', this._handleMoveEnd, this);
+  },
+
+  _handleMoveEnd(event) {
+    this.getWithDefault('onExtentChange', Ember.K)(event.frameState.extent);
+  },
+
   actions: {
     cesiumEnabled() {
       this.set('cesiumEnabled', true);
+      this.getWithDefault('onCesiumEnabledChange', Ember.K)(true);
     },
     cesiumDisabled() {
       this.set('cesiumEnabled', false);
+      this.getWithDefault('onCesiumEnabledChange', Ember.K)(false);
     },
   },
 });
