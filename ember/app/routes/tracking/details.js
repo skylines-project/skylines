c8094df7ec541b4618bf36cc0076523d77345084
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-09-29 21:12:55 +0200

    routes/tracking/details: Replace ember-islands with AJAX call

--- ember/app/routes/tracking/details.js
+++ ember/app/routes/tracking/details.js
@@ -1,5 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {},
+  ajax: Ember.inject.service(),
+
+  model({ user_ids }) {
+    return this.get('ajax').request(`/tracking/${user_ids}/`);
+  },
 });
