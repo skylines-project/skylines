c74ea3ec599cdbfc1eec114868999315711192f9
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-09-29 22:18:22 +0200

    components/flight-page: Move sync ajax call into route model hook

--- ember/app/routes/flight/index.js
+++ ember/app/routes/flight/index.js
@@ -1,15 +1,25 @@
 import Ember from 'ember';
+import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
   ajax: Ember.inject.service(),
+  fixCalc: Ember.inject.service(),
 
   model() {
+    let ajax = this.get('ajax');
     let id = this.modelFor('flight').ids[0];
-    return this.get('ajax').request(`/flights/${id}/?extended`);
+
+    return RSVP.hash({
+      data: ajax.request(`/flights/${id}/?extended`),
+      path: ajax.request(`/flights/${id}/json`),
+    });
   },
 
-  setupController(controller) {
+  setupController(controller, model) {
     this._super(...arguments);
     controller.set('ids', this.modelFor('flight').ids);
+    controller.set('model', model.data);
+
+    this.get('fixCalc').addFlight(model.path);
   },
 });
