a61bf06321dc739fe15545d1635b582f63d39e6a
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-10-12 18:33:52 +0200

    schemas/validate: Add EmptyOr validator

--- skylines/schemas/validate.py
+++ skylines/schemas/validate.py
@@ -5,3 +5,12 @@ class NotEmpty(Validator):
     def __call__(self, value):
         if len(value) == 0:
             raise ValidationError('Must not be empty.')
+
+
+class EmptyOr(Validator):
+    def __init__(self, other_validator):
+        self.other_validator = other_validator
+
+    def __call__(self, value):
+        if len(value) != 0:
+            self.other_validator(value)
