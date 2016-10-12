2b9218e817975f914d54fa38803ac924b79fbd5d
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-10-12 19:27:37 +0200

    Install "ember-cli-deploy" and related addons

---
+++ ember/config/deploy.js
@@ -0,0 +1,37 @@
+/* jshint node: true */
+
+module.exports = function(deployTarget) {
+  var ENV = {
+    build: {},
+    // include other plugin configuration that applies to all deploy targets here
+    'revision-data': {
+      type: 'git-commit',
+    },
+  };
+
+  if (deployTarget === 'development') {
+    ENV.build.environment = 'development';
+    // configure other plugins for development deploy target here
+  }
+
+  if (deployTarget === 'staging') {
+    ENV.build.environment = 'production';
+    // configure other plugins for staging deploy target here
+  }
+
+  if (deployTarget === 'production') {
+    ENV.build.environment = 'production';
+    // configure other plugins for production deploy target here
+    ENV['rsync-assets'] = {
+      destination: 'skylines@skylines.aero:/home/skylines/src/skylines/frontend/static',
+      flags: ['z'],
+      ssh: true,
+      privateKeyPath: process.env['PRIVATE_KEY_PATH'],
+    };
+  }
+
+  // Note: if you need to build some configuration asynchronously, you can return
+  // a promise that resolves with the ENV object instead of returning the
+  // ENV object synchronously.
+  return ENV;
+};
