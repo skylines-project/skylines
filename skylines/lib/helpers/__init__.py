ec8a392b642b9aaf2fa3a8a65cad2d345b9cdcc3
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-10-15 22:57:04 +0200

    helpers: Remove unused helpers

--- skylines/lib/helpers/__init__.py
+++ skylines/lib/helpers/__init__.py
@@ -6,29 +6,7 @@
 
 from __future__ import absolute_import
 
-import datetime
-from urllib import urlencode
-
 from flask import flash
-from webhelpers import date, feedgenerator, html, number, misc, text
 
 from .country import *
 from skylines.lib.formatter.units import get_setting_name
-from skylines.lib.datetime import to_seconds_of_day
-from skylines.frontend.views.upload import UploadStatus
-
-html_formatter = HtmlFormatter(style='tango', nowrap=True)
-
-# Jinja2 doesn't seem to have min/max... strange!
-min = min
-max = max
-
-
-def url(base_url='/', **params):
-    if not isinstance(base_url, basestring) and hasattr(base_url, '__iter__'):
-        base_url = '/'.join(base_url)
-
-    if params:
-        return '?'.join((base_url, urlencode(params)))
-
-    return base_url
