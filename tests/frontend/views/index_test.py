c70724a123012fdd45b6697037841035f1c0287e
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-10-15 21:10:11 +0200

    tests/frontend: Remove obsolete "index" test

--- tests/frontend/views/index_test.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-def test_index(browser):
-    """ The front page is show the about page """
-    browser.open('/')
-
-    assert 'Welcome to SkyLines' in browser.contents
