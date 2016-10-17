f09a6a45a1013fb5815d5adfb7b312181e9f87ed
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-10-17 21:56:42 +0200

    instance-initializers: Move setLocale() call to application route

--- ember/app/instance-initializers/init.js
+++ ember/app/instance-initializers/init.js
@@ -1,12 +1,7 @@
-import Ember from 'ember';
-
 export function initialize(appInstance) {
   // boot up services
   appInstance.lookup('service:account');
   appInstance.lookup('service:pinned-flights');
-
-  appInstance.lookup('service:intl')
-    .setLocale([Ember.$('meta[name=skylines-locale]').attr('content'), 'en']);
 }
 
 export default {
--- ember/app/routes/application.js
+++ ember/app/routes/application.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   account: Ember.inject.service(),
+  intl: Ember.inject.service(),
+
+  beforeModel() {
+    this.get('intl').setLocale([Ember.$('meta[name=skylines-locale]').attr('content'), 'en']);
+  },
 
   activate() {
     this._super(...arguments);
