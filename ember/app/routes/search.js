4c02060ab968e4e91973346df70ebfd51c9ff401
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-10-18 18:43:34 +0200

    templates/topbar: Convert search form to Ember

--- ember/app/routes/search.js
+++ ember/app/routes/search.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   ajax: Ember.inject.service(),
+  searchTextService: Ember.inject.service('searchText'),
 
   queryParams: {
     text: {
@@ -11,9 +12,16 @@ export default Ember.Route.extend({
 
   model(params, transition) {
     let searchText = transition.queryParams && transition.queryParams.text;
+    this.set('searchTextService.text', searchText);
+
     if (searchText) {
       return this.get('ajax').request(`/search/?text=${searchText}`);
     }
+  },
+
+  deactive() {
+    this._super(...arguments);
+    this.set('searchTextService.text', null);
   },
 
   actions: {
