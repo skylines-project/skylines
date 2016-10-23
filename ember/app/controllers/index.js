655e710a11df53654b4a1efebfc3a33aebbcdb95
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-10-23 20:42:42 +0200

    templates/index: Use "href-to" helper

--- ember/app/controllers/index.js
+++ ember/app/controllers/index.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   account: Ember.inject.service(),
 
-  notificationsURL: Ember.computed('account.user', function() {
-    return this.get('account.user') ? '/notifications' : '/timeline';
+  notificationsTarget: Ember.computed('account.user', function() {
+    return this.get('account.user') ? 'notifications' : 'timeline';
   }),
 });
