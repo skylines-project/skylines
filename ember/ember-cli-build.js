8925de30b2c75dd5a105be2a9d953863825a3a91
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-10-25 21:43:41 +0200

    Bower: Remove obsolete "jQuery-ajaxTransport-XDomainRequest" dependency

--- ember/ember-cli-build.js
+++ ember/ember-cli-build.js
@@ -31,8 +31,6 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  app.import('bower_components/jQuery-ajaxTransport-XDomainRequest/jquery.xdomainrequest.min.js');
-
   app.import('vendor/openlayers/ol3cesium.js');
   app.import('vendor/openlayers/ol.css');
 
