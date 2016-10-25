c4f571b650c342b5a7e570caff06d605091b475e
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-10-25 18:14:57 +0200

    routes/login: Redirect to "index" route after login on login page

--- ember/app/routes/login.js
+++ ember/app/routes/login.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Ember.Route.extend(UnauthenticatedRouteMixin);
+export default Ember.Route.extend(UnauthenticatedRouteMixin, {
+  setupController() {
+    this._super(...arguments);
+    this.controllerFor('application').set('inLoginRoute', true);
+  },
+
+  resetController() {
+    this._super(...arguments);
+    this.controllerFor('application').set('inLoginRoute', false);
+  },
+});
