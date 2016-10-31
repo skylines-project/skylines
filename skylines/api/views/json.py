295be8958b51afe1ba16ff3eecb7ee1cb7925c65
Tobias Bieniek <tobias.bieniek@qsc.de>
2016-10-31 20:36:22 +0100

    api/views/json: Remove JSONP option

--- skylines/api/views/json.py
+++ skylines/api/views/json.py
@@ -10,21 +10,7 @@ def jsonify(data, status=200, headers=None):
     if current_app.config['JSONIFY_PRETTYPRINT_REGULAR'] and not request.is_xhr:
         indent = 2
 
-    # Determine if this is a JSONP request
-    callback = request.args.get('callback', False)
-    if callback:
-        content = str(callback) + '(' + json.dumps({
-            'meta': {
-                'status': status,
-            },
-            'data': data,
-        }, indent=indent) + ')'
-        mimetype = 'application/javascript'
-        status = 200
-
-    else:
-        content = json.dumps(data, indent=indent)
-        mimetype = 'application/json'
+    content = json.dumps(data, indent=indent)
 
     return current_app.response_class(
-        content, mimetype=mimetype, headers=headers), status
+        content, mimetype='application/json', headers=headers), status
