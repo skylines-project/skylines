e251a8a8f7a40cc2a739932d2efed01aca104f9d
pyrog <pyrog@laposte.net>
2016-11-15 22:43:42 +0100

    Display symbol according to aircraft model

--- ember/app/components/plane-icons-composer.js
+++ ember/app/components/plane-icons-composer.js
@@ -14,7 +14,7 @@ export default Ember.Component.extend({
   init() {
     this._super(...arguments);
 
-    let style = new ol.style.Icon({
+    let glider = new ol.style.Icon({
       anchor: [0.5, 0.5],
       anchorXUnits: 'fraction',
       anchorYUnits: 'fraction',
@@ -23,10 +23,53 @@ export default Ember.Component.extend({
       rotation: 0,
       rotateWithView: true,
     });
+    let motorglider = new ol.style.Icon({
+      anchor: [0.5, 0.5],
+      anchorXUnits: 'fraction',
+      anchorYUnits: 'fraction',
+      size: [40, 24],
+      src: '/images/motorglider_symbol.png',
+      rotation: 0,
+      rotateWithView: true,
+    });
+    let paraglider = new ol.style.Icon({
+      anchor: [0.5, 0.5],
+      anchorXUnits: 'fraction',
+      anchorYUnits: 'fraction',
+      size: [40, 24],
+      src: '/images/paraglider_symbol.png',
+      rotation: 0,
+      rotateWithView: true,
+    });
+    let hangglider = new ol.style.Icon({
+      anchor: [0.5, 0.5],
+      anchorXUnits: 'fraction',
+      anchorYUnits: 'fraction',
+      size: [40, 14],
+      src: '/images/hangglider_symbol.png',
+      rotation: 0,
+      rotateWithView: true,
+    });
+    // TODO: draw the symbol and update src property
+    let ul = new ol.style.Icon({
+      anchor: [0.5, 0.5],
+      anchorXUnits: 'fraction',
+      anchorYUnits: 'fraction',
+      size: [24, 24],
+      src: '/images/logo.png',
+      rotation: 0,
+      rotateWithView: true,
+    });
 
-    style.load();
+    let styles = { glider, motorglider, paraglider, hangglider, ul };
+    // TODO: use a loop to load the styles
+    styles.glider.load();
+    styles.motorglider.load();
+    styles.paraglider.load();
+    styles.hangglider.load();
+    styles.ul.load();
 
-    this.set('style', style);
+    this.set('styles', styles);
   },
 
   didInsertElement() {
@@ -42,11 +85,15 @@ export default Ember.Component.extend({
   },
 
   renderIcons(context) {
-    let style = this.get('style');
+    let styles = this.get('styles');
 
     this.get('fixes').forEach(fix => {
       let point = fix.get('pointXY');
+
       if (point) {
+        let type = fix.flight.model && fix.flight.model.type || 'glider';
+        let style = styles[type];
+
         style.setRotation(fix.get('heading'));
         context.setImageStyle(style);
         context.drawPointGeometry(point);
