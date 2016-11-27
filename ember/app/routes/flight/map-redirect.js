017701a205af3ac3bc2c9cf6baedf3797af157fc
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-11-27 14:06:03 +0100

    Use redirectTo() instead of transitionTo() in "redirect" hooks

--- ember/app/routes/flight/map-redirect.js
+++ ember/app/routes/flight/map-redirect.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   redirect() {
-    this.transitionTo('flight.index');
+    this.redirectTo('flight.index');
   },
 });
--- ember/app/routes/settings/index.js
+++ ember/app/routes/settings/index.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   redirect() {
-    this.transitionTo('settings.profile');
+    this.redirectTo('settings.profile');
   },
 });
