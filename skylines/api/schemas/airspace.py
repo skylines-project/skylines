bda5bb3f8f68a047225ec13a469cee915613e7ef
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-12-22 13:50:57 +0100

    api/schemas/airspace: Simplify "_class" attribute renaming

--- skylines/api/schemas/airspace.py
+++ skylines/api/schemas/airspace.py
@@ -1,20 +1,13 @@
-from marshmallow import post_dump
-
-from skylines.api.schemas.base import replace_keywords
 from skylines.schemas import Schema, fields
 
 
 class AirspaceSchema(Schema):
     id = fields.Integer()
     name = fields.String()
-    _class = fields.String(attribute='airspace_class')
+    _class = fields.String(attribute='airspace_class', dump_to='class')
     top = fields.String()
     base = fields.String()
     shape = fields.GeometryField(attribute='the_geom')
     country = fields.String(attribute='country_code')
     created_at = fields.DateTime(attribute='time_created')
     modified_at = fields.DateTime(attribute='time_modified')
-
-    @post_dump
-    def replace_keywords(self, data):
-        return replace_keywords(data)
