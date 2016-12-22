83aaefb9a5b016994c19ba4c0917db3bc53c85a7
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-12-22 13:01:03 +0100

    api: Remove unused "user" schema

--- skylines/api/schemas/__init__.py
+++ skylines/api/schemas/__init__.py
@@ -1,18 +1,10 @@
 from .airspace import AirspaceSchema
-from .user import UserSchema, AuthenticatedUserSchema
 from .wave import WaveSchema
 
 __all__ = [
     AirspaceSchema,
-    UserSchema,
-    AuthenticatedUserSchema,
     WaveSchema,
 ]
 
 airspace_list_schema = AirspaceSchema(only=('name', '_class', 'top', 'base', 'country'))
-
-user_schema = UserSchema()
-user_list_schema = UserSchema(only=('id', 'name', 'first_name', 'last_name'))
-current_user_schema = AuthenticatedUserSchema()
-
 wave_list_schema = WaveSchema()
