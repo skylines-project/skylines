55cd1ab49f3efcbac91d40eb9b24ca876b77d0af
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-02-06 20:08:48 +0100

    tests/api: Add more "POST /clubs/:id" tests

--- tests/api/views/clubs/update_test.py
+++ tests/api/views/clubs/update_test.py
@@ -49,6 +49,41 @@ def test_update_without_authentication(db_session, client):
     assert club.website == None
 
 
+def test_non_json_data(db_session, client, test_user):
+    sfn = clubs.sfn()
+    test_user.club = sfn
+    add_fixtures(db_session, sfn)
+
+    res = client.post('/clubs/{id}'.format(id=sfn.id), headers=auth_for(test_user), data='foobar?')
+    assert res.status_code == 400
+    assert res.json['error'] == 'invalid-request'
+
+
+def test_invalid_data(db_session, client, test_user):
+    sfn = clubs.sfn()
+    test_user.club = sfn
+    add_fixtures(db_session, sfn)
+
+    res = client.post('/clubs/{id}'.format(id=sfn.id), headers=auth_for(test_user), json={
+        'name': '',
+    })
+    assert res.status_code == 422
+    assert res.json['error'] == 'validation-failed'
+
+
+def test_existing_club(db_session, client, test_user):
+    lva = clubs.lva()
+    sfn = clubs.sfn()
+    test_user.club = sfn
+    add_fixtures(db_session, lva)
+
+    res = client.post('/clubs/{id}'.format(id=sfn.id), headers=auth_for(test_user), json={
+        'name': 'LV Aachen',
+    })
+    assert res.status_code == 422
+    assert res.json['error'] == 'duplicate-club-name'
+
+
 def test_missing(db_session, client, test_user):
     res = client.post('/clubs/1000000000', headers=auth_for(test_user), json={
         'name': 'foobar',
