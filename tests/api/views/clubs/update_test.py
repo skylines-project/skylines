e62a6f3db38f754e8a7cf4915ffc1641c8d70137
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-02-06 19:07:55 +0100

    tests/api: Add "POST /clubs/:id" tests

---
+++ tests/api/views/clubs/update_test.py
@@ -0,0 +1,71 @@
+from skylines.model import Club
+from tests.api import basic_auth
+from tests.data import add_fixtures, clubs
+
+
+def test_update(db_session, client, test_user):
+    sfn = clubs.sfn()
+    test_user.club = sfn
+    add_fixtures(db_session, sfn)
+
+    headers = basic_auth(test_user.email_address, test_user.original_password)
+
+    res = client.post('/clubs/{id}'.format(id=sfn.id), headers=headers, json={
+        'name': 'foobar',
+        'website': 'https://foobar.de',
+    })
+    assert res.status_code == 200
+
+    club = Club.get(sfn.id)
+    assert club.name == 'foobar'
+    assert club.website == 'https://foobar.de'
+
+
+def test_update_without_permission(db_session, client, test_user):
+    sfn = clubs.sfn()
+    add_fixtures(db_session, sfn)
+
+    headers = basic_auth(test_user.email_address, test_user.original_password)
+
+    res = client.post('/clubs/{id}'.format(id=sfn.id), headers=headers, json={
+        'name': 'foobar',
+        'website': 'https://foobar.de',
+    })
+    assert res.status_code == 403
+
+    club = Club.get(sfn.id)
+    assert club.name == 'Sportflug Niederberg'
+    assert club.website == None
+
+
+def test_update_without_authentication(db_session, client):
+    sfn = clubs.sfn()
+    add_fixtures(db_session, sfn)
+
+    res = client.post('/clubs/{id}'.format(id=sfn.id), json={
+        'name': 'foobar',
+        'website': 'https://foobar.de',
+    })
+    assert res.status_code == 401
+
+    club = Club.get(sfn.id)
+    assert club.name == 'Sportflug Niederberg'
+    assert club.website == None
+
+
+def test_missing(db_session, client, test_user):
+    headers = basic_auth(test_user.email_address, test_user.original_password)
+    res = client.post('/clubs/1000000000', headers=headers, json={
+        'name': 'foobar',
+        'website': 'https://foobar.de',
+    })
+    assert res.status_code == 404
+
+
+def test_invalid_id(db_session, client, test_user):
+    headers = basic_auth(test_user.email_address, test_user.original_password)
+    res = client.post('/clubs/abc', headers=headers, json={
+        'name': 'foobar',
+        'website': 'https://foobar.de',
+    })
+    assert res.status_code == 404
