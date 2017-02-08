890cdaa7d40729990d554073126273d180daf20d
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-02-08 21:05:13 +0100

    tests/api: Add another "GET /flights/:id" test

--- tests/api/views/flights/read_test.py
+++ tests/api/views/flights/read_test.py
@@ -50,3 +50,58 @@ def test_basic_flight(db_session, client):
             },
         }
     }
+
+
+def test_filled_flight(db_session, client):
+    lva = clubs.lva()
+    john = users.john(club=lva)
+    flight = flights.filled(pilot=john, co_pilot_name='jane', club=lva, igc_file=igcs.filled(owner=john))
+    add_fixtures(db_session, flight)
+
+    res = client.get('/flights/{id}'.format(id=flight.id))
+    assert res.status_code == 200
+    print res.json
+    assert res.json == {
+        u'flight': {
+            u'id': flight.id,
+            u'timeCreated': u'2016-12-30T11:23:45+00:00',
+            u'pilot': {
+                u'id': john.id,
+                u'name': u'John Doe'
+            },
+            u'pilotName': None,
+            u'copilot': None,
+            u'copilotName': u'jane',
+            u'club': {
+                u'id': lva.id,
+                u'name': u'LV Aachen',
+            },
+            u'model': None,
+            u'registration': u'D-1234',
+            u'competitionId': u'701',
+            u'scoreDate': u'2011-06-18',
+            u'takeoffTime': u'2016-12-30T11:12:23+00:00',
+            u'scoreStartTime': u'2016-12-30T11:17:23+00:00',
+            u'scoreEndTime': u'2016-12-30T16:04:40+00:00',
+            u'landingTime': u'2016-12-30T16:15:40+00:00',
+            u'takeoffAirport': None,
+            u'landingAirport': None,
+            u'distance': 512,
+            u'triangleDistance': 432,
+            u'rawScore': 799.0,
+            u'score': 799.0,
+            u'speed': None,
+            u'privacyLevel': 0,
+            u'igcFile': {
+                u'owner': {
+                    u'id': john.id,
+                    u'name': u'John Doe'
+                },
+                u'filename': u'abc1234d.igc',
+                u'registration': u'D-4449',
+                u'competitionId': u'TH',
+                u'model': u'Hornet',
+                u'date': u'2017-01-15',
+            },
+        }
+    }
--- tests/data/igcs.py
+++ tests/data/igcs.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime
 
 from skylines.model import IGCFile
 
@@ -9,5 +9,21 @@ def simple(owner, **kwargs):
         filename='simple.igc',
         md5='ebc87aa50aec6a6667e1c9251a68a90e',
         date_utc=date(2011, 6, 18),
+        **kwargs
+    )
+
+
+def filled(owner, **kwargs):
+    return IGCFile(
+        owner=owner,
+        time_created=datetime(2017, 1, 15, 21, 23, 40),
+        filename='abc1234d.igc',
+        md5='12345aa50aec6a6667e1c9251a68a90e',
+        logger_id='GC1',
+        logger_manufacturer_id='FLA',
+        registration='D-4449',
+        competition_id='TH',
+        model='Hornet',
+        date_utc=date(2017, 1, 15),
         **kwargs
     )
