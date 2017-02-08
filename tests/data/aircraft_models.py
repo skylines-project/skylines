12326811a9ac9e8956b2b7d71d7a7fe6ae0cdbdf
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-02-08 21:16:17 +0100

    tests/api/aircraft_models: Extract fixtures into "tests/data" files

--- tests/data/aircraft_models.py
+++ tests/data/aircraft_models.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 from skylines.model import AircraftModel
 
 
@@ -7,5 +9,56 @@ def hornet(**kwargs):
         kind=1,
         igc_index=100,
         dmst_index=100,
+        **kwargs
+    )
+
+
+def nimeta(**kwargs):
+    return AircraftModel(
+        name='Nimeta',
+        kind=1,
+        igc_index=142,
+        dmst_index=100,
+        **kwargs
+    )
+
+
+def ask13(**kwargs):
+    return AircraftModel(
+        name='ASK 13',
+        igc_index=42,
+        dmst_index=17,
+        **kwargs
+    )
+
+
+def dimona(**kwargs):
+    return AircraftModel(
+        name='Dimona',
+        kind=2,
+        **kwargs
+    )
+
+
+def epsilon(**kwargs):
+    return AircraftModel(
+        name='EPSILON',
+        kind=3,
+        **kwargs
+    )
+
+
+def delta(**kwargs):
+    return AircraftModel(
+        name=u'Δ',
+        kind=4,
+        **kwargs
+    )
+
+
+def falcon9(**kwargs):
+    return AircraftModel(
+        name='Falcon 9',
+        kind=5,
         **kwargs
     )
