bcc40b08c59ba8fcb8efc9044c2ea6e11ed9df12
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-02-11 22:52:22 +0100

    tests/api: Add more "GET /users" tests

--- tests/api/views/users/list_test.py
+++ tests/api/views/users/list_test.py
@@ -1,4 +1,4 @@
-from tests.data import add_fixtures, users
+from tests.data import add_fixtures, users, clubs
 
 
 def test_list_users(db_session, client):
@@ -12,5 +12,42 @@ def test_list_users(db_session, client):
             u'id': john.id,
             u'name': u'John Doe',
             u'club': None,
+        }]
+    }
+
+
+def test_with_club(db_session, client):
+    john = users.john(club=clubs.lva())
+    add_fixtures(db_session, john)
+
+    res = client.get('/users')
+    assert res.status_code == 200
+    assert res.json == {
+        u'users': [{
+            u'id': john.id,
+            u'name': u'John Doe',
+            u'club': {
+                u'id': john.club.id,
+                u'name': u'LV Aachen',
+            },
+        }]
+    }
+
+
+def test_with_club_parameter(db_session, client):
+    john = users.john(club=clubs.lva())
+    add_fixtures(db_session, john, users.jane(), users.max())
+
+    res = client.get('/users')
+    assert res.status_code == 200
+    assert len(res.json['users']) == 3
+
+    res = client.get('/users?club={club}'.format(club=john.club.id))
+    assert res.status_code == 200
+    assert len(res.json['users']) == 1
+    assert res.json == {
+        u'users': [{
+            u'id': john.id,
+            u'name': u'John Doe',
         }]
     }
