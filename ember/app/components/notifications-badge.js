356cdefe09f20e5db41c5ad82883a0b41e933603
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-09-02 13:35:22 +0200

    styles: Fix notifications badge styling

--- ember/app/components/notifications-badge.js
+++ ember/app/components/notifications-badge.js
@@ -2,8 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   notifications: Ember.inject.service(),
+  intl: Ember.inject.service(),
 
   tagName: 'span',
   classNames: ['badge'],
   classNameBindings: ['notifications.hasUnread:badge-warning'],
+  attributeBindings: ['title'],
+
+  title: Ember.computed('intl.locale', function() {
+    return this.get('intl').t('notifications');
+  }),
 });
