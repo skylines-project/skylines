2d03ee6654a0a71e5214b958160ceeff1e605541
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-03-25 20:45:50 +0200

    routes/application: Add missing `raven` service injection

--- ember/app/routes/application.js
+++ ember/app/routes/application.js
@@ -14,6 +14,7 @@ export default Route.extend(ApplicationRouteMixin, {
   ajax: service(),
   cookies: service(),
   intl: service(),
+  raven: service(),
   session: service(),
   units: service(),
 
