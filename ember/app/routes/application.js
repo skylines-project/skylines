8637b785bcfede84348f56f9533bfe2ccd0266f0
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-05-19 15:38:29 +0200

    services/intl: Load Intl.js polyfill conditionally on app startup

--- ember/app/routes/application.js
+++ ember/app/routes/application.js
@@ -20,6 +20,10 @@ export default Route.extend(ApplicationRouteMixin, {
 
   async beforeModel() {
     let locale = await this._determineLocale();
+    if (!window.Intl) {
+      debug(`Loading Intl.js polyfill...`);
+      await this.intl.loadIntlPolyfill();
+    }
     await this.intl.loadAndSetLocale(locale);
   },
 
