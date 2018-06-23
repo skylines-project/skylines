2e2d65ca2befcc2d560540fd97da20ac4ec40380
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-06-23 17:21:44 +0200

    Polly: Passthrough `ember-percy` requests

--- ember/tests/helpers/setup-polly.js
+++ ember/tests/helpers/setup-polly.js
@@ -6,6 +6,8 @@ export function setupPolly(hooks, options = {}) {
   hooks.beforeEach(function() {
     let { server } = this.polly;
 
+    server.get('/_percy/:anything').passthrough();
+
     server.get('/translations/:locale.json').passthrough();
 
     server.get('/api/locale').intercept((req, res) => {
