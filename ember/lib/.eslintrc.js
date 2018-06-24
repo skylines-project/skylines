acc7d0b12d9560b5e56d4de87719f3acc2582c01
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-06-24 14:42:02 +0200

    Workaround `ENOENT` issue

--- ember/lib/.eslintrc.js
+++ ember/lib/.eslintrc.js
@@ -3,4 +3,9 @@ module.exports = {
     node: true,
     browser: false,
   },
+  rules: {
+    // unfortunate workaround for:
+    // ENOENT: no such file or directory, scandir 'skylines/ember/lib/freestyle/app/app/controllers'
+    'prettier/prettier': 'off',
+  },
 };
