0d887576b58a792d294f6d49cc4cdd354789e64e
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-07-05 10:08:00 +0200

    tracking/server: Remove unused import

--- skylines/tracking/server.py
+++ skylines/tracking/server.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, print_function
 
 import sys
 import struct
-from datetime import datetime, time, timedelta
+from datetime import datetime, timedelta
 
 from gevent.server import DatagramServer
 
