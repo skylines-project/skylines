a0863e53ccc8f548486eaa5f3e1f79774dea4b75
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-10-26 17:45:46 +0200

    api/clubs/list/test: Use `pytest-voluptuous` to simplify JSON compare code

--- tests/api/views/clubs/list_test.py
+++ tests/api/views/clubs/list_test.py
@@ -1,32 +1,35 @@
+from pytest_voluptuous import S
+from voluptuous.validators import ExactSequence
+
 from tests.data import add_fixtures, clubs
 
 
 def test_list_all(db_session, client):
-    sfn = clubs.sfn()
-    lva = clubs.lva()
-    add_fixtures(db_session, sfn, lva)
+    add_fixtures(db_session, clubs.sfn(), clubs.lva())
 
     res = client.get("/clubs")
     assert res.status_code == 200
-    assert res.json == {
-        "clubs": [
-            {"id": lva.id, "name": "LV Aachen"},
-            {"id": sfn.id, "name": "Sportflug Niederberg"},
-        ]
-    }
+    assert res.json == S(
+        {
+            "clubs": ExactSequence(
+                [
+                    {"id": int, "name": "LV Aachen"},
+                    {"id": int, "name": "Sportflug Niederberg"},
+                ]
+            )
+        }
+    )
 
 
 def test_name_filter(db_session, client):
-    sfn = clubs.sfn()
-    lva = clubs.lva()
-    add_fixtures(db_session, sfn, lva)
+    add_fixtures(db_session, clubs.sfn(), clubs.lva())
 
     res = client.get("/clubs?name=LV%20Aachen")
     assert res.status_code == 200
-    assert res.json == {"clubs": [{"id": lva.id, "name": "LV Aachen"}]}
+    assert res.json == S({"clubs": ExactSequence([{"id": int, "name": "LV Aachen"}])})
 
 
 def test_name_filter_with_unknown_club(db_session, client):
     res = client.get("/clubs?name=Unknown")
     assert res.status_code == 200
-    assert res.json == {"clubs": []}
+    assert res.json == S({"clubs": ExactSequence([])})
