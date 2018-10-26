70867c8ecc5947097e9238e30123b9051df694e3
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-10-26 18:04:17 +0200

    api/notifications: Return `time` including timezone information

--- skylines/api/views/notifications.py
+++ skylines/api/views/notifications.py
@@ -2,6 +2,8 @@ from flask import Blueprint, request
 from sqlalchemy.orm import subqueryload, contains_eager
 from sqlalchemy.sql.expression import or_
 
+import pytz
+
 from skylines.api.json import jsonify
 from skylines.database import db
 from skylines.api.oauth import oauth
@@ -80,7 +82,7 @@ def convert_event(e):
     event = {
         "id": e.id,
         "type": TYPES.get(e.type, "unknown"),
-        "time": e.time.isoformat(),
+        "time": e.time.replace(tzinfo=pytz.utc).isoformat(),
         "actor": {"id": e.actor_id, "name": e.actor.name},
     }
 
