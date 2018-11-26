60db5b8800328b7796d3daf5775a2d464cad8bd6
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-11-26 10:33:38 +0100

    Replace `ember-cli-eslint` with regular ESLint

--- ember/.eslintrc.js
+++ ember/.eslintrc.js
@@ -13,4 +13,25 @@ module.exports = {
     'ember/order-in-routes': 'off',
     'prettier/prettier': 'error',
   },
+  overrides: [
+    // node files
+    {
+      files: [
+        '.eslintrc.js',
+        '.template-lintrc.js',
+        'ember-cli-build.js',
+        'testem.js',
+        'config/**/*.js',
+        'lib/*/index.js',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015,
+      },
+      env: {
+        browser: false,
+        node: true,
+      },
+    },
+  ],
 };
