1d050eccb9582ad27be7367d7e7c553b97a0e6a8
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-04-06 21:30:34 +0200

    uwsgi/mapproxy: Use hardcoded relative path to `mapproxy.yaml` file

--- uwsgi/mapproxy.py
+++ uwsgi/mapproxy.py
@@ -1,19 +1,10 @@
 import os
 import sys
-import argparse
 from mapproxy.wsgiapp import make_wsgi_app
-
-parser = argparse.ArgumentParser(description="Run the MapProxy WSGI app.")
-parser.add_argument(
-    "config_file",
-    nargs="?",
-    metavar="mapproxy.yaml",
-    help="path to the configuration YAML file",
-)
-args = parser.parse_args()
 
 sys.path.append(os.path.dirname(sys.argv[0]))
 
-from mapproxy.wsgiapp import make_wsgi_app
-
-application = make_wsgi_app(args.config_file)
+config_file = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "..", "mapproxy", "mapproxy.yaml"
+)
+application = make_wsgi_app(config_file)
