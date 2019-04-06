53b0f47e04d03d3d15dc2ac40fe8a2a82fbefb4f
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-04-06 21:30:52 +0200

    uwsgi/mapproxy: Remove unnecessary system path adjustment

--- uwsgi/mapproxy.py
+++ uwsgi/mapproxy.py
@@ -1,8 +1,5 @@
 import os
-import sys
 from mapproxy.wsgiapp import make_wsgi_app
-
-sys.path.append(os.path.dirname(sys.argv[0]))
 
 config_file = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), "..", "mapproxy", "mapproxy.yaml"
