ddbde78085e94145cfcda65dcee97d4b6323a6b2
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-06-15 13:54:44 +0200

    fix-table: Use regular `computed()` instead of `ember-awesome-macros`

--- ember/app/components/fix-table.js
+++ ember/app/components/fix-table.js
@@ -1,23 +1,25 @@
 import Component from '@ember/component';
-
-import { gt } from 'ember-awesome-macros';
-import { map } from 'ember-awesome-macros/array';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   flights: null,
   fixes: null,
   selection: null,
 
-  data: map('fixes', function(fix, i) {
-    let flight = fix.get('flight');
-    let id = flight.get('id');
-    let color = flight.get('color');
-    let competitionId = flight.get('competition_id') || flight.get('registration');
-    let removable = i !== 0;
-    return { id, color, competitionId, removable, fix };
+  data: computed('fixes.@each.flight', function() {
+    return this.fixes.map((fix, i) => {
+      let flight = fix.get('flight');
+      let id = flight.get('id');
+      let color = flight.get('color');
+      let competitionId = flight.get('competition_id') || flight.get('registration');
+      let removable = i !== 0;
+      return { id, color, competitionId, removable, fix };
+    });
   }),
 
-  selectable: gt('data.length', 1),
+  selectable: computed('data.[]', function() {
+    return this.data.length > 1;
+  }),
 
   actions: {
     select(id) {
