321ee2ed041410a790f8c183c05443ebb391676d
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-10-28 22:24:42 +0100

    Add `lint:hbs` script using ember-template-lint

---
+++ ember/.template-lintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: 'recommended',
+
+  rules: {
+    'block-indentation': false,
+    'img-alt-attributes': false,
+    'no-html-comments': false,
+    'no-inline-styles': false,
+    'no-invalid-interactive': false,
+    'no-unnecessary-concat': false,
+    'no-unused-block-params': false,
+    quotes: false,
+    'self-closing-void-elements': false,
+    'simple-unless': false,
+    'style-concatenation': false,
+    'table-groups': false,
+  },
+};
