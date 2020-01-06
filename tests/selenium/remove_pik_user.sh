1948c96d7ed519e6a136321886a138df548b1f87
Bret Hess <bret.hess@gmail.com>
2020-01-06 16:44:56 -0700

    testing changes.  Error is in choose-club.js, clubsWithNull

--- tests/selenium/remove_pik_user.sh
+++
@@ -1,6 +0,0 @@
-
-
-sudo sudo -u postgres psql -d skylines -c "DELETE FROM igc_files WHERE date_condor='2019-03-12';"
-sudo sudo -u postgres psql -d skylines -c "DELETE FROM flights WHERE date_local='2019-03-12';"
-sudo sudo -u postgres psql -d skylines -c "DELETE FROM users WHERE first_name='pik';"
-sudo sudo -u postgres psql -d skylines -c "DELETE FROM clubs WHERE name='newgroup';"
