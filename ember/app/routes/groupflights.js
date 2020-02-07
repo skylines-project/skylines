19ed0ee4618cda9474b53b30615c61ff99cf75d8
Bret Hess <bret.hess@gmail.com>
2020-02-07 16:38:15 -0700

    more routes, template, controller files for groupflights

---
+++ ember/app/routes/groupflights.js
@@ -0,0 +1,42 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+
+export default Route.extend({
+  ajax: service(),
+
+  queryParams: {
+    page: { refreshModel: true },
+    year: { refreshModel: true },
+  },
+
+  model(params) {
+    let data = {
+      year: params.year,
+      page: params.page,
+    };
+
+    return this.ajax.request('/api/groupflights', { data });
+  },
+
+  setupController(controller) {
+    this._super(...arguments);
+    controller.set('year', this.paramsFor('clubs').year);
+  },
+
+  resetController(controller, isExiting) {
+    this._super(...arguments);
+    if (isExiting) {
+      controller.set('page', 1);
+    }
+  },
+
+  actions: {
+    loading(transition) {
+//      let controller = this.controllerFor('clubs');
+      controller.set('loading', true);
+      transition.promise.finally(() => {
+        controller.set('loading', false);
+      });
+    },
+  },
+});
