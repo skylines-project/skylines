9d0125591a19e1e2dc562073e938ddd9582b3528
Bret Hess <bret.hess@gmail.com>
2020-02-18 14:15:52 -0700

    removed 2 frontend folders

--- ember/app/routes/groupflight/index.js
+++
@@ -1,8 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default Route.extend({
-  setupController(controller) {
-    this._super(...arguments);
-    controller.set('groupflight', this.controllerFor('groupflight').get('model'));
-  },
-});
