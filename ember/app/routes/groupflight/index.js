0827899823317ee259ab3144b1b009c293045bdd
Bret Hess <bret.hess@gmail.com>
2020-02-20 17:16:27 -0700

    Got firstPath to enter groupflightpage.js through "model" in index.js instead of set controller.  Still have errors after that point in index.js

--- ember/app/routes/groupflight/index.js
+++ ember/app/routes/groupflight/index.js
@@ -4,12 +4,18 @@ import { inject as service } from '@ember/service';
 export default Route.extend({
   ajax: service(),
 
+  model() {
+    let { club_id } = this.paramsFor('club');
+    return this.ajax.request(`/api/flights/${this.modelFor('groupflight').ids[0]}/json`);
+  },
+
+
   setupController(controller) {
     this._super(...arguments);
     controller.set('groupflight', this.modelFor('groupflight').groupflight);
     controller.set('ids', this.modelFor('groupflight').ids);
     controller.set('club', this.modelFor('groupflight').club);
-    controller.set('firstPath', this.ajax.request(`/api/flights/${this.modelFor('groupflight').ids[0]}/json`));
+//    controller.set('firstPath', this.ajax.request(`/api/flights/${this.modelFor('groupflight').ids[0]}/json`));
   },
 
 });
