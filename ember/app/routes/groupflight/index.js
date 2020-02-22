369d57717154f9e0c4827d7fbc1b048022b6ea34
Bret Hess <bret.hess@gmail.com>
2020-02-22 10:05:06 -0700

    success! in getting to display of groupflight map

--- ember/app/routes/groupflight/index.js
+++ ember/app/routes/groupflight/index.js
@@ -1,15 +1,36 @@
+//import Route from '@ember/routing/route';
+//import { inject as service } from '@ember/service';
+//
+//export default Route.extend({
+//  ajax: service(),
+//  ids: modelFor('groupflight').ids.split(',').map(it => parseInt(it, 10)),
+
+//  model() {
+////  console.log(ids);
+//    let ids = this.modelFor('groupflight').ids.split(',').map(it => parseInt(it, 10));
+//    return { ids };
+//  },
+
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+//import RSVP from 'rsvp';
 
 export default Route.extend({
-  ajax: service(),
+    ajax: service(),
+  //model includes groupflight, ids, club
+//    model({ groupflight_id }) {
+//      return this.ajax.request(`/api/groupflights/${groupflight_id}`)
+//    },
 
-  setupController(controller) {
-    this._super(...arguments);
-    controller.set('groupflight', this.modelFor('groupflight').groupflight);
-    controller.set('ids', this.modelFor('groupflight').ids);
-    controller.set('club', this.modelFor('groupflight').club);
-    controller.set('firstPath', this.ajax.request(`/api/flights/${this.modelFor('groupflight').ids[0]}/json`));
+  afterModel() {
+  console.log(this.modelFor('groupflight').ids[0]);
+    this.transitionTo('flight', this.modelFor('groupflight').ids.join(','));
   },
 
 });
+
+//  model(params) {
+//    let ids = params.flight_ids.split(',').map(it => parseInt(it, 10));
+//    return { ids };
+//  },
+
