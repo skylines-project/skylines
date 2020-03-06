06ec37b4c7fc0c94a478fe3667c9940f2fa71e4c
Bret Hess <bret.hess@gmail.com>
2020-03-05 19:29:53 -0700

    merged old and new date detection on upload

--- skylines/api/views/upload.py
+++ skylines/api/views/upload.py
@@ -199,29 +199,32 @@ def _encode_flight_path(fp, qnh):
 
 def get_date_from_name(filename):
     #Local date must be somewhere in the file name.  It will guess a date if ambiguous
-    filename = filename.replace(".igc","").replace('_','-').replace(' ','-').replace('.','-').replace(',','-')
-    filename = re.sub("[A-Za-z]", "", filename) #keep only digits and separators
-    now = datetime.utcnow()
     try:
-        trialDate = dparser.parse(filename, fuzzy=True)
-    except: #run through 10-character segments of string (maximum length of date YYYYxMMxDD):
-        for istart in range(0,len(filename)-10):
-            if filename[istart].isdigit(): #check only strings starting with digit, not separator
-                try:
-                    # print 'test', filename[istart:istart + 10]
-                    trialDate = dparser.parse(filename[istart:istart+10], fuzzy=True)
-                except:
-                    continue
+        filename = filename.replace(".igc","").replace('_','-').replace(' ','-').replace('.','-').replace(',','-')
+    except:
+        filename = re.sub("[A-Za-z]", "", filename) #keep only digits and separators
+        now = datetime.utcnow()
+        try:
+            trialDate = dparser.parse(filename, fuzzy=True)
+        except: #run through 10-character segments of string (maximum length of date YYYYxMMxDD):
+            for istart in range(0,len(filename)-10):
+                if filename[istart].isdigit(): #check only strings starting with digit, not separator
+                    try:
+                        # print 'test', filename[istart:istart + 10]
+                        trialDate = dparser.parse(filename[istart:istart+10], fuzzy=True)
+                    except:
+                        continue
+                    else:
+                        if now - timedelta (hours=20*365*24) <= trialDate <= now + timedelta(hours=24): #reasonable dates
+                            return trialDate #success
                 else:
-                    if now - timedelta (hours=20*365*24) <= trialDate <= now + timedelta(hours=24): #reasonable dates
-                        return trialDate #success
-            else:
-                continue
+                    continue
+        else:
+            if now - timedelta (hours=20*365*24) <= trialDate <= now + timedelta(hours=24): #reasonable dates
+                return trialDate  # success
+        return dparser.parse(filename, fuzzy=True) #this line will throw an error
     else:
-        if now - timedelta (hours=20*365*24) <= trialDate <= now + timedelta(hours=24): #reasonable dates
-            return trialDate  # success
-    return dparser.parse(filename, fuzzy=True) #this line will throw an error
-
+        return dparser.parse(filename, fuzzy=True)
 @upload_blueprint.route("/flights/upload", methods=("POST",), strict_slashes=False)
 @oauth.required()
 def index_post():
