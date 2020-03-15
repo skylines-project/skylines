09d17c96b4a43622d7b4050588078505fa419796
Bret Hess <bret.hess@gmail.com>
2020-03-15 13:57:57 -0600

    install files for nginx, ssl, pgadmin

---
+++ installNGINX_SSL.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install nginx
+
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install certbot
