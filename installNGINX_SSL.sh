b7ae56290c1eaa7050a56c7b141b98932ad892c2
Bret Hess <bret.hess@gmail.com>
2020-03-15 17:20:49 -0600

    installNGINX_SSL.sh successful in getting new certificate

--- installNGINX_SSL.sh
+++ installNGINX_SSL.sh
@@ -5,3 +5,26 @@ sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:certbot/certbot
 sudo apt-get update
 sudo apt-get install certbot
+
+cd /etc/nginx/
+sudo cp nginx.conf nginx.conf.orig
+
+sudo cp sites-available/default sites-available/skylinescondor.com
+sudo ln -s /etc/nginx/sites-available/skylinescondor.com /etc/nginx/sites-enabled/
+sudo rm sites-enabled/default
+
+sudo vim sites-available/skylinescondor.com
+# replace;
+#    server_name _;
+# with 
+# server_name skylinescondor.com;
+
+# location ^~ /.well-known/acme-challenge/ {
+# 	allow all;
+#   default_type "text/plain";
+# }
+sudo service nginx reload
+
+sudo certbot certonly --webroot --webroot-path=/var/www/html -d skylinescondor.com
+
+The client lacks sufficient authorization :: Invalid response from .well-known/acme-challenge/
