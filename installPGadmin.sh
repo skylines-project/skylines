8f6c713849e50f5007d31e0a7ffe93b8b1f7f564
Bret Hess <bret.hess@gmail.com>
2020-03-18 12:55:12 -0600

    improved installPGadmin.sh (success)

--- installPGadmin.sh
+++ installPGadmin.sh
@@ -4,23 +4,23 @@ pipenv shell
 
 sudo pipenv install <wheel>
 
-# lots of permissioni problems with pgadmin.
+# Permissioni problems with pgadmin.
 
-https://stackoverflow.com/questions/46707935/oserror-errno-13-permission-denied-var-lib-pgadmin
-f you do not want to change the permission of anything, you can always override default paths in pgAdmin4.
-
-Create a file named config_local.py (if not already present) at your installation location ../pgadmin4/web/
-
-import os
-DATA_DIR = os.path.realpath(os.path.expanduser(u'~/.pgadmin/'))
-LOG_FILE = os.path.join(DATA_DIR, 'pgadmin4.log')
-SQLITE_PATH = os.path.join(DATA_DIR, 'pgadmin4.db')
+#https://stackoverflow.com/questions/46707935/oserror-errno-13-permission-denied-var-lib-pgadmin
+vim ~/.local/share/virtualenvs/pgadmin-WsDn56it/lib/python2.7/site-packages/pgadmin4/config_local.py
+#paste
+import os 
+DATA_DIR = os.path.realpath(os.path.expanduser(u'~/.pgadmin/')) 
+LOG_FILE = os.path.join(DATA_DIR, 'pgadmin4.log') 
+SQLITE_PATH = os.path.join(DATA_DIR, 'pgadmin4.db') 
 SESSION_DB_PATH = os.path.join(DATA_DIR, 'sessions') 
 STORAGE_DIR = os.path.join(DATA_DIR, 'storage')
+
+!!!This solves the var/lib permission problem. 
 
 
 
 # start:
-python ~/.lpocal/share/virtualenvs/pgadmin-WsDn56it/lib/python2.7/site-packages/pgadmin4/pgAdmin4.pyls
+python ~/.local/share/virtualenvs/pgadmin-WsDn56it/lib/python2.7/site-packages/pgadmin4/pgAdmin4.py
 
-I
+make sure password is set:  psql skylines <then> ALTER USER bret PASSWORD '<password>';
