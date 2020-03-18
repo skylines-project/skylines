245b1120e6cd4413972de958b9ad682520d85b12
Bret Hess <bret.hess@gmail.com>
2020-03-17 21:37:48 -0600

    updated two install files

--- installPGadmin.sh
+++ installPGadmin.sh
@@ -1,8 +1,9 @@
-sudo chown -R $USER /var/log/
-sudo chown -R $USER /lib/log/
 mkdir ~/pgadmin
 cd ~/pgadmin
 pipenv shell
 
-#now run 'pipenv install <wheel>'
-#python ~/.local/share/virtualenvs/pgadmin-WsDn56it/lib/python2.7/site-packages/pgadmin4/pgAdmin4.py
+sudo pipenv install <wheel>
+chown -R bret:root /var/lib
+chown -R bret:root /var/log
+# start:
+python ~/.lpocal/share/virtualenvs/pgadmin-WsDn56it/lib/python2.7/site-packages/pgadmin4/pgAdmin4.py
