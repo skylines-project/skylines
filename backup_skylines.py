a0c56d71c5ab4200a479fe40622610483818be49
Bret Hess <bret.hess@gmail.com>
2020-04-11 15:45:50 -0600

    pg_dump backup working

--- backup_skylines.py
+++ backup_skylines.py
@@ -1,4 +1,5 @@
-import os, sys, datetime, time, subprocess
+import os, sys, datetime, subprocess
+import time as t
 import paramiko
 
 """Ember server runs this script and saves to Nginx server, which saves to Google Drive
@@ -37,48 +38,58 @@ run = True
 while run:
     now = datetime.datetime.now()
     nowStr = now.strftime("_{}".format(timeFormat))
-    print "Current date and time:",nowStr
-    # try:
-    dumpFile = '/home/bret/servers/database-backups/skylinesdump{}.custom'.format(nowStr)
-    os.system('sudo -u bret pg_dump --format=custom skylines > {}'.format(dumpFile))
+    # print "Current date and time:",nowStr
+    try:
+        dumpFile = '/home/bret/servers/database-backups/skylinesdump{}.custom'.format(nowStr)
+        os.system('sudo -u bret pg_dump --format=custom skylines > {}'.format(dumpFile))
+    except:
+        print 'error in pg_dump step'
     os.system('scp {} {}@{}:{}'.format(dumpFile,username,host,saveDir))
-        # command = 'sudo -u bret pg_dump --format=custom skylines > /home/bret/servers/database-backups/skylinesdump{}.custom'.format(nowStr)
-        # process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-        # output, error = process.communicate()
-    # except:
-    #     print 'Error in skylines dump', nowStr
-    # try:
-    files = ftp.listdir(saveDir)
-    dumps = []
-    dumpTimes = []
-    dumpSizes = []
-    for file in files:
-        if 'dump' in file and file.split('_')[1]>0:
-            dumps.append(file)
-            dumpTimeStamp = file.split('_')[1].replace('.custom','')
-            dumpTimes.append(datetime.datetime.strptime(dumpTimeStamp, format(timeFormat)))
-            size = ftp.stat('{}/{}'.format(saveDir,file)).st_size
-            dumpSizes.append(size)
-            print '{:.2f} MB, {}'.format(size/float(10**6),file)
-    # except:
-    #     print 'Error in ftp', nowStr
-    #sort by time
+    try:
+        files = ftp.listdir(saveDir)
+        dumps = []
+        dumpTimes = []
+        dumpSizes = []
+        for file in files:
+            if 'dump' in file:
+                try:
+                    file.split('_')[1]>0
+                    dumps.append(file)
+                    dumpTimeStamp = file.split('_')[1].replace('.custom','')
+                    dumpTimes.append(datetime.datetime.strptime(dumpTimeStamp, format(timeFormat)))
+                    try:
+                        size = ftp.stat('{}/{}'.format(saveDir, file)).st_size
+                        # print 'test', file, size
+                    except:
+                        size = None
+                    dumpSizes.append(size)
+                    # print '{:.2f} MB, {}'.format(size / float(10 ** 6), file)
+                except:
+                    'skip file'
+    except:
+        print 'error in reading dumps'
     allInfo = zip(dumpTimes,dumps,dumpSizes)
+    dumpsInfo =  [[dump,timeFile,size] for timeFile,dump,size in sorted(allInfo,reverse=True)] # name, timeFile, size
+    for i in range(len(dumpsInfo)):
+        file = dumpsInfo[i][0]
+        size = dumpsInfo[i][2]
+        print '{:.2f} MB, {}'.format(size / float(10 ** 6), file)
+    oldestDump = dumpsInfo[-1]
+    while len(dumpsInfo) > nkeep and oldestDump[2] <= dumpsInfo[0][2]: #Delete oldest if newest is larger or same size
+        try: #delete oldest
+            stdin, stdout, stderr = ssh.exec_command('rm {}/{}'.format(saveDir,oldestDump[0]))
+            # Wait for the command to terminate
+            while not stdout.channel.exit_status_ready() and not stdout.channel.recv_ready():
+                t.sleep(1)
+            stdoutstring = stdout.readlines()
+            stderrstring = stderr.readlines()
+            print 'Deleted', oldestDump[0]
+            dumpsInfo.pop()
+            oldestDump = dumpsInfo[-1]
+        except:
+            print 'Error in deleting oldest dump',oldestDump
 
-    dumpsInfo =  [[dump,time,size] for time,dump,size_ in sorted(allInfo,reverse=True)]
-
-    while len(dumpsInfo) > nkeep:
-        os.system('sudo -u bret pg_dump --format=custom skylines > /home/bret/servers/database-backups/skylinesdump{}.custom'.format(nowStr))
-        dumpsInfo.pop()
-
-    print dumpsInfo
-
-
-
-
-
-
-
-    time.sleep(5)
+    print
+    t.sleep(5)
 
 
