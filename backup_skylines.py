31d556f966153eb5f3978181e8a6a737b3124f2b
Bret Hess <bret.hess@gmail.com>
2020-04-11 09:09:01 -0600

    progress on backup_skylines.py

--- backup_skylines.py
+++ backup_skylines.py
@@ -1,4 +1,4 @@
-import os, sys, datetime, time
+import os, sys, datetime, time, subprocess
 import paramiko
 
 """Ember server runs this script and saves to Nginx server, which saves to Google Drive
@@ -10,10 +10,11 @@ then delete the oldest dump.
 def dateFile(filename):
     return filename.split('.')[0].split('_')[1]
 
-saveDir = 'bret@192.168.1.241:/home/bret/google_drive/'
+saveDir = '/home/bret/google_drive/'
 htDir = '{}/htdocs'.format(saveDir)
 
 nkeep = 3
+timeFormat = '%Y-%m-%d.%H.%M.%S'
 
 #connection
 host = '192.168.1.241'
@@ -35,19 +36,48 @@ ftp = ssh.open_sftp()
 run = True
 while run:
     now = datetime.datetime.now()
-    nowStr = now.strftime("_%Y-%m-%d.%H.%M.%S")
-    print("Current date and time:",nowStr)
-    # automatically add keys without requiring human intervention
-    try:
-        files = ftp.listdir()
-        print 'Files', files
-        dumps = []
-        for file in files:
-            if 'dump' in file:
-                dumps.append(file)
-                print file
-    except:
-        print 'Error in ftp'
+    nowStr = now.strftime("_{}".format(timeFormat))
+    print "Current date and time:",nowStr
+    # try:
+    dumpFile = '/home/bret/servers/database-backups/skylinesdump{}.custom'.format(nowStr)
+    os.system('sudo -u bret pg_dump --format=custom skylines > {}'.format(dumpFile))
+    os.system('scp {} {}@{}:{}'.format(dumpFile,username,host,saveDir))
+        # command = 'sudo -u bret pg_dump --format=custom skylines > /home/bret/servers/database-backups/skylinesdump{}.custom'.format(nowStr)
+        # process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+        # output, error = process.communicate()
+    # except:
+    #     print 'Error in skylines dump', nowStr
+    # try:
+    files = ftp.listdir(saveDir)
+    dumps = []
+    dumpTimes = []
+    dumpSizes = []
+    for file in files:
+        if 'dump' in file and file.split('_')[1]>0:
+            dumps.append(file)
+            dumpTimeStamp = file.split('_')[1].replace('.custom','')
+            dumpTimes.append(datetime.datetime.strptime(dumpTimeStamp, format(timeFormat)))
+            size = ftp.stat('{}/{}'.format(saveDir,file)).st_size
+            dumpSizes.append(size)
+            print '{:.2f} MB, {}'.format(size/float(10**6),file)
+    # except:
+    #     print 'Error in ftp', nowStr
+    #sort by time
+    allInfo = zip(dumpTimes,dumps,dumpSizes)
+
+    dumpsInfo =  [[dump,time,size] for time,dump,size_ in sorted(allInfo,reverse=True)]
+
+    while len(dumpsInfo) > nkeep:
+        os.system('sudo -u bret pg_dump --format=custom skylines > /home/bret/servers/database-backups/skylinesdump{}.custom'.format(nowStr))
+        dumpsInfo.pop()
+
+    print dumpsInfo
+
+
+
+
+
+
 
     time.sleep(5)
 
