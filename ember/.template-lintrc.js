2531887aa9abef46aa88ba03e0fb92c6bb27a8a1
Tobias Bieniek <tobias.bieniek@gmail.com>
2020-05-24 08:14:01 +0200

    template-lint: Adjust rule configuration

--- ember/.template-lintrc.js
+++ ember/.template-lintrc.js
@@ -3,9 +3,13 @@ module.exports = {
 
   rules: {
     'block-indentation': false,
-    'img-alt-attributes': false,
+    'link-href-attributes': false,
+    'link-rel-noopener': false,
     'no-inline-styles': false,
     'no-invalid-interactive': false,
+    'no-negated-condition': false,
+    'require-button-type': false,
+    'require-valid-alt-text': false,
     'style-concatenation': false,
     'table-groups': false,
   },
