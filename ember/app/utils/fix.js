0817b4e1d7cc9e56386b84876ccb773096684849
Tobias Bieniek <tobias@bieniek.cloud>
2020-06-27 19:58:09 +0200

    utils/fix: Convert to ES6 class

--- ember/app/utils/fix.js
+++ ember/app/utils/fix.js
@@ -1,6 +1,5 @@
 import EmberObject, { computed } from '@ember/object';
 import { readOnly } from '@ember/object/computed';
-import Ember from 'ember';
 
 import ol from 'openlayers';
 
@@ -9,65 +8,63 @@ import safeComputed from '../computed/safe-computed';
 import geographicDistance from '../utils/geo-distance';
 import getNextSmallerIndex from '../utils/next-smaller-index';
 
-let Fix = EmberObject.extend({
-  fixCalc: null,
+export default class Fix extends EmberObject {
+  @readOnly('fixCalc.time') _t;
 
-  _t: readOnly('fixCalc.time'),
-
-  t: computed('_t', 'flight.{startTime,endTime}', function () {
+  @computed('_t', 'flight.{startTime,endTime}')
+  get t() {
     let _t = this._t;
     if (_t === -1) {
       return this.get('flight.endTime');
     } else if (_t >= this.get('flight.startTime') && _t <= this.get('flight.endTime')) {
       return _t;
     }
-  }),
+  }
 
-  _index: safeComputed('t', 'flight.time', (t, time) => getNextSmallerIndex(time, t)),
+  @safeComputed('t', 'flight.time', (t, time) => getNextSmallerIndex(time, t)) _index;
 
-  t_prev: safeComputed('_index', 'flight.time', (index, time) => time[index]),
-  t_next: safeComputed('_index', 'flight.time', (index, time) => time[index + 1]),
+  @safeComputed('_index', 'flight.time', (index, time) => time[index]) t_prev;
+  @safeComputed('_index', 'flight.time', (index, time) => time[index + 1]) t_next;
 
-  time: readOnly('t_prev'),
+  @readOnly('t_prev') time;
 
-  coordinate: safeComputed('t', 'flight.geometry', (time, geometry) => geometry.getCoordinateAtM(time)),
+  @safeComputed('t', 'flight.geometry', (time, geometry) => geometry.getCoordinateAtM(time)) coordinate;
 
-  lon: readOnly('coordinate.0'),
-  lat: readOnly('coordinate.1'),
+  @readOnly('coordinate.0') lon;
+  @readOnly('coordinate.1') lat;
 
-  'alt-msl': safeComputed('coordinate.2', 'flight.geoid', (altitude, geoid) => altitude - geoid),
+  @safeComputed('coordinate.2', 'flight.geoid', (altitude, geoid) => altitude - geoid) 'alt-msl';
 
-  'alt-gnd': safeComputed('alt-msl', 'elevation', (altitude, elevation) => {
+  @safeComputed('alt-msl', 'elevation', (altitude, elevation) => {
     let value = altitude - elevation;
     return value >= 0 ? value : 0;
-  }),
+  })
+  'alt-gnd';
 
-  point: computedPoint('coordinate'),
-  pointXY: computedPoint('coordinate', 'XY'),
+  @computedPoint('coordinate') point;
+  @computedPoint('coordinate', 'XY') pointXY;
 
-  heading: safeComputed('_coordinate_prev', '_coordinate_next', (prev, next) =>
+  @safeComputed('_coordinate_prev', '_coordinate_next', (prev, next) =>
     Math.atan2(next[0] - prev[0], next[1] - prev[1]),
-  ),
+  )
+  heading;
 
-  vario: safeComputed('_coordinate_prev.2', '_coordinate_next.2', '_dt', (prev, next, dt) => (next - prev) / dt),
+  @safeComputed('_coordinate_prev.2', '_coordinate_next.2', '_dt', (prev, next, dt) => (next - prev) / dt) vario;
 
-  speed: safeComputed('_coordinate_prev', '_coordinate_next', '_dt', (prev, next, dt) => {
+  @safeComputed('_coordinate_prev', '_coordinate_next', '_dt', (prev, next, dt) => {
     let loc_prev = ol.proj.transform(prev, 'EPSG:3857', 'EPSG:4326');
     let loc_next = ol.proj.transform(next, 'EPSG:3857', 'EPSG:4326');
 
     return geographicDistance(loc_next, loc_prev) / dt;
-  }),
+  })
+  speed;
 
-  _dt: safeComputed('t_prev', 't_next', (prev, next) => next - prev),
+  @safeComputed('t_prev', 't_next', (prev, next) => next - prev) _dt;
 
-  _coordinate_prev: safeComputed('t_prev', 'flight.geometry', (time, geometry) => geometry.getCoordinateAtM(time)),
-  _coordinate_next: safeComputed('t_next', 'flight.geometry', (time, geometry) => geometry.getCoordinateAtM(time)),
+  @safeComputed('t_prev', 'flight.geometry', (time, geometry) => geometry.getCoordinateAtM(time)) _coordinate_prev;
+  @safeComputed('t_next', 'flight.geometry', (time, geometry) => geometry.getCoordinateAtM(time)) _coordinate_next;
 
-  elevation: safeComputed('flight.elev_h', '_elev_index', (elev_h, index) => elev_h[index]),
+  @safeComputed('flight.elev_h', '_elev_index', (elev_h, index) => elev_h[index]) elevation;
 
-  _elev_index: safeComputed('flight.elev_t', 't', (elev_t, t) => getNextSmallerIndex(elev_t, t)),
-});
-
-Fix[Ember.NAME_KEY] = 'Fix';
-
-export default Fix;
+  @safeComputed('flight.elev_t', 't', (elev_t, t) => getNextSmallerIndex(elev_t, t)) _elev_index;
+}
