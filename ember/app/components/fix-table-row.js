acc919d639ebd61abef250222c784dba24de4911
Tobias Bieniek <tobias@bieniek.cloud>
2020-06-27 19:29:00 +0200

    FixTableRow: Convert to ES6 class

--- ember/app/components/fix-table-row.js
+++ ember/app/components/fix-table-row.js
@@ -2,23 +2,25 @@ import Component from '@ember/component';
 import { action, computed } from '@ember/object';
 import { htmlSafe } from '@ember/template';
 
-export default Component.extend({
-  tagName: '',
-  selectable: false,
+export default class extends Component {
+  tagName = '';
 
-  badgeStyle: computed('row.color', function () {
+  selectable = false;
+
+  @computed('row.color')
+  get badgeStyle() {
     return htmlSafe(`background-color: ${this.row.color}`);
-  }),
+  }
 
-  actions: {
-    remove() {
-      this.onRemove(this.get('row.id'));
-    },
-  },
+  @action
+  remove() {
+    this.onRemove(this.get('row.id'));
+  }
 
-  handleClick: action(function () {
+  @action
+  handleClick() {
     if (this.selectable) {
       this.onSelect(this.get('row.id'));
     }
-  }),
-});
+  }
+}
