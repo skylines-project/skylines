902536dc7f679789ff2c6ab2b50137c8d29f6710
Tobias Bieniek <tobias@bieniek.cloud>
2020-06-27 18:18:07 +0200

    XFlag: Replace `codeLower` property with `lowercase` helper

--- ember/app/components/x-flag.js
+++ ember/app/components/x-flag.js
@@ -1,9 +1,5 @@
 import Component from '@ember/component';
 
-import safeComputed from '../computed/safe-computed';
-
 export default Component.extend({
   tagName: '',
-
-  codeLower: safeComputed('code', code => code.toLowerCase()),
 });
