1ad5efc19911715b1fac8e397439d148980820dc
Tobias Bieniek <tobias@bieniek.cloud>
2020-06-28 23:05:09 +0200

    SortableTableHeader: Use splattributes

--- ember/app/components/sortable-table-header.js
+++ ember/app/components/sortable-table-header.js
@@ -1,18 +1,8 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
 
 export default Component.extend({
   tagName: '',
 
   column: null,
   defaultOrder: 'asc',
-
-  cssClass: computed('class', function () {
-    let _class = this['class'];
-    if (_class) {
-      return `${_class} sortable`;
-    } else {
-      return 'sortable';
-    }
-  }),
 });
