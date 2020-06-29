7981ddc8009cbbd52c907f3606403f8d23d9ab86
Tobias Bieniek <tobias@bieniek.cloud>
2020-06-29 23:18:56 +0200

    services/cesium-loader: Extract `loadTask`

--- ember/app/services/cesium-loader.js
+++ ember/app/services/cesium-loader.js
@@ -1,19 +1,26 @@
 import { debug } from '@ember/debug';
 import Service from '@ember/service';
 
+import { task } from 'ember-concurrency';
+
 export default class CesiumLoaderService extends Service {
   loaderPromise = null;
 
   load() {
     let promise = this.loaderPromise;
     if (!promise) {
-      debug('Loading Cesium...');
-      promise = loadJS('/cesium/Cesium.js');
+      promise = this.loadTask.perform();
       this.set('loaderPromise', promise);
     }
 
     return promise;
   }
+
+  @(task(function* () {
+    debug('Loading Cesium...');
+    yield loadJS('/cesium/Cesium.js');
+  }).drop())
+  loadTask;
 }
 
 function loadJS(url) {
