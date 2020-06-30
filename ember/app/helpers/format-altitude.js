bd64ae2311632c0a9147252789bfee56e7759972
Tobias Bieniek <tobias@bieniek.cloud>
2020-06-30 10:04:32 +0200

    helpers/format-altitude: Remove obsolete `units` observer
    This is no longer necessary due to the auto-tracking inside the `units` service

--- ember/app/helpers/format-altitude.js
+++ ember/app/helpers/format-altitude.js
@@ -1,13 +1,8 @@
 import Helper from '@ember/component/helper';
-import { observer } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default Helper.extend({
   units: service(),
-
-  altitudeUnitObserver: observer('units.altitudeUnit', function () {
-    this.recompute();
-  }),
 
   compute([value], options) {
     return this.units.formatAltitude(value, options);
