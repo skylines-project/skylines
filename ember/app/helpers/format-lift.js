4177fd76636dd389698e45cd7aeb57005290ac69
Tobias Bieniek <tobias@bieniek.cloud>
2020-06-30 10:04:39 +0200

    helpers/format-lift: Remove obsolete `units` observer
    This is no longer necessary due to the auto-tracking inside the `units` service

--- ember/app/helpers/format-lift.js
+++ ember/app/helpers/format-lift.js
@@ -1,13 +1,8 @@
 import Helper from '@ember/component/helper';
-import { observer } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default Helper.extend({
   units: service(),
-
-  liftUnitObserver: observer('units.liftUnit', function () {
-    this.recompute();
-  }),
 
   compute([value], options) {
     return this.units.formatLift(value, options);
