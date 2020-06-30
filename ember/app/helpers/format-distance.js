a50c68cc2765ba99b6eecf6fa8f3694e82af775a
Tobias Bieniek <tobias@bieniek.cloud>
2020-06-30 10:00:09 +0200

    helpers/format-distance: Remove obsolete `units` observer
    This is no longer necessary due to the auto-tracking inside the `units` service

--- ember/app/helpers/format-distance.js
+++ ember/app/helpers/format-distance.js
@@ -1,13 +1,8 @@
 import Helper from '@ember/component/helper';
-import { observer } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default Helper.extend({
   units: service(),
-
-  distanceUnitObserver: observer('units.distanceUnit', function () {
-    this.recompute();
-  }),
 
   compute([value], options) {
     return this.units.formatDistance(value, options);
