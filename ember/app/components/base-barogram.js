78f1bb6d3cbe2a572095e3e4bbaf2f715ab6239d
Tobias Bieniek <tobias@bieniek.cloud>
2020-07-01 10:59:32 +0200

    BaseBarogram: Convert `getContests()` to `contestData` computed property

--- ember/app/components/base-barogram.js
+++ ember/app/components/base-barogram.js
@@ -101,7 +101,7 @@ export default Component.extend({
     data = data.concat(this.activeTraces);
     data = data.concat(this.passiveTraces);
     data = data.concat(this.enlData);
-    data = data.concat(this.getContests());
+    data = data.concat(this.contestData.filter(Boolean));
 
     this.flot.setData(data);
   },
@@ -130,41 +130,30 @@ export default Component.extend({
     yaxis: 2,
   })),
 
-  getContests() {
-    // Skip the function if there are no contest markers
-    let contests = this.contests;
-    if (!contests) {
-      return [];
+  contestData: map('contests.@each.{times,color}', contest => {
+    let times = contest.get('times');
+    if (times.length < 1) {
+      return;
     }
 
-    // Iterate through the contests
-    return contests
-      .map(contest => {
-        let times = contest.get('times');
-        if (times.length < 1) {
-          return;
-        }
+    let color = contest.get('color');
 
-        let color = contest.get('color');
+    // Add the turnpoint markers to the markings array
+    let markings = times.map(time => ({
+      position: time * 1000,
+    }));
 
-        // Add the turnpoint markers to the markings array
-        let markings = times.map(time => ({
-          position: time * 1000,
-        }));
-
-        // Add the chart series for this contest to the data array
-        return {
-          marks: {
-            show: true,
-            lineWidth: 1,
-            toothSize: 6,
-            color,
-            fillColor: color,
-          },
-          data: [],
-          markdata: markings,
-        };
-      })
-      .filter(Boolean);
-  },
+    // Add the chart series for this contest to the data array
+    return {
+      marks: {
+        show: true,
+        lineWidth: 1,
+        toothSize: 6,
+        color,
+        fillColor: color,
+      },
+      data: [],
+      markdata: markings,
+    };
+  }),
 });
