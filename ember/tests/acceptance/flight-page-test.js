f69951a3e72f2cac1222581076e5f3681dd45c84
Tobias Bieniek <tobias@bieniek.cloud>
2020-07-12 15:19:17 +0200

    tests/acceptance/flight-page: Use Mirage.js instead of Polly.js

--- ember/tests/acceptance/flight-page-test.js
+++ ember/tests/acceptance/flight-page-test.js
@@ -2,15 +2,19 @@ import { visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import { module, test } from 'qunit';
 
+import { setupMirage } from 'ember-cli-mirage/test-support';
 import { percySnapshot } from 'ember-percy';
 
-import { setupPolly } from 'skylines/tests/test-helpers/setup-polly';
+import * as MockFlight from 'skylines/mirage/vcr/flights/87296';
 
 module('Acceptance | Flight Page', function (hooks) {
   setupApplicationTest(hooks);
-  setupPolly(hooks);
+  setupMirage(hooks);
 
   test('it works', async function (assert) {
+    this.server.get('/api/flights/87296/json', MockFlight.JSON);
+    this.server.get('/api/flights/87296', MockFlight.EXTENDED);
+
     await visit('/flights/87296');
     await percySnapshot('Flight Page');
     assert.dom('[data-test-pilot-names]').hasText('Tobias Bieniek');
