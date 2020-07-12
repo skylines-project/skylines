7003a091283aaa3a9040b42cc9a22d62a809e935
Tobias Bieniek <tobias@bieniek.cloud>
2020-07-12 16:04:47 +0200

    Disable `ember-cli-mirage` in dev mode

--- ember/config/environment.js
+++ ember/config/environment.js
@@ -55,6 +55,8 @@ module.exports = function (environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+
+    ENV['ember-cli-mirage'] = { enabled: false };
   }
 
   if (environment === 'test') {
