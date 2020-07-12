d765299a93636e663c1b6255797ce3cf3293d8ca
Tobias Bieniek <tobias@bieniek.cloud>
2020-07-12 14:41:49 +0200

    mirage: Add translation passthrough and locale endpoint

--- ember/mirage/config.js
+++ ember/mirage/config.js
@@ -1,26 +1,5 @@
-export default function() {
+export default function () {
+  this.passthrough('/translations/**');
 
-  // These comments are here to help you get started. Feel free to delete them.
-
-  /*
-    Config (with defaults).
-
-    Note: these only affect routes defined *after* them!
-  */
-
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
-
-  /*
-    Shorthand cheatsheet:
-
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
-
-    https://www.ember-cli-mirage.com/docs/route-handlers/shorthands
-  */
+  this.get('/api/locale', { locale: 'en' });
 }
