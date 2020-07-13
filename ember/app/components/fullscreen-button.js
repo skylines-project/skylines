81bf125301a6ee5a8b8158208ddfc57977a13362
Tobias Bieniek <tobias@bieniek.cloud>
2020-07-13 11:30:36 +0200

    FullscreenButton: Use `querySelector()` instead of jQuery

--- ember/app/components/fullscreen-button.js
+++ ember/app/components/fullscreen-button.js
@@ -3,13 +3,11 @@
 import Component from '@ember/component';
 import { action } from '@ember/object';
 
-import $ from 'jquery';
-
 export default Component.extend({
   tagName: '',
 
   toggle: action(function () {
     let element = this.fullscreenElement;
-    BigScreen.toggle($(element)[0]);
+    BigScreen.toggle(document.querySelector(element));
   }),
 });
