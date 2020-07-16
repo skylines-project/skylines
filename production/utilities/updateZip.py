58a91f7a1131aa49d28e5936e07196afc4bec1f4
hess8 <bret.hess@gmail.com>
2020-07-15 20:50:49 -0600

    progress, but errors

--- production/utilities/updateZip.py
+++ production/utilities/updateZip.py
@@ -1,14 +1,27 @@
+# '''run in windows cmd window (no admin needed)
+#         SET PATH=%PATH%;"C:\Program Files\7-Zip"
+#         python d:\skylinesC\production\utilities\updateZip.py
+#         
+#         '''
+
 import os,sys
 import py7zr
+import shutil
 
 def readfile(filepath):
     with open(filepath) as f:
         lines = f.read().splitlines() #strips the lines of \n
     return lines
 
+def sevenzip(tempPath,landPath):
+    os.system('7z a -t7z "{}" "{}"'.format(tempPath,landPath)) #quotes to handle spaces in windows file names
+#     with py7zr.SevenZipFile(tempPath, 'w') as archive:
+#                     archive.writeall(landPath, 'base')  #This seems slow, but uses threads well
 
-mainDir = 'Z:\\Condor\\Landscapes'
-otherDir = 'E:\\landscapes_for_symlinks'  #py7zr does not follow symlinks
+mainDir = 'Z:\\temp'
+otherDir = 'Z:\\temp2'
+# mainDir = 'Z:\\Condor\\Landscapes'
+# otherDir = 'E:\\landscapes_for_symlinks'  #py7zr does not follow symlinks
 zipDir = 'S:\\Skylines-C\landscapes-zip'
 
 allLands = []
@@ -23,8 +36,7 @@ for item in otherList:
         print ('Updated symlink for {}.'.format(item))
         mainPath = '{}\\{}'.format(mainDir,item)
         otherPath = '{}\\{}'.format(otherDir,item)
-        os.system('mklink /D "{}" "{}"'.format(mainPath,otherPath))
-
+        os.system('mklink /D "{}" "{}"'.format(mainPath,otherPath)) 
 
 #landscapes
 for item in os.listdir(mainDir):
@@ -39,13 +51,19 @@ for item in os.listdir(otherDir):
 #zips
 for item in os.listdir(zipDir):
     if item[-1]=='z':
-        allZips.append('{}\{}'.format(zipDir,item)) #leave out \ because used as string, not path
+        allZips.append('{}\{}'.format(zipDir,item)) 
         
-#create zips            
-for i, path, in enumerate(allLandPaths):
+#remove old temp zip files
+for item in os.listdir(mainDir):
+    if 'temp' in item:
+        tempPath = mainDir+'\\{}'.format(item.replace(' ','_'))
+        os.remove(tempPath)
+        
+#create zips                
+for i, landPath, in enumerate(allLandPaths):
     land = allLands[i]
 #     print (land)
-    iniPath = os.path.join(path,'{}.ini'.format(land))
+    iniPath = os.path.join(landPath,'{}.ini'.format(land))
     if os.path.exists(iniPath):
         lines = readfile(iniPath)
         if len(lines) > 1:
@@ -55,14 +73,15 @@ for i, path, in enumerate(allLandPaths):
             sys.exit('Stop: version line does not exist')
         zipPath = '{}\\{}.v{}.7z'.format(zipDir,land,version)
         if zipPath not in allZips and land != 'WestGermany3':
-            print(zipPath)
-#             os.system('py7zr c {}  {}/'.format(zipPath,path))
-#             with py7zr.SevenZipFile(zipPath, 'w') as archive:
-#                 archive.writeall(path, 'base')
+            print(zipPath,)
+#             try:
+            tempPath = mainDir+'\\temp_{}.7z'.format(land)
+            sevenzip(tempPath,landPath)
+            shutil.move(tempPath,zipPath)
+            print ('completed')
+#             except:
+#                 print (' error')
               
     else:
         print('Skipping: {} does not exist'.format(iniPath))
-        
-
-
 print ('Done')
