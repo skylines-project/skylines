b0e86022f5b9e02991dc44353e83078e1f3faa95
Tobias Bieniek <tobias@bieniek.cloud>
2020-07-23 00:04:36 +0200

    services/notification-counter: Use `rawTimeout()` to avoid test waiters

--- ember/app/services/notification-counter.js
+++ ember/app/services/notification-counter.js
@@ -1,9 +1,8 @@
 import { computed } from '@ember/object';
 import { gt } from '@ember/object/computed';
 import Service, { inject as service } from '@ember/service';
-import Ember from 'ember';
 
-import { task, timeout } from 'ember-concurrency';
+import { task, rawTimeout } from 'ember-concurrency';
 
 export default class NotificationCounterService extends Service {
   @service ajax;
@@ -23,11 +22,10 @@ export default class NotificationCounterService extends Service {
   }
 
   @(task(function* () {
-    // eslint-disable-next-line no-constant-condition
-    while (!Ember.testing) {
+    while (true) {
       let { events } = yield this.ajax.request('/api/notifications');
       this.set('counter', events.filter(it => it.unread).length);
-      yield timeout(60000);
+      yield rawTimeout(60000);
     }
   }).drop())
   updateTask;
