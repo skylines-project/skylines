9feb1710e2e08c1670da2d1452939f9345211f7d
Tobias Bieniek <tobias@bieniek.cloud>
2020-07-26 16:43:05 +0200

    FullscreenButton: Show warning if fullscreen mode is not available

--- ember/app/components/fullscreen-button.js
+++ ember/app/components/fullscreen-button.js
@@ -1,11 +1,19 @@
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 
 import screenfull from 'screenfull';
 
 export default class FullscreenButton extends Component {
+  @service intl;
+  @service notifications;
+
   @action toggle() {
-    let element = this.args.fullscreenElement;
-    screenfull.toggle(document.querySelector(element));
+    if (screenfull.isEnabled) {
+      let element = this.args.fullscreenElement;
+      screenfull.toggle(document.querySelector(element));
+    } else {
+      this.notifications.warning(this.intl.t('fullscreen-not-supported'));
+    }
   }
 }
