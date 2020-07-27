4a0cd468096d917c32e94192130c3bb290cb6e7f
Tobias Bieniek <tobias@bieniek.cloud>
2020-07-27 23:29:05 +0200

    FlightPhaseRow: Convert to Glimmer component

--- ember/app/components/flight-phase-row.js
+++ ember/app/components/flight-phase-row.js
@@ -1,34 +1,32 @@
-import Component from '@ember/component';
 import { action } from '@ember/object';
 import { equal } from '@ember/object/computed';
+import Component from '@glimmer/component';
 
 import safeComputed from '../computed/safe-computed';
 
 export default class extends Component {
-  tagName = '';
+  @equal('args.phase.type', 'circling') isCircling;
+  @equal('args.phase.type', 'powered') isPowered;
 
-  @equal('phase.type', 'circling') isCircling;
-  @equal('phase.type', 'powered') isPowered;
+  @equal('args.phase.circlingDirection', 'left') isCirclingLeft;
+  @equal('args.phase.circlingDirection', 'right') isCirclingRight;
 
-  @equal('phase.circlingDirection', 'left') isCirclingLeft;
-  @equal('phase.circlingDirection', 'right') isCirclingRight;
+  @safeComputed('args.phase.glideRate', gr => (Math.abs(gr) > 1000 ? Infinity : gr)) glideRate;
 
-  @safeComputed('phase.glideRate', gr => (Math.abs(gr) > 1000 ? Infinity : gr)) glideRate;
-
-  @safeComputed('selection', function (selection) {
-    let phase = this.phase;
+  @safeComputed('args.selection', function (selection) {
+    let phase = this.args.phase;
     return selection.start === phase.secondsOfDay && selection.end === phase.secondsOfDay + phase.duration;
   })
   selected;
 
   @action
   handleClick() {
-    let onSelect = this.onSelect;
+    let onSelect = this.args.onSelect;
 
     if (this.selected) {
       onSelect(null);
     } else {
-      let phase = this.phase;
+      let phase = this.args.phase;
       onSelect({
         start: phase.secondsOfDay,
         end: phase.secondsOfDay + phase.duration,
