04a313ddd092c07fe28c1bc38a970e7788ea4797
Tobias Bieniek <tobias@bieniek.cloud>
2020-07-27 22:37:53 +0200

    CesiumPlaneModel: Convert to Glimmer component

--- ember/app/components/cesium-plane-model.js
+++ ember/app/components/cesium-plane-model.js
@@ -1,16 +1,11 @@
 /* globals Cesium */
 
-import Component from '@ember/component';
 import { action } from '@ember/object';
+import Component from '@glimmer/component';
 
 import { transform } from 'ol/proj';
 
 export default class CesiumPlaneModel extends Component {
-  tagName = '';
-
-  scene = null;
-  fix = null;
-
   entity = Cesium.Model.fromGltf({
     url: '../../3d/AS21.glb',
     scale: 1,
@@ -28,13 +23,13 @@ export default class CesiumPlaneModel extends Component {
     this.entity.modelMatrix = Cesium.Transforms.headingPitchRollToFixedFrame(position, rotation);
   }
 
-  didInsertElement() {
-    super.didInsertElement(...arguments);
-    this.scene.primitives.add(this.entity);
+  constructor() {
+    super(...arguments);
+    this.args.scene.primitives.add(this.entity);
   }
 
-  willDestroyElement() {
-    super.willDestroyElement(...arguments);
-    this.scene.primitives.remove(this.entity);
+  willDestroy() {
+    super.willDestroy(...arguments);
+    this.args.scene.primitives.remove(this.entity);
   }
 }
