28f3699086a9a142c3faa682e93dd77dd68fc4b3
Tobias Bieniek <tobias@bieniek.cloud>
2020-08-02 17:45:03 +0200

    mirage/user: Add `club` relationship

--- ember/mirage/serializers/user.js
+++ ember/mirage/serializers/user.js
@@ -4,6 +4,15 @@ export default BaseSerializer.extend({
   serialize(object, request) {
     let json = BaseSerializer.prototype.serialize.apply(this, arguments);
 
+    if (object.club) {
+      json.club = {
+        id: Number(object.club.id),
+        name: object.club.name,
+      };
+    } else {
+      json.club = null;
+    }
+
     if (request.url.startsWith('/api/users/')) {
       delete json.altitudeUnit;
       delete json.distanceUnit;
--- ember/tests/mirage/user/get-test.js
+++ ember/tests/mirage/user/get-test.js
@@ -29,6 +29,31 @@ module('Mirage | User', function (hooks) {
       });
     });
 
+    test('returns the requested user and club', async function (assert) {
+      let user = this.server.create('user');
+      let club = this.server.create('club', { owner: user });
+      user.update({ club });
+
+      let response = await fetch(`/api/users/${user.id}`);
+      assert.equal(response.status, 200);
+
+      let responsePayload = await response.json();
+      assert.matchJson(responsePayload, {
+        id: 1,
+        club: {
+          id: 1,
+          name: 'AeroClub Aachen',
+        },
+        firstName: 'John',
+        followers: 107,
+        following: 128,
+        lastName: 'Doe',
+        name: 'John Doe',
+        trackingCallsign: 'JD',
+        trackingDelay: 0,
+      });
+    });
+
     test('returns 404 if the user does not exist', async function (assert) {
       let response = await fetch(`/api/users/42`);
       assert.equal(response.status, 404);
