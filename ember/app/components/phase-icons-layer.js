a59f921d4efcc473dd1b90d4e6c3ff73af8d52f5
Tobias Bieniek <tobias@bieniek.cloud>
2020-08-02 20:51:52 +0200

    PhaseIconsLayer: Convert to Glimmer component

--- ember/app/components/phase-icons-layer.js
+++ ember/app/components/phase-icons-layer.js
@@ -1,5 +1,5 @@
-import Component from '@ember/component';
 import { action } from '@ember/object';
+import Component from '@glimmer/component';
 
 import Feature from 'ol/Feature';
 import Point from 'ol/geom/Point';
@@ -24,34 +24,27 @@ END_ICON.load();
 const START_STYLE = new Style({ image: START_ICON });
 const END_STYLE = new Style({ image: END_ICON });
 
-export default Component.extend({
-  tagName: '',
+export default class PhaseIconsLayer extends Component {
+  layer = new VectorLayer({
+    source: new VectorSource({
+      useSpatialIndex: false,
+    }),
+    style(feature) {
+      return feature.get('type') === 'end' ? END_STYLE : START_STYLE;
+    },
+    zIndex: 100,
+  });
 
-  map: null,
-  coordinates: null,
-
-  init() {
-    this._super(...arguments);
-
-    this.layer = new VectorLayer({
-      source: new VectorSource({
-        useSpatialIndex: false,
-      }),
-      style(feature) {
-        return feature.get('type') === 'end' ? END_STYLE : START_STYLE;
-      },
-      zIndex: 100,
-    });
-
-    this.map.addLayer(this.layer);
-
+  constructor() {
+    super(...arguments);
+    this.args.map.addLayer(this.layer);
     this.updateSource();
-  },
+  }
 
   willDestroy() {
-    this._super(...arguments);
-    this.map.removeLayer(this.layer);
-  },
+    super.willDestroy(...arguments);
+    this.args.map.removeLayer(this.layer);
+  }
 
   @action
   updateSource() {
@@ -59,9 +52,10 @@ export default Component.extend({
 
     source.clear();
 
-    if (this.coordinates) {
-      let startPoint = new Point(this.coordinates[0]);
-      let endPoint = new Point(this.coordinates[this.coordinates.length - 1]);
+    let { coordinates } = this.args;
+    if (coordinates) {
+      let startPoint = new Point(coordinates[0]);
+      let endPoint = new Point(coordinates[coordinates.length - 1]);
 
       let startFeature = new Feature({ geometry: startPoint, type: 'start' });
       let endFeature = new Feature({ geometry: endPoint, type: 'end' });
@@ -69,5 +63,5 @@ export default Component.extend({
       source.addFeature(startFeature);
       source.addFeature(endFeature);
     }
-  },
-});
+  }
+}
