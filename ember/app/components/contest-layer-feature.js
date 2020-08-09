cb000a6fa81c2db93ded076a9a076054ff9d3450
Tobias Bieniek <tobias@bieniek.cloud>
2020-08-09 13:43:18 +0200

    ContestLayerFeature: Replace unnecessary computed property

--- ember/app/components/contest-layer-feature.js
+++ ember/app/components/contest-layer-feature.js
@@ -1,21 +1,17 @@
-import { computed } from '@ember/object';
 import Component from '@glimmer/component';
 
 import Feature from 'ol/Feature';
 
 export default class ContestLayerFeature extends Component {
-  @computed
-  get feature() {
-    let contest = this.args.contest;
-    return new Feature({
-      geometry: contest.geometry,
-      isTriangle: contest.isTriangle,
-    });
-  }
-
   constructor() {
     super(...arguments);
-    this.args.source.addFeature(this.feature);
+
+    const { contest, source } = this.args;
+
+    let { geometry, isTriangle } = contest;
+    this.feature = new Feature({ geometry, isTriangle });
+
+    source.addFeature(this.feature);
   }
 
   willDestroy() {
