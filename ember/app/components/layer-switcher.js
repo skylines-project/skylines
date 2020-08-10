e0b69aa0ca9fd004341a77f7c0177d6db3f1d5e2
Tobias Bieniek <tobias@bieniek.cloud>
2020-08-11 01:16:10 +0200

    LayerSwitcher: Convert to Glimmer component

--- ember/app/components/layer-switcher.js
+++ ember/app/components/layer-switcher.js
@@ -1,12 +1,10 @@
-import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 
 import { BASE_LAYERS, OVERLAY_LAYERS } from '../services/map-settings';
 
 export default class LayerSwitcher extends Component {
-  tagName = '';
-
   @service mapSettings;
 
   @tracked open = false;
