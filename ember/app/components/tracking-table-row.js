1a79052e8dd1b1e026c007bfed7e68c1c0bbd22c
Tobias Bieniek <tobias@bieniek.cloud>
2020-08-11 01:32:12 +0200

    TrackingTableRow: Convert to Glimmer component

--- ember/app/components/tracking-table-row.js
+++ ember/app/components/tracking-table-row.js
@@ -1,10 +1,8 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 
 import safeComputed from '../computed/safe-computed';
 
 export default class TrackingTableRow extends Component {
-  tagName = '';
-
   @safeComputed('track.altitude', 'track.elevation', (altitude, elevation) => Math.max(altitude - elevation, 0))
   altitudeAGL;
 }
