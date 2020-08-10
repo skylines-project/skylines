7b2bf0bd6a4c9a919e1dfcee3afc10fa0311a7d8
Tobias Bieniek <tobias@bieniek.cloud>
2020-08-11 01:08:22 +0200

    FlightListNav: Convert to Glimmer component

--- ember/app/components/flight-list-nav.js
+++ ember/app/components/flight-list-nav.js
@@ -1,22 +1,20 @@
-import Component from '@ember/component';
 import { action } from '@ember/object';
 import { notEmpty } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
+import Component from '@glimmer/component';
 
 import safeComputed from '../computed/safe-computed';
 import addDays from '../utils/add-days';
 
 export default class FlightListNav extends Component {
-  tagName = '';
-
   @service account;
   @service pinnedFlights;
   @service router;
 
   @notEmpty('pinnedFlights.pinned') hasPinned;
 
-  @safeComputed('date', date => addDays(date, -1)) prevDate;
-  @safeComputed('date', date => addDays(date, 1)) nextDate;
+  @safeComputed('args.date', date => addDays(date, -1)) prevDate;
+  @safeComputed('args.date', date => addDays(date, 1)) nextDate;
 
   @action
   dateSelected(_, date) {
