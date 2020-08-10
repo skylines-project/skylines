251143afad694aaadfe297387516b7205f506dca
Tobias Bieniek <tobias@bieniek.cloud>
2020-08-11 01:15:16 +0200

    LayerSwitcher: Convert `open` to tracked property

--- ember/app/components/layer-switcher.js
+++ ember/app/components/layer-switcher.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 import { BASE_LAYERS, OVERLAY_LAYERS } from '../services/map-settings';
 
@@ -8,7 +9,7 @@ export default class LayerSwitcher extends Component {
 
   @service mapSettings;
 
-  open = false;
+  @tracked open = false;
 
   BASE_LAYERS = BASE_LAYERS;
   OVERLAY_LAYERS = OVERLAY_LAYERS;
