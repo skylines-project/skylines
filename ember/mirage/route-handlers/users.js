ed995f177b64520347a932029a45e78f3a537efd
Tobias Bieniek <tobias@bieniek.cloud>
2020-08-23 10:14:02 +0200

    mirage: Implement `POST /api/users/recover` route handler

--- ember/mirage/route-handlers/users.js
+++ ember/mirage/route-handlers/users.js
@@ -26,4 +26,50 @@ export function register(server) {
 
     return new Response(200, {}, { result });
   });
+
+  server.post('/api/users/recover', function (schema, request) {
+    let { user: currentUser } = getSession(schema);
+
+    let json;
+    try {
+      json = JSON.parse(request.requestBody);
+    } catch (error) {
+      return new Response(400, {}, { error: 'invalid-request' });
+    }
+
+    if ('recoveryKey' in json) {
+      let { password, recoveryKey } = json;
+      if (!password || !recoveryKey) {
+        return new Response(422, {}, { error: 'validation-failed' });
+      }
+
+      let user = schema.users.findBy({ recoveryKey });
+      if (!user) {
+        return new Response(422, {}, { error: 'recovery-key-unknown' });
+      }
+
+      user.update({ password, recoveryKey: null });
+
+      return {};
+    } else {
+      let { email } = json;
+      if (!email) {
+        return new Response(422, {}, { error: 'validation-failed' });
+      }
+
+      let user = schema.users.findBy({ email });
+      if (!user) {
+        return new Response(422, {}, { error: 'email-unknown' });
+      }
+
+      user.update({ recoveryKey: 'abc123def' });
+
+      if (currentUser?.admin) {
+        let url = `http://skylines.aero/users/recover?key=${user.recoveryKey}`;
+        return { url };
+      } else {
+        return {};
+      }
+    }
+  });
 }
