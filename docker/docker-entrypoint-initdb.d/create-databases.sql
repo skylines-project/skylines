f93a5397fa2a48e9c2b6a04b19a2a73e635f8bc4
Tobias Bieniek <tobias@bieniek.cloud>
2020-12-28 17:52:43 +0100

    Docker: Use `docker-entrypoint-initdb.d` to create necessary databases

---
+++ docker/docker-entrypoint-initdb.d/create-databases.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE skylines;
+\connect skylines;
+CREATE EXTENSION postgis;
+CREATE EXTENSION fuzzystrmatch;
+
+CREATE DATABASE skylines_test;
+\connect skylines_test;
+CREATE EXTENSION postgis;
+CREATE EXTENSION fuzzystrmatch;
