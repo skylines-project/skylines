8ecce695517426e828f1f5e4d42c8973b170a8a3
Tobias Bieniek <tobias@bieniek.cloud>
2021-01-07 23:03:20 +0100

    mirage: Add basic `POST /api/settings` route handler

--- ember/mirage/route-handlers/settings.js
+++ ember/mirage/route-handlers/settings.js
@@ -11,4 +11,43 @@ export function register(server) {
 
     return user;
   });
+
+  server.post('/api/settings', function (schema, request) {
+    let { user } = getSession(schema);
+    if (!user) {
+      return new Response(401, {}, { error: 'invalid-token' });
+    }
+
+    let data = JSON.parse(request.requestBody);
+
+    if ('email' in data) {
+      let { email } = data;
+      if (email !== user.email && schema.users.findBy({ email })) {
+        return new Response(422, {}, { error: 'email-exists-already' });
+      }
+
+      user.update({ email });
+    }
+
+    const simpleFields = [
+      'altitudeUnit',
+      'distanceUnit',
+      'firstName',
+      'lastName',
+      'liftUnit',
+      'speedUnit',
+      'trackingCallsign',
+      'trackingDelay',
+    ];
+    for (let key of simpleFields) {
+      if (key in data) {
+        user.update({ [key]: data[key] });
+      }
+    }
+
+    // TODO password update
+    // TODO club update
+
+    return new Response(200, {}, {});
+  });
 }
