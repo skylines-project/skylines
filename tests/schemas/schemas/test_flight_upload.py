e46bf49f9ca38ade915d5b44e346f7738790a66c
Tobias Bieniek <tobias@bieniek.cloud>
2021-01-16 18:48:39 +0100

    FlightUploadSchema: Add optional WeGlide fields

--- tests/schemas/schemas/test_flight_upload.py
+++ tests/schemas/schemas/test_flight_upload.py
@@ -1,3 +1,4 @@
+from datetime import date
 import pytest
 
 from skylines.schemas import FlightUploadSchema
@@ -53,4 +54,60 @@ def test_empty_pilot_name(schema):
 def test_pilot_id_and_name(schema):
     data, errors = schema.load({"pilotId": 123, "pilotName": "JD"})
     assert data == {"pilot_id": 123}
+    assert errors == {}
+
+
+def test_with_weglide(schema):
+    data, errors = schema.load(
+        {"pilotId": 123, "weglideUserId": 123, "weglideBirthday": "2020-01-01"}
+    )
+    assert data == {
+        "pilot_id": 123,
+        "weglideUserId": 123,
+        "weglideBirthday": date(2020, 1, 1),
+    }
+    assert errors == {}
+
+
+def test_weglide_with_broken_id(schema):
+    data, errors = schema.load(
+        {"pilotId": 123, "weglideUserId": "foo", "weglideBirthday": "2020-01-01"}
+    )
+    assert data == {"pilot_id": 123, "weglideBirthday": date(2020, 1, 1)}
+    assert errors == {"weglideUserId": [u"Not a valid integer."]}
+
+
+def test_weglide_with_missing_id(schema):
+    data, errors = schema.load({"pilotId": 123, "weglideBirthday": "2020-01-01"})
+    assert data == {"pilot_id": 123, "weglideBirthday": date(2020, 1, 1)}
+    assert errors == {}
+
+
+def test_weglide_with_empty_id(schema):
+    data, errors = schema.load(
+        {"pilotId": 123, "weglideUserId": "", "weglideBirthday": "2020-01-01"}
+    )
+    assert data == {"pilot_id": 123, "weglideBirthday": date(2020, 1, 1)}
+    assert errors == {}
+
+
+def test_weglide_with_broken_birthday(schema):
+    data, errors = schema.load(
+        {"pilotId": 123, "weglideUserId": 123, "weglideBirthday": "2abc-01-01"}
+    )
+    assert data == {"pilot_id": 123, "weglideUserId": 123}
+    assert errors == {"weglideBirthday": [u"Not a valid date."]}
+
+
+def test_weglide_with_missing_birthday(schema):
+    data, errors = schema.load({"pilotId": 123, "weglideUserId": 123})
+    assert data == {"pilot_id": 123, "weglideUserId": 123}
+    assert errors == {}
+
+
+def test_weglide_with_empty_birthday(schema):
+    data, errors = schema.load(
+        {"pilotId": 123, "weglideUserId": 123, "weglideBirthday": ""}
+    )
+    assert data == {"pilot_id": 123, "weglideUserId": 123}
     assert errors == {}
