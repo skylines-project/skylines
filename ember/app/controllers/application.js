d26743af9114bbab4d53d0a58352945872902c85
Tobias Bieniek <tobias@bieniek.cloud>
2021-01-21 17:06:00 +0100

    controllers/application: Use `router` service

--- ember/app/controllers/application.js
+++ ember/app/controllers/application.js
@@ -1,9 +1,12 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class ApplicationController extends Controller {
+  @service router;
+
   @action search(text) {
-    this.transitionToRoute('search', {
+    this.router.transitionTo('search', {
       queryParams: { text },
     });
   }
